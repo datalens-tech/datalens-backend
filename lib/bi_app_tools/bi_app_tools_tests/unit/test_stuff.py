5fc2705654e66221a420b639f5aed35f1c1d8a0d
Konstantin Chupin <91148200+ya-kc@users.noreply.github.com>
2023-08-29 18:47:14 +0200

    [BI-4830] Remove tier-0 build rudiments (entry points) (#199)

--- lib/bi_app_tools/bi_app_tools_tests/unit/test_stuff.py
+++ lib/bi_app_tools/bi_app_tools_tests/unit/test_stuff.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 
-from bi_app_tools.entry_point import BIEntryPoint
-
 
 def test_some():
-    ep = BIEntryPoint()
-    assert ep.usage
+    assert True
--- lib/bi_core/bi_core/entry_point.py
+++
@@ -1,3 +0,0 @@
-from __future__ import annotations
-
-from bi_app_tools.entry_point import BIEntryPoint  # TODO: finish the transition  # noqa
