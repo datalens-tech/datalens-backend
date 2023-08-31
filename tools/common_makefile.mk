6280b7f096b395b9da6ea2960ebb8cc037cc3254
Grigory Statsenko <altvod@users.noreply.github.com>
2023-08-31 15:41:39 +0200

    Cleaned up some legacy stuff in makefiles (#228)

--- tools/common_makefile.mk
+++ tools/common_makefile.mk
@@ -16,27 +16,10 @@ docker-remote:
 	cd ../../tools/local_dev && $(MAKE) docker-remote
 
 
-# Because importing some of the bi_core stuff unexpectedly imports many other packages
-# (former `bi_core[db]`, now in the install_requires),
-# all local dependencies deleted in `requirements_actual.txt`, but required by bi_core, have to be installed.
-# Override in including makefiles as needed (pretty much just `LOCAL_LIBS := $(LOCAL_LIBS) lib/bi_formula`)
-# WARNING: `lib/bi_common` depends on `bi-sqlalchemy-postgres yandex-bi-app-tools
-# yandex-bi-configs`, and specifying them together breaks installation on pip>=20.2.
-# Same with `bi_integration_tests` -> `lib/bi_cloud_integration`
-LOCAL_LIBS = lib/bi_core lib/bi_testing lib/bi_sqlalchemy_chyt lib/clickhouse-sqlalchemy lib/sqlalchemy_metrika_api lib/statcommons
-# See also: `local_dev/requirements/all_local_packages.lst`, documented in `local_dev/README.md`
-
-
 requirements_actual.txt: requirements.txt
 	python -c "import re; data = open('requirements.txt').read(); data = re.sub(r'\n(sqlalchemy-metrika-api|sqlalchemy-statface-api|statcommons|statinfra-chyt-sqlalchemy|statinfra-clickhouse-sqlalchemy|yandex-bi-core|yandex-bi-formula)[^\n]+', '', data); fobj = open('requirements_actual.txt', 'w'); fobj.write(data); fobj.close()"
 	touch -r requirements.txt requirements_actual.txt
 # ^ `touch` should make the clean+build undistinguishable from the original.
-
-
-requirements_tox.txt: requirements_actual.txt
-	( cat requirements_actual.txt; for path in $(LOCAL_LIBS); do printf -- "-e file:./../../%s#egg=%s\n" "$$path" "$$(sed -rn "s/ *name='([^']+)'.*/\1/ p" "../../$$path/setup.py")"; done; ) > requirements_tox.txt
-	sed -i '/^certifi-yandex==/ d' requirements_tox.txt
-	printf 'certifi-yandex\n' >> requirements_tox.txt
 
 
 .PHONY: tests-tier1-clean
@@ -47,12 +30,6 @@ tests-tier1-clean:
 .PHONY: build-tier1-clean
 build-tier1-clean:
 	rm -rf local_libs requirements_actual.txt requirements_tox.txt
-
-
-.PHONY: build-tier1-prepare
-build-tier1-prepare: build-tier1-clean requirements_actual.txt
-	mkdir -p local_libs
-	for path in $(LOCAL_LIBS); do cp -a "../$$path" ./local_libs/; done
 
 
 image := $$(python -c "import json; import sys; f = open('app/.package.json'); r = json.load(f); sys.stdout.write(r['meta']['name']); f.close()")
