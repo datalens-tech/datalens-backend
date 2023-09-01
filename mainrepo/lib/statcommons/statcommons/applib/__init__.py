e9a2985cb9f360fa7ff287ba93d85e0f0528ef4d
dmi-feo <fdi1992@gmail.com>
2023-09-01 21:46:30 +0200

    remove unused stuff from statcommons + run tests on file removals (#266)

--- mainrepo/lib/statcommons/statcommons/applib/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding: utf8
-"""
-...
-"""
-
-from __future__ import division, absolute_import, print_function, unicode_literals
--- mainrepo/lib/statcommons/statcommons/base.py
+++
@@ -1,7 +0,0 @@
-# coding: utf8
-# pylint: disable=unused-wildcard-import
-"""
-Utilities not fitting in any category.
-"""
-
-from __future__ import division, absolute_import, print_function, unicode_literals
