96a27451e1a323c1975e05468ba58f3fae01e834
KonstantAnxiety <58992437+KonstantAnxiety@users.noreply.github.com>
2023-09-05 11:51:52 +0300

    Fix import in chyt internal connector tests (#334)

--- lib/bi_connector_chyt_internal/bi_connector_chyt_internal_tests/unit/test_connection_form.py
+++ lib/bi_connector_chyt_internal/bi_connector_chyt_internal_tests/unit/test_connection_form.py
@@ -1,4 +1,4 @@
-from bi_api_connector.form_config.testing.test_connection_form_base import ConnectionFormTestBase
+from bi_api_lib_testing.connection_form_base import ConnectionFormTestBase
 from bi_api_connector.i18n.localizer import CONFIGS as BI_API_CONNECTOR_CONFIGS
 from bi_connector_chyt_internal.bi.connection_form.user_auth_form.form_config import CHYTInternalUserConnectionFormFactory
 from bi_connector_chyt_internal.bi.connection_form.token_auth_form.form_config import CHYTInternalTokenConnectionFormFactory
