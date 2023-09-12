4084dfd929c76a47aeb83dc80d47a5a0edd426bf
KonstantAnxiety <58992437+KonstantAnxiety@users.noreply.github.com>
2023-09-12 23:35:55 +0300

    Move setup_iptables script to app/bi_api; remove sync_ch_tables script (#492)

--- mainrepo/lib/bi_core/bi_core/bin/query_executor_async.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from __future__ import annotations
-
-from bi_core.connection_executors.remote_query_executor.app_async import async_qe_main as main
-
-
-if __name__ == '__main__':
-    main()
