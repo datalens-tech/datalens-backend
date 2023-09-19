d154cf39e3479e2eb522a749283c2a9cdf38dc62
Grigory Statsenko <altvod@users.noreply.github.com>
2023-09-19 09:44:05 +0200

    Updated formula ref i18n domain names (#612)

--- lib/bi_connector_mssql/bi_connector_mssql/formula_ref/i18n.py
+++ lib/bi_connector_mssql/bi_connector_mssql/formula_ref/i18n.py
@@ -7,7 +7,7 @@ from dl_i18n.localizer_base import TranslationConfig
 
 import bi_connector_mssql as package
 
-DOMAIN = f"bi_formula_ref_{package.__name__}"
+DOMAIN = f"dl_formula_ref_{package.__name__}"
 
 _LOCALE_DIR = os.path.join(os.path.dirname(__file__), "..", "locales")
 
