bee0f86a1c2dd362e5eb10a2839004d116a6c3bb
Andrey Snytin <asnytin@yandex-team.ru>
2023-09-20 17:29:16 +0200

    fixed connection api schema target_cls for mdb ch & mysql (#686)

--- lib/bi_connector_clickhouse_mdb/bi_connector_clickhouse_mdb/bi/api_schema/connection.py
+++ lib/bi_connector_clickhouse_mdb/bi_connector_clickhouse_mdb/bi/api_schema/connection.py
@@ -2,6 +2,8 @@ from dl_connector_clickhouse.bi.api_schema.connection import ClickHouseConnectio
 
 from bi_connector_mdb_base.bi.api_schema.connection_mixins import MDBDatabaseSchemaMixin
 
+from bi_connector_clickhouse_mdb.core.us_connection import ConnectionClickhouseMDB
+
 
 class ClickHouseMDBConnectionSchema(MDBDatabaseSchemaMixin, ClickHouseConnectionSchema):
-    pass
+    TARGET_CLS = ConnectionClickhouseMDB
--- lib/bi_connector_mysql_mdb/bi_connector_mysql_mdb/bi/api_schema/connection.py
+++ lib/bi_connector_mysql_mdb/bi_connector_mysql_mdb/bi/api_schema/connection.py
@@ -1,6 +1,7 @@
 from bi_connector_mdb_base.bi.api_schema.connection_mixins import MDBDatabaseSchemaMixin
 from bi_connector_mysql.bi.api_schema.connection import MySQLConnectionSchema
+from bi_connector_mysql_mdb.core.us_connection import ConnectionMySQLMDB
 
 
 class MySQLMDBConnectionSchema(MDBDatabaseSchemaMixin, MySQLConnectionSchema):
-    pass
+    TARGET_CLS = ConnectionMySQLMDB
