4c2204e7a7b43a2da0601fb9a8cd636ca2784c6c
Grigory Statsenko <altvod@users.noreply.github.com>
2023-09-21 12:14:50 +0200

    Fixed Greenplum tests (#702)

--- mainrepo/lib/dl_connector_greenplum/dl_connector_greenplum_tests/db/config.py
+++ mainrepo/lib/dl_connector_greenplum/dl_connector_greenplum_tests/db/config.py
@@ -2,6 +2,7 @@ from dl_api_lib_testing.configuration import BiApiTestEnvironmentConfiguration
 from dl_core_testing.configuration import DefaultCoreTestConfiguration
 from dl_testing.containers import get_test_container_hostport
 
+
 # Infra settings
 CORE_TEST_CONFIG = DefaultCoreTestConfiguration(
     host_us_http=get_test_container_hostport("us", fallback_port=51911).host,
@@ -9,13 +10,13 @@ CORE_TEST_CONFIG = DefaultCoreTestConfiguration(
     host_us_pg=get_test_container_hostport("pg-us", fallback_port=51910).host,
     port_us_pg_5432=get_test_container_hostport("pg-us", fallback_port=51910).port,
     us_master_token="AC1ofiek8coB",
+    core_connector_ep_names=["greenplum"],
 )
 
 DB_CORE_URL = f'bi_postgresql://datalens:qwerty@{get_test_container_hostport("db-postgres-13", fallback_port=52301).as_pair()}/test_data'
 
 BI_TEST_CONFIG = BiApiTestEnvironmentConfiguration(
-    bi_api_connector_whitelist=["greenplum"],
-    core_connector_whitelist=["greenplum"],
+    api_connector_ep_names=["greenplum"],
     core_test_config=CORE_TEST_CONFIG,
     ext_query_executer_secret_key="_some_test_secret_key_",
 )
