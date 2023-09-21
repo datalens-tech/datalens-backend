c0aec580ec5bc8f360c9f537bf634154bd1b9012
Nick Proskurin <42863572+MCPN@users.noreply.github.com>
2023-09-21 18:11:05 +0200

    BI-4817: disable failing greenplum test (#705)

--- mainrepo/lib/dl_connector_greenplum/dl_connector_greenplum_tests/db/api/test_data.py
+++ mainrepo/lib/dl_connector_greenplum/dl_connector_greenplum_tests/db/api/test_data.py
@@ -6,10 +6,15 @@ from dl_api_lib_testing.connector.data_api_suites import (
     DefaultConnectorDataResultTestSuite,
 )
 from dl_connector_greenplum_tests.db.api.base import GreenplumDataApiTestBase
+from dl_testing.regulated_test import RegulatedTestParams
 
 
 class TestGreenplumDataResult(GreenplumDataApiTestBase, DefaultConnectorDataResultTestSuite):
-    pass
+    test_params = RegulatedTestParams(
+        mark_tests_failed={
+            DefaultConnectorDataResultTestSuite.test_array_not_contains_filter: "BI-4951",  # TODO: FIXME
+        }
+    )
 
 
 class TestGreenplumDataGroupBy(GreenplumDataApiTestBase, DefaultConnectorDataGroupByFormulaTestSuite):
