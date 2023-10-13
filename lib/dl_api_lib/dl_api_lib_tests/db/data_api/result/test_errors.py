c46e1dd1b42ba920e89499202f77e8218b309a83
Nick Proskurin <42863572+MCPN@users.noreply.github.com>
2023-10-13 18:27:40 +0200

    Add more result error tests (#19)

--- lib/dl_api_lib/dl_api_lib_tests/db/data_api/result/test_errors.py
+++ lib/dl_api_lib/dl_api_lib_tests/db/data_api/result/test_errors.py
@@ -1,6 +1,9 @@
 from dl_api_client.dsmaker.primitives import ResultField
 from dl_api_lib_tests.db.base import DefaultApiTestBase
-from dl_constants.enums import CalcMode
+from dl_constants.enums import (
+    AggregationFunction,
+    CalcMode,
+)
 
 
 class TestResultErrors(DefaultApiTestBase):
@@ -22,3 +25,52 @@ class TestResultErrors(DefaultApiTestBase):
         result_resp = data_api.get_result(dataset=ds, fields=[ds.find_field(title)], fail_ok=True)
         assert result_resp.status_code == 400
         assert result_resp.bi_status_code == "ERR.DS_API.FORMULA.PARSE.UNEXPECTED_EOF.EMPTY_FORMULA"
+
+    def test_filter_errors(self, saved_dataset, data_api):
+        ds = saved_dataset
+        ds.result_schema["New date"] = ds.field(formula="DATE(NOW())")  # just a date field
+        ds.result_schema["New bool"] = ds.field(formula='"P" = "NP"')  # just a bool field
+
+        # invalid value for date
+        result_resp = data_api.get_result(
+            dataset=ds,
+            fields=[ds.find_field(title="New date")],
+            filters=[ds.find_field(title="New date").filter("EQ", values=["Office Supplies"])],
+            fail_ok=True,
+        )
+        assert result_resp.status_code == 400
+        assert result_resp.bi_status_code == "ERR.DS_API.FILTER.INVALID_VALUE"
+
+        # a very special check for invalid bool values
+        result_resp = data_api.get_result(
+            dataset=ds,
+            fields=[ds.find_field(title="New date")],
+            filters=[ds.result_schema["New bool"].filter("EQ", values=[""])],
+            fail_ok=True,
+        )
+        assert result_resp.status_code == 400
+        assert result_resp.bi_status_code == "ERR.DS_API.FILTER.INVALID_VALUE"
+
+        # unknown field
+        result_resp = data_api.get_result(
+            dataset=ds,
+            fields=[ds.find_field(title="New date")],
+            filters=[ds.field(title="Made-up field").filter("EQ", values=["whatever"])],
+            fail_ok=True,
+        )
+        assert result_resp.status_code == 400
+        assert result_resp.bi_status_code == "ERR.DS_API.FIELD.NOT_FOUND"
+
+    def test_invalid_group_by_configuration(self, saved_dataset, data_api):
+        ds = saved_dataset
+        ds.result_schema["Measure"] = ds.field(title="Measure", aggregation=AggregationFunction.sum)
+
+        result_resp = data_api.get_result(
+            dataset=saved_dataset,
+            fields=[ds.find_field(title="Measure")],
+            disable_group_by=True,
+            fail_ok=True,
+        )
+        assert result_resp.status_code == 400
+        assert result_resp.bi_status_code == "ERR.DS_API.INVALID_GROUP_BY_CONFIGURATION"
+        assert result_resp.json["message"] == "Invalid parameter disable_group_by for dataset with measure fields"
