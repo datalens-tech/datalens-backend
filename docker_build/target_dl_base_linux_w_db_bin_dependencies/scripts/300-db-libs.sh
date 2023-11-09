5c89abc0b8d53711d7f7662e22f461ba27533410
Max Zuev <mail@thenno.me>
2023-11-09 20:28:34 +0100

    Fix gh_build_image build

--- docker_build/target_dl_base_linux_w_db_bin_dependencies/scripts/300-db-libs.sh
+++ docker_build/target_dl_base_linux_w_db_bin_dependencies/scripts/300-db-libs.sh
@@ -3,5 +3,4 @@ set -eu
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get install --yes --fix-missing \
-  # For MSSQL
-  freetds-dev tdsodbc unixodbc unixodbc-dev libtcmalloc-minimal4
+  freetds-dev tdsodbc unixodbc unixodbc-dev libtcmalloc-minimal4  # For MSSQL
