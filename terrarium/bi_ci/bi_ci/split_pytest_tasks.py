221120e090ee3f9eb8206cd31e1e00dc84b00f9d
ovsds <ovsds@yandex-team.ru>
2023-11-29 15:31:41 +0100

    fix: and refactor pytest_split

--- terrarium/bi_ci/bi_ci/split_pytest_tasks.py
+++ terrarium/bi_ci/bi_ci/split_pytest_tasks.py
@@ -1,7 +1,7 @@
-from collections import defaultdict
-from functools import partial
 import json
-from pathlib import Path
+import functools
+import pathlib
+import typing
 
 import clize
 from tomlkit import parse
@@ -20,54 +20,95 @@ target_path = "aio caches capabilities common"
 labels = ["fat"]
 """
 
+DEFAULT_MODE = "base"
 
-def format_output(name: str, sections: list[tuple[str, str]]) -> str:
+
+def format_output(name: str, sections: list[tuple[pathlib.Path, str]]) -> str:
     data = [f"{path}:{target}" for path, target in sections]
+
     return f"{name}={json.dumps(data)}"
+
+
+def read_package_paths(targets_path: pathlib.Path) -> typing.Generator[str, None, None]:
+    with open(targets_path, "r") as file:
+        data = file.read()
+        data = data.strip()
+        data = data.replace("'", '"')
+
+    for item in json.loads(data):
+        yield item.strip()
+
+
+def read_pytest_targets(path: pathlib.Path) -> typing.Optional[dict[str, typing.Any]]:
+    if not path.is_file():
+        print(f"File {path} not found")
+        raise FileNotFoundError(f"File {path} not found")
+
+    with open(path, "r") as file:
+        toml_data = parse(file.read())
+
+    return toml_data.get("datalens", {}).get("pytest", {})
+
+
+def get_package_tests(
+    package_path: pathlib.Path,
+    requested_mode: str,
+) -> typing.Generator[tuple[pathlib.Path, str], None, None]:
+    try:
+        pytest_targets = read_pytest_targets(package_path / "pyproject.toml")
+    except FileNotFoundError:
+        return
+
+    for section in pytest_targets.keys():
+        spec = pytest_targets.get(section, {})
+        labels = spec.get("labels", [])
+
+        if requested_mode in labels:
+            yield package_path, section
+
+
+def get_default_package_tests(
+    package_path: pathlib.Path,
+) -> typing.Generator[tuple[pathlib.Path, str], None, None]:
+    try:
+        pytest_targets = read_pytest_targets(package_path / "pyproject.toml")
+    except FileNotFoundError:
+        return
+
+    if len(pytest_targets) == 0:
+        yield package_path, "__default__"
+
+    for section, spec in pytest_targets.items():
+        labels = spec.get("labels", [])
+
+        if len(labels) == 0:
+            yield package_path, section
+
+
+def get_tests(
+    requested_mode: str,
+    root_dir: pathlib.Path,
+    test_targets_json_path: pathlib.Path,
+) -> typing.Generator[tuple[pathlib.Path, str], None, None]:
+
+    for package_path in read_package_paths(test_targets_json_path):
+        if requested_mode == DEFAULT_MODE:
+            yield from get_default_package_tests(root_dir / package_path)
+        else:
+            yield from get_package_tests(root_dir / package_path, requested_mode)
 
 
 def split_tests(
     requested_mode: str,
-    root_dir: Path,
-    test_targets_json_path: Path,
+    root_dir: pathlib.Path,
+    test_targets_json_path: pathlib.Path,
 ) -> None:
-    raw = open(test_targets_json_path).read().strip().replace("'", '"')
-    paths = json.loads(raw)
-
-    # print(paths)
-    split_result: dict[str, list[tuple]] = defaultdict(list)
-
-    # ipdb.set_trace()
-    for package_path in paths:
-        path = root_dir / package_path.strip() / "pyproject.toml"
-        # print(path)
-        if not path.is_file():
-            continue
-
-        with open(path, "r") as file:
-            toml_data = parse(file.read())
-
-        pytest_targets = toml_data.get("datalens", {}).get("pytest", {})
-
-        if pytest_targets:
-            for section in pytest_targets.keys():
-                spec = pytest_targets.get("section", {})
-                # supporting only a single label for now
-                for category in spec.get("labels", []):
-                    split_result[category].append((package_path, section))
-                    break
-                else:
-                    split_result["base"].append((package_path, section))
-        else:
-            split_result["base"].append((package_path, "__default__"))
-
-    print(format_output(f"split_{requested_mode}", split_result[requested_mode]))
-    # note: I would expect following to be usable, but no idea how to make it with GHA :(
-    # for mode, result in sorted(split_result.items()):
-    #     print(format_output(f"split_{mode}", result), end=" ")
+    result = list(get_tests(requested_mode, root_dir, test_targets_json_path))
+    formatted_output = format_output(f"split_{requested_mode}", result)
+    print(formatted_output)
 
 
-cmd = partial(clize.run, split_tests)
+cmd = functools.partial(clize.run, split_tests)
 
 if __name__ == "__main__":
     cmd()
