daddeb185f671eadb9f10f749aae58f5a35a7c1c
Ovsyannikov Dmitrii <ovsds@yandex-team.ru>
2023-12-19 16:07:10 +0100

    BI-5142: add build to release (#180)
    * (BI-5142) feat: add debian_docker build
    
    * (BI-5142) feat: add build to release
    
    * (BI-5142) feat: add build to release

---
+++ docker_build/debian_docker/scripts/100-docker.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -eu
+export DEBIAN_FRONTEND=noninteractive
+
+echo 'Installing docker packages...'
+
+echo '  Installing docker source list...'
+apt-get install --yes \
+  curl \
+  gpg \
+  ca-certificates
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian/ bookworm stable" > /etc/apt/sources.list.d/docker.list
+
+apt-get update
+
+echo '  Installing packages...'
+
+apt-get install --yes \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-compose-plugin
