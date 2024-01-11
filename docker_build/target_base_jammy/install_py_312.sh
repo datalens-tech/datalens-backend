a6ee6a64b407831f047fff4fc03dba9263af3392
Dmitry Nadein <nadein@nebius.com>
2024-01-11 16:09:38 +0000

    Update python to version 3.12, bump dependencies versions

--- docker_build/target_base_jammy/install_py_312.sh
+++ docker_build/target_base_jammy/install_py_312.sh
@@ -2,14 +2,10 @@
 
 set -exu
 
-add-apt-repository -y ppa:deadsnakes/ppa
+echo 'deb http://ppa.launchpad.net/deadsnakes/ppa/ubuntu jammy main' > /etc/apt/sources.list.d/deadsnakes.list
 
-for y in $(seq 1 5)
-do
-   apt-get update || sleep "$y" ;
-done
+apt-get update
 
 apt-get install -y python3.12 python3.12-dev python3-pip python3.12-venv || sleep "$y" ;
-
 
 ln -sf python3.12 /usr/bin/python && ln -sf python3.12 /usr/bin/python3
