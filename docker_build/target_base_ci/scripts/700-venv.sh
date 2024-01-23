7414f6ba8cf79d978ca5ae4581ac1dc3f0182c18
Dmitry Nadein <nadein@nebius.com>
2024-01-23 22:22:35 +0000

    remove pandas installation from wheel

--- docker_build/target_base_ci/scripts/700-venv.sh
+++ docker_build/target_base_ci/scripts/700-venv.sh
@@ -10,10 +10,5 @@ pip install --no-input poetry==1.7.1
 
 cd /src/metapkg
 
-# TODO FIX: Remove pandas install from github repository after pandas removing
-mkdir datalens_wheels_latest
-curl -sL https://github.com/datalens-tech/datalens-wheels/archive/refs/tags/0.2.0.tar.gz | tar xfz - -C datalens_wheels_latest/
-pip install datalens_wheels_latest/datalens-wheels-0.2.0/pandas_binary/pandas-2.0.3-cp312-cp312-linux_x86_64.whl
-
 poetry export --without=dev --without=ci --without-hashes --format=requirements.txt > requirements.txt
 pip install -r requirements.txt
