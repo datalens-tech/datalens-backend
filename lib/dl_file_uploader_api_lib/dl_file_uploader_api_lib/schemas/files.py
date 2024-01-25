5f8f8ba9b5b426c68efd113ef8244e04f3b828ea
vallbull <33630435+vallbull@users.noreply.github.com>
2024-01-25 11:24:24 +0300

    Add a more understandable error in the absence of a token (#259)
    Co-authored-by: Valeria Bulanova <bulanovavv@yandex-team.ru>

--- lib/dl_file_uploader_api_lib/dl_file_uploader_api_lib/schemas/files.py
+++ lib/dl_file_uploader_api_lib/dl_file_uploader_api_lib/schemas/files.py
@@ -38,7 +38,9 @@ def validate_yadocs_data(data: dict) -> None:
     if not ((data["public_link"] is None) ^ (data["private_path"] is None)):
         raise ValueError("Expected exactly one of [`private_path`, `public_link`] to be specified")
     if data["public_link"] is None and data["oauth_token"] is None and data["connection_id"] is None:
-        raise ma.ValidationError("Expected `oauth_token` or `connection_id` to be specified")
+        raise ma.ValidationError(
+            "Authentication is required to work with private files (expected `oauth_token` or `connection_id` to be specified)"
+        )
 
 
 class FileLinkRequestSchema(BaseRequestSchema):
