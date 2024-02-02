a9e22b232c180dc46a29eb46b43bb46db7a363c1
Grigorii Statsenko <altvod@users.noreply.github.com>
2024-02-02 09:59:58 +0100

    Minor additions to dashql primitives (#274)

--- lib/dl_dashsql/dl_dashsql/formatting/shortcuts.py
+++ lib/dl_dashsql/dl_dashsql/formatting/shortcuts.py
@@ -2,6 +2,18 @@ from typing import Sequence
 
 from dl_dashsql.formatting.base import QueryIncomingParameter
 from dl_dashsql.formatting.values import DefaultValueQueryFormatterFactory
+from dl_dashsql.typed_query.primitives import TypedQueryParameter
+
+
+def params_for_formatter(params: Sequence[TypedQueryParameter]) -> Sequence[QueryIncomingParameter]:
+    return [
+        QueryIncomingParameter(
+            original_name=param.name,
+            user_type=param.user_type,
+            value=param.value,
+        )
+        for param in params
+    ]
 
 
 def format_query_for_debug(query: str, incoming_parameters: Sequence[QueryIncomingParameter]) -> str:
