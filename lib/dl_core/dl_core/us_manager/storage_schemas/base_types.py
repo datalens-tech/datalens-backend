4c8383c8f95d7eac28fb57afb247b0f45e748ffe
Grigorii Statsenko <gstatsenko@nebius.com>
2024-02-05 19:29:00 +0100

    More fixes for native types

--- lib/dl_core/dl_core/us_manager/storage_schemas/base_types.py
+++ lib/dl_core/dl_core/us_manager/storage_schemas/base_types.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import Any
 
 from marshmallow import (
+    EXCLUDE,
     Schema,
     fields,
     post_load,
@@ -16,11 +17,16 @@ from dl_core.db.native_type import GenericNativeType
 class NativeTypeSchema(Schema):
     """(currently used for uploads / ProviderConnection)"""
 
+    class Meta:
+        # Same as in `NativeTypeSchemaBase`, to ignore `conn_type`
+        unknown = EXCLUDE
+
     name = fields.String()
 
     @post_load(pass_many=False)
     def to_object(self, data: dict[str, Any], **kwargs: Any) -> Any:
         if "conn_type" in data:
+            # TODO: Remove once all datasets have been migrated
             data = data.copy()
             data.pop("conn_type")
         return GenericNativeType(**data)
