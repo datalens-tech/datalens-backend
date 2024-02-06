e13ff23ca631f720981d7cd02c7c5b565b7c8931
KonstantAnxiety <58992437+KonstantAnxiety@users.noreply.github.com>
2024-02-06 19:15:48 +0300

    Make removal of unbound expressions from GROUP BY optional based on dialect (#290)

--- lib/dl_connector_ydb/dl_connector_ydb/formula/connector.py
+++ lib/dl_connector_ydb/dl_connector_ydb/formula/connector.py
@@ -1,6 +1,7 @@
 from ydb.sqlalchemy import YqlDialect as SAYqlDialect
 
 from dl_formula.connectors.base.connector import FormulaConnector
+from dl_query_processing.compilation.query_mutator import RemoveConstFromGroupByFormulaAtomicQueryMutator
 
 from dl_connector_ydb.formula.constants import YqlDialect as YqlDialectNS
 from dl_connector_ydb.formula.definitions.all import DEFINITIONS
@@ -11,3 +12,7 @@ class YQLFormulaConnector(FormulaConnector):
     dialects = YqlDialectNS.YQL
     op_definitions = DEFINITIONS
     sa_dialect = SAYqlDialect()
+
+    @classmethod
+    def registration_hook(cls) -> None:
+        RemoveConstFromGroupByFormulaAtomicQueryMutator.register_dialect(cls.dialects)
