dece47dcab2c2acb351a7619f9acef3540d3b538
Nikolay Proskurin <n1ckpr0s@yandex.ru>
2024-02-08 14:41:42 +0100

    fix mypy in dl_formula.definitions.common

--- lib/dl_formula/dl_formula/definitions/common.py
+++ lib/dl_formula/dl_formula/definitions/common.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import (
+    TYPE_CHECKING,
+    Callable,
+    Optional,
+    TypeVar,
+)
 
 import sqlalchemy as sa
 from sqlalchemy.sql.elements import (
+    Case,
     UnaryExpression,
     WithinGroup,
 )
@@ -17,23 +23,33 @@ from dl_formula.shortcuts import n
 
 
 if TYPE_CHECKING:
-    from sqlalchemy.sql.elements import ClauseElement
+    from sqlalchemy.sql.elements import (
+        ClauseElement,
+        ColumnElement,
+    )
 
 
 class _TextClauseHack(sa.sql.elements.TextClause):
-    def __bool__(self):  # type: ignore  # 2024-01-24 # TODO: Function is missing a return type annotation  [no-untyped-def]
+    def __bool__(self) -> bool:  # type: ignore  # TODO: bug in sqlalchemy stubs? Return type "bool" of "__bool__" incompatible with return type "None" in supertype "ClauseElement"  [override]
         # Possibly a bug in sqlalchemy 1.4: query caching fails as, normally,
         # 'Boolean value of this clause is not defined'.
         # Not that the cache should be used here anyway, most likely.
         return True
 
 
-def raw_sql(sql_text):  # type: ignore  # 2024-01-24 # TODO: Function is missing a type annotation  [no-untyped-def]
+def raw_sql(sql_text: str) -> _TextClauseHack:
     """Basically just `sa.text(sql_text)` with hacks"""
     return _TextClauseHack(sql_text)
 
 
-def make_binary_chain(binary_callable, *args, wrap_as_nodes: bool = True):  # type: ignore  # 2024-01-24 # TODO: Function is missing a return type annotation  [no-untyped-def]
+_BINARY_CHAIN_TV = TypeVar("_BINARY_CHAIN_TV")
+
+
+def make_binary_chain(
+    binary_callable: Callable[[_BINARY_CHAIN_TV, _BINARY_CHAIN_TV], _BINARY_CHAIN_TV],
+    *args: _BINARY_CHAIN_TV,
+    wrap_as_nodes: bool = True,
+) -> Optional[_BINARY_CHAIN_TV]:
     if len(args) == 0:
         return None
 
@@ -52,7 +68,7 @@ def make_binary_chain(binary_callable, *args, wrap_as_nodes: bool = True):  # ty
     return expr
 
 
-def quantile_value(quant: float):  # type: ignore  # 2024-01-24 # TODO: Function is missing a return type annotation  [no-untyped-def]
+def quantile_value(quant: float) -> float:
     """
     Validate and normalize the value for a quantile.
     """
@@ -76,7 +92,7 @@ def desc(value: ClauseElement) -> ClauseElement:
     return value
 
 
-def ifnotnull(value, expr):  # type: ignore  # 2024-01-24 # TODO: Function is missing a type annotation  [no-untyped-def]
+def ifnotnull(value: ColumnElement, expr: ClauseElement) -> Case:
     """Wrap `expr` in an `sa.case` that results in a NULL for a NULL `value`"""
     return sa.case([(value.is_(None), sa.null())], else_=expr)
 
@@ -86,5 +102,5 @@ class _PatchedWithinGroup(WithinGroup):
         return self.__class__, (self.element, *self.order_by)
 
 
-def within_group(clause_el: ClauseElement, *order_by) -> _PatchedWithinGroup:  # type: ignore  # 2024-01-24 # TODO: Function is missing a type annotation for one or more arguments  [no-untyped-def]
+def within_group(clause_el: ClauseElement, *order_by: ClauseElement) -> _PatchedWithinGroup:
     return _PatchedWithinGroup(clause_el, *order_by)
