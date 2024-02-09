1ebc641857bdd9be1dae4e28e3450d23ff5fd6d7
Grigorii Statsenko <altvod@users.noreply.github.com>
2024-02-09 09:53:18 +0100

    Added logging of CHYT data query response headers (#295)

--- lib/dl_connector_chyt/dl_connector_chyt/core/async_adapters.py
+++ lib/dl_connector_chyt/dl_connector_chyt/core/async_adapters.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 import logging
 
+from aiohttp import ClientResponse
 import attr
 from clickhouse_sqlalchemy.drivers.http.transport import _get_type  # noqa
 
+from dl_core.connection_executors.models.db_adapter_data import DBAdapterQuery
 from dl_core.utils import get_current_w3c_tracing_headers
 
 from dl_connector_chyt.core.constants import CONNECTION_TYPE_CHYT
@@ -28,3 +30,8 @@ class AsyncCHYTAdapter(BaseAsyncClickHouseAdapter):
             override_sampled=self.ch_utils.get_tracing_sample_flag_override(self._req_ctx_info),
             req_id=self._req_ctx_info.request_id,
         )
+
+    async def _make_query(self, dba_q: DBAdapterQuery, mirroring_mode: bool = False) -> ClientResponse:
+        resp = await super()._make_query(dba_q=dba_q, mirroring_mode=mirroring_mode)
+        LOGGER.info(f"CHYT Response headers: {resp.headers}")
+        return resp
