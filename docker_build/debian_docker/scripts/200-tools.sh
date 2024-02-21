52db0e951ff8dc3ad31751f3a5dd5afdd3d39fea
KonstantAnxiety <konstasa@yandex-team.ru>
2024-02-21 17:12:29 +0300

    BI-5255 Install gh cli in debian_docker image

---
+++ docker_build/debian_docker/scripts/200-tools.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eu
+export DEBIAN_FRONTEND=noninteractive
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+apt update
+apt install --yes \
+  gh
