602c045dbd948e96daa99a46d968c4569e841863
KonstantAnxiety <konstasa@yandex-team.ru>
2024-02-21 20:28:05 +0300

    Specify correct data source spec in GP dsrc migrator (inherit GP dsrc migrator from PG)

--- lib/dl_connector_greenplum/dl_connector_greenplum/core/data_source_migration.py
+++ lib/dl_connector_greenplum/dl_connector_greenplum/core/data_source_migration.py
@@ -4,9 +4,9 @@ from dl_connector_greenplum.core.constants import (
     SOURCE_TYPE_GP_SUBSELECT,
     SOURCE_TYPE_GP_TABLE,
 )
+from dl_connector_postgresql.core.postgresql.data_source_migration import PostgreSQLDataSourceMigrator
 
 
-class GreenPlumDataSourceMigrator(DefaultSQLDataSourceMigrator):
+class GreenPlumDataSourceMigrator(PostgreSQLDataSourceMigrator):
     table_source_type = SOURCE_TYPE_GP_TABLE
     subselect_source_type = SOURCE_TYPE_GP_SUBSELECT
-    default_schema_name = "public"
