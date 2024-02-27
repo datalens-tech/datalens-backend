55d38059552ba5b67e76fac63071354ba5defe68
vallbull <33630435+vallbull@users.noreply.github.com>
2024-02-27 05:41:45 +0300

    Move clickhouse test suites to separate package (#339)
    * Move clickhouse test suites to separate package
    
    * Fix codestyle
    
    * Fix mypy

--- lib/dl_connector_clickhouse/dl_connector_clickhouse_tests/db/formula/test_functions_datetime.py
+++ lib/dl_connector_clickhouse/dl_connector_clickhouse_tests/db/formula/test_functions_datetime.py
@@ -1,17 +1,8 @@
-from dl_formula_testing.testcases.functions_datetime import DefaultDateTimeFunctionFormulaConnectorTestSuite
-
+from dl_connector_clickhouse.formula.testing.test_suites import DateTimeFunctionClickHouseTestSuite
 from dl_connector_clickhouse_tests.db.formula.base import (
     ClickHouse_21_8TestBase,
     ClickHouse_22_10TestBase,
 )
-
-
-class DateTimeFunctionClickHouseTestSuite(DefaultDateTimeFunctionFormulaConnectorTestSuite):
-    supports_dateadd_non_const_unit_num = True
-    supports_deprecated_dateadd = True
-    supports_deprecated_datepart_2 = True
-    supports_datetrunc_3 = True
-    supports_datetimetz = True
 
 
 class TestDateTimeFunctionClickHouse_21_8(ClickHouse_21_8TestBase, DateTimeFunctionClickHouseTestSuite):
