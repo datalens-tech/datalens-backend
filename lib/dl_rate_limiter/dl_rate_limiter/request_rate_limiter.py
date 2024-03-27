60efecf99e03efcc8c60d84df5de54bc5e5bdaca
Ovsyannikov Dmitrii <ovsds@yandex-team.ru>
2024-03-27 16:43:20 +0100

    feat: add dl_rate_limiter (#402)

---
+++ lib/dl_rate_limiter/dl_rate_limiter/request_rate_limiter.py
@@ -0,0 +1,124 @@
+import logging
+import re
+import typing
+
+import attr
+
+import dl_rate_limiter.event_rate_limiter as event_rate_limiter
+
+
+logger = logging.getLogger(__name__)
+
+
+class TemplateError(Exception):
+    pass
+
+
+@attr.s(auto_attribs=True)
+class Request:
+    url: str
+    method: str
+    headers: typing.Mapping[str, str] = attr.ib(factory=dict)
+
+
+@attr.s(auto_attribs=True)
+class RequestEventKeyTemplate:
+    key: str
+    headers: frozenset[str]
+
+    def generate_key(self, request: Request) -> str:
+        try:
+            headers_values = ":".join(request.headers[header] for header in self.headers)
+        except KeyError as exc:
+            raise TemplateError("Header not found in request headers") from exc
+
+        return f"{self.key}:{headers_values}"
+
+
+@attr.s(auto_attribs=True)
+class RequestPattern:
+    url_regex: re.Pattern
+    methods: frozenset[str]
+    event_key_template: RequestEventKeyTemplate
+    limit: int
+    window_ms: int
+
+    def matches(self, request: Request) -> bool:
+        return self.url_regex.match(request.url) is not None and request.method in self.methods
+
+
+class SyncRequestRateLimiterProtocol(typing.Protocol):
+    def check_limit(self, request: Request) -> bool:
+        ...
+
+
+class AsyncRequestRateLimiterProtocol(typing.Protocol):
+    async def check_limit(self, request: Request) -> bool:
+        ...
+
+
+@attr.s(auto_attribs=True)
+class SyncRequestRateLimiter:
+    _event_limiter: event_rate_limiter.SyncEventRateLimiterProtocol
+    _patterns: list[RequestPattern] = attr.ib(factory=list)
+
+    def check_limit(self, request: Request) -> bool:
+        for pattern in self._patterns:
+            if not pattern.matches(request):
+                continue
+
+            try:
+                event_key = pattern.event_key_template.generate_key(request)
+            except TemplateError:
+                logger.exception(f"Template error for request {request}")
+                continue
+
+            result = self._event_limiter.check_limit(
+                event_key=event_key,
+                limit=pattern.limit,
+                window_ms=pattern.window_ms,
+            )
+
+            if result is False:
+                return False
+
+        return True
+
+
+@attr.s(auto_attribs=True)
+class AsyncRequestRateLimiter:
+    _event_limiter: event_rate_limiter.AsyncEventRateLimiterProtocol
+    _patterns: list[RequestPattern] = attr.ib(factory=list)
+
+    async def check_limit(self, request: Request) -> bool:
+        for pattern in self._patterns:
+            if not pattern.matches(request):
+                continue
+
+            try:
+                event_key = pattern.event_key_template.generate_key(request)
+            except TemplateError:
+                logger.exception(f"Template error for request {request}")
+                continue
+
+            result = await self._event_limiter.check_limit(
+                event_key=event_key,
+                limit=pattern.limit,
+                window_ms=pattern.window_ms,
+            )
+
+            if result is False:
+                return False
+
+        return True
+
+
+__all__ = [
+    "AsyncRequestRateLimiter",
+    "SyncRequestRateLimiter",
+    "RequestEventKeyTemplate",
+    "RequestPattern",
+    "Request",
+    "AsyncRequestRateLimiterProtocol",
+    "SyncRequestRateLimiterProtocol",
+]
---
+++ lib/dl_rate_limiter/dl_rate_limiter_tests/db/middlewares/test_flask.py
@@ -0,0 +1,58 @@
+import flask
+import flask.testing
+import pytest
+
+import dl_rate_limiter
+
+
+@pytest.fixture(name="app")
+def fixture_app(sync_request_limiter: dl_rate_limiter.SyncRequestRateLimiter) -> flask.Flask:
+    # create and configure the app
+    app = flask.Flask(__name__)
+
+    # return 200 for all requests
+    @app.route("/<path:path>")
+    def wildcard(path: str):
+        return "OK"
+
+    app.config.update({"TESTING": True})
+    middleware = dl_rate_limiter.FlaskMiddleware(rate_limiter=sync_request_limiter)
+    app.before_request(middleware.process)
+
+    yield app
+
+
+@pytest.fixture(name="client")
+def fixture_client(app: flask.Flask) -> flask.testing.FlaskClient:
+    return app.test_client()
+
+
+@pytest.fixture(name="runner")
+def fixture_runner(app: flask.Flask) -> flask.testing.FlaskCliRunner:
+    return app.test_cli_runner()
+
+
+def test_hello(client: flask.testing.FlaskClient):
+    response = client.get("/hello")
+    assert response.data == b"OK"
+
+
+def test_unlimited(client: flask.testing.FlaskClient):
+    responses = [client.get("/unlimited/1", headers={"X-Test-Header": "test"}) for _ in range(10)]
+    assert all(response.status_code == 200 for response in responses)
+
+
+def test_limited(client: flask.testing.FlaskClient):
+    responses = [client.get("/limited/1", headers={"X-Test-Header": "test"}) for _ in range(20)]
+    assert sum(response.status_code == 429 for response in responses) == 15
+    assert sum(response.status_code == 200 for response in responses) == 5
+
+
+def test_limited_multiple_limits(client: flask.testing.FlaskClient):
+    responses = [client.get("/limited/more_specifically/1", headers={"X-Test-Header": "test"}) for _ in range(10)]
+    assert sum(response.status_code == 429 for response in responses) == 9
+    assert sum(response.status_code == 200 for response in responses) == 1
+
+    responses = [client.get("/limited/1", headers={"X-Test-Header": "test"}) for _ in range(20)]
+    assert sum(response.status_code == 429 for response in responses) == 16
+    assert sum(response.status_code == 200 for response in responses) == 4
