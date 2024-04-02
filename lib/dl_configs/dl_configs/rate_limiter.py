c8539042e0fd2df46ee7348923cd998cd49990cd
ovsds <ovsds@yandex-team.ru>
2024-04-02 14:01:05 +0200

    fix: mypy

--- lib/dl_configs/dl_configs/rate_limiter.py
+++ lib/dl_configs/dl_configs/rate_limiter.py
@@ -15,13 +15,13 @@ from dl_configs.utils import (
 
 @attr.s(frozen=True)
 class RequestEventKeyTemplate(SettingsBase):
-    key: str = s_attrib("KEY")
-    headers: tuple[str, ...] = s_attrib("HEADERS", missing_factory=tuple)
+    key: str = s_attrib("KEY")  # type: ignore
+    headers: tuple[str, ...] = s_attrib("HEADERS", missing_factory=tuple)  # type: ignore
 
     @classmethod
     def from_json(cls, json_value: typing.Any) -> typing_extensions.Self:
         assert isinstance(json_value, typing.Mapping)
-        return cls(
+        return cls(  # type: ignore
             key=json_value["KEY"],
             headers=tuple(json_value["HEADERS"]),
         )
@@ -29,16 +29,16 @@ class RequestEventKeyTemplate(SettingsBase):
 
 @attr.s(frozen=True)
 class RateLimiterPattern(SettingsBase):
-    url_regex: str = s_attrib("URL_REGEX")
-    methods: tuple[str, ...] = s_attrib("METHODS", missing_factory=tuple)
-    event_key_template: RequestEventKeyTemplate = s_attrib("EVENT_KEY_TEMPLATE")
-    limit: int = s_attrib("LIMIT")
-    window_ms: int = s_attrib("WINDOW_MS")
+    url_regex: str = s_attrib("URL_REGEX")  # type: ignore
+    methods: tuple[str, ...] = s_attrib("METHODS", missing_factory=tuple)  # type: ignore
+    event_key_template: RequestEventKeyTemplate = s_attrib("EVENT_KEY_TEMPLATE")  # type: ignore
+    limit: int = s_attrib("LIMIT")  # type: ignore
+    window_ms: int = s_attrib("WINDOW_MS")  # type: ignore
 
     @classmethod
     def from_json(cls, json_value: typing.Any) -> typing_extensions.Self:
         assert isinstance(json_value, typing.Mapping)
-        return cls(
+        return cls(  # type: ignore
             url_regex=json_value["URL_REGEX"],
             methods=tuple(json_value["METHODS"]),
             event_key_template=RequestEventKeyTemplate.from_json(json_value["EVENT_KEY_TEMPLATE"]),
@@ -49,7 +49,7 @@ class RateLimiterPattern(SettingsBase):
 
 @attr.s(frozen=True)
 class RateLimiterConfig(SettingsBase):
-    PATTERNS: tuple[RateLimiterPattern, ...] = s_attrib(
+    PATTERNS: tuple[RateLimiterPattern, ...] = s_attrib(  # type: ignore
         "PATTERNS",
         env_var_converter=tuple_of_models_env_converter_factory(RateLimiterPattern),
         json_converter=tuple_of_models_json_converter_factory(RateLimiterPattern),
@@ -62,6 +62,6 @@ class RateLimiterConfig(SettingsBase):
             return None
 
         assert isinstance(json_value, typing.Mapping)
-        return cls(
+        return cls(  # type: ignore
             PATTERNS=(RateLimiterPattern.from_json(entry) for entry in json_value["PATTERNS"]),
         )
