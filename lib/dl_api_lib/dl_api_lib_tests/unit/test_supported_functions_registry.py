379f52fc7a0ce206b50139884f07bbac1380f702
Nick Proskurin <mcpn@yandex-team.ru>
2024-05-08 21:20:25 +0200

    cq

--- lib/dl_api_lib/dl_api_lib_tests/unit/test_supported_functions_registry.py
+++ lib/dl_api_lib/dl_api_lib_tests/unit/test_supported_functions_registry.py
@@ -17,6 +17,7 @@ from dl_formula.definitions.scope import Scope
 from dl_formula.definitions.type_strategy import FromArgs
 from dl_formula.mutation.registry import get_mutation_lookup_functions_names
 
+
 V = TranslationVariant.make
 
 
