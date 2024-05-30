d37ea00a65d8a23627230ecc216636061c16af6d
Dmitrii Ovsyannikov <ovsds@yandex-team.ru>
2024-05-30 18:26:16 +0200

    build: replace mirrored images from docker hub (#467)

--- docker_build/target_base_ci/scripts/700-venv.sh
+++ docker_build/target_base_ci/scripts/700-venv.sh
@@ -6,7 +6,8 @@ mkdir /venv
 python -m venv /venv
 source /venv/bin/activate
 
-pip install --no-input poetry==1.7.1
+# TODO FIX: pyproject-hooks fix version: https://github.com/python-poetry/poetry/issues/9351
+pip install --no-input poetry==1.7.1 pyproject-hooks==1.0.0
 
 cd /src/metapkg
 poetry export --without=dev --without=ci --without-hashes --format=requirements.txt > requirements.txt
