12c84bffe84ba325234cc5445c50b33f9b2032d7
br0vk1n-ia <br0vk1n-ia@yandex-team.ru>
2024-06-07 12:31:36 +0300

    BI-5166: fix other db tests

--- lib/dl_connector_clickhouse/dl_connector_clickhouse_tests/db/formula/test_functions_math.py
+++ lib/dl_connector_clickhouse/dl_connector_clickhouse_tests/db/formula/test_functions_math.py
@@ -1,6 +1,10 @@
 from dl_connector_clickhouse.formula.testing.test_suites import MathFunctionClickHouseTestSuite
 from dl_connector_clickhouse_tests.db.formula.base import ClickHouse_21_8TestBase
-
+from dl_formula_testing.evaluator import DbEvaluator
 
 class TestMathFunctionClickHouse_21_8(ClickHouse_21_8TestBase, MathFunctionClickHouseTestSuite):
-    pass
+    def test_rounding_to_0(self, dbe: DbEvaluator) -> None:
+        assert str(dbe.eval("CEILING(-0.1)")) == "0.0"
+        assert str(dbe.eval("ROUND(-0.1)")) == "0.0"
+        assert str(dbe.eval("ROUND(-0.1, 0)")) == "0.0"
+        print('penis')
