48fcd2326e7d1b5b322b79d8d08c234b044c3ea1
br0vk1n-ia <br0vk1n-ia@yandex-team.ru>
2024-06-07 12:55:44 +0300

    BI-5166: isort

--- lib/dl_connector_clickhouse/dl_connector_clickhouse_tests/db/formula/test_functions_math.py
+++ lib/dl_connector_clickhouse/dl_connector_clickhouse_tests/db/formula/test_functions_math.py
@@ -1,6 +1,8 @@
+from dl_formula_testing.evaluator import DbEvaluator
+
 from dl_connector_clickhouse.formula.testing.test_suites import MathFunctionClickHouseTestSuite
 from dl_connector_clickhouse_tests.db.formula.base import ClickHouse_21_8TestBase
-from dl_formula_testing.evaluator import DbEvaluator
+
 
 class TestMathFunctionClickHouse_21_8(ClickHouse_21_8TestBase, MathFunctionClickHouseTestSuite):
     def test_rounding_to_0(self, dbe: DbEvaluator) -> None:
