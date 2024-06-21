e453c7251685d1935ef353aae12d44e3b7ae7144
Valeria Bulanova <bulanovavv@yandex-team.ru>
2024-06-21 18:39:45 +0300

    Fix

--- lib/dl_connector_ydb/dl_connector_ydb/core/ydb/target_dto.py
+++ lib/dl_connector_ydb/dl_connector_ydb/core/ydb/target_dto.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 
 import attr
 
-from dl_connector_ydb.core.ydb.constants import YDBAuthTypeMode
 from dl_core.connection_executors.models.connection_target_dto_base import BaseSQLConnTargetDTO
+
+from dl_connector_ydb.core.ydb.constants import YDBAuthTypeMode
 
 
 @attr.s(frozen=True)
