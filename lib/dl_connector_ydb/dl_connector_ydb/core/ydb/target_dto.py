43dd289af52791cca067a0477aefc8e5a938b5c9
Valeria Bulanova <bulanovavv@yandex-team.ru>
2024-06-24 17:46:24 +0300

    Fix

--- lib/dl_connector_ydb/dl_connector_ydb/core/ydb/target_dto.py
+++ lib/dl_connector_ydb/dl_connector_ydb/core/ydb/target_dto.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
-from typing import (
-    TYPE_CHECKING,
-    Type,
-    TypeVar,
-)
+from typing import TYPE_CHECKING
 
 import attr
+import typing_extensions
 
 from dl_core.connection_executors.models.connection_target_dto_base import BaseSQLConnTargetDTO
 
@@ -15,8 +12,6 @@ from dl_connector_ydb.core.ydb.constants import YDBAuthTypeMode
 
 if TYPE_CHECKING:
     from dl_constants.types import TJSONLike
-
-_CT_DTO_TV = TypeVar("_CT_DTO_TV", bound="YDBConnTargetDTO")
 
 
 @attr.s(frozen=True)
@@ -30,9 +25,9 @@ class YDBConnTargetDTO(BaseSQLConnTargetDTO):
         }
 
     @classmethod
-    def _from_jsonable_dict(cls: Type[_CT_DTO_TV], data: dict) -> _CT_DTO_TV:
+    def _from_jsonable_dict(cls, data: dict) -> typing_extensions.Self:
         return cls(
-            **{  # type: ignore  # TODO: fix
+            **{
                 **data,
                 "auth_type": YDBAuthTypeMode[data["auth_type"]],
             }
