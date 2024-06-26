aed73ceb80a9028d4d7cb3a0706d0453adc0af80
br0vk1n-ia <br0vk1n-ia@yandex-team.ru>
2024-06-26 09:36:41 +0300

    BI-5376: Add db_name validation (#490)
    * BI-5376: Add db_name validation
    
    * BI-5376: format, lint
    
    * BI-5376: make tests required for connections that use classic schema
    
    * FIX: review changes
    
    * FIX: ruff

--- lib/dl_api_lib_testing/dl_api_lib_testing/connector/connection_suite.py
+++ lib/dl_api_lib_testing/dl_api_lib_testing/connector/connection_suite.py
@@ -1,5 +1,8 @@
 import json
 from typing import Optional
+import uuid
+
+import pytest
 
 from dl_api_client.dsmaker.api.http_sync_base import SyncHttpClientBase
 from dl_api_lib_testing.connection_base import ConnectionTestBase
@@ -79,3 +82,34 @@ class DefaultConnectorConnectionTestSuite(ConnectionTestBase, RegulatedTestCase)
         assert isinstance(resp.json["options"]["allow_typed_query_usage"], bool)
         assert len(resp.json["options"]["query_types"]) > 0
         assert any([qt["query_type"] == "generic_query" for qt in resp.json["options"]["query_types"]])
+
+    def test_create_connections__query_params_in_db_name__error(
+        self,
+        control_api_sync_client: SyncHttpClientBase,
+        saved_connection_id: str,
+        bi_headers: Optional[dict[str, str]],
+        connection_params: dict,
+    ) -> None:
+        if "db_name" not in connection_params:
+            pytest.skip("No db_name field in schema")
+
+        # arrange
+        connection_params = connection_params.copy()
+        connection_params["db_name"] = "db1?sslmode=required"
+        data = dict(
+            name=f"{self.conn_type.name} conn {uuid.uuid4()}",
+            type=self.conn_type.name,
+            **connection_params,
+        )
+
+        # act
+        resp = control_api_sync_client.post(
+            "/api/v1/connections",
+            content_type="application/json",
+            data=json.dumps(data),
+            headers=bi_headers,
+        )
+
+        # assert
+        assert resp.status_code == 400
+        assert resp.json == {"db_name": ["There must be no query params in field db_name, found: db1?sslmode=required"]}
