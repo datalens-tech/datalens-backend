490a995e90af9475c88e799e51c981e5e2600706
KonstantAnxiety <58992437+KonstantAnxiety@users.noreply.github.com>
2024-06-26 19:18:24 +0300

    BI-5237 Add Content-Type header to all CH sqlalchemy requests; add conn sources test (#512)
    * BI-5237 Add Content-Type header to all CH sqlalchemy requests; add conn sources test
    
    * xfail greenplum get sources

--- lib/dl_connector_greenplum/dl_connector_greenplum_tests/db/api/test_connection.py
+++ lib/dl_connector_greenplum/dl_connector_greenplum_tests/db/api/test_connection.py
@@ -2,11 +2,21 @@ from typing import Optional
 
 from dl_api_client.dsmaker.api.http_sync_base import SyncHttpClientBase
 from dl_api_lib_testing.connector.connection_suite import DefaultConnectorConnectionTestSuite
+from dl_testing.regulated_test import RegulatedTestParams
 
 from dl_connector_greenplum_tests.db.api.base import GreenplumConnectionTestBase
 
 
 class TestGreenplumSQLConnection(GreenplumConnectionTestBase, DefaultConnectorConnectionTestSuite):
+    test_params = RegulatedTestParams(
+        mark_tests_failed={
+            DefaultConnectorConnectionTestSuite.test_connection_sources: (
+                "Fails because we use PG instead of GP: "
+                'psycopg2.errors.UndefinedTable: relation "pg_partitions" does not exist'
+            ),
+        },
+    )
+
     def test_create_connection(
         self,
         control_api_sync_client: SyncHttpClientBase,
