5c7f7fc76d9db2c7650c04cfb1147df6ab230c64
Nick Proskurin <mcpn@yandex-team.ru>
2024-07-02 13:34:15 +0200

    utils tests

---
+++ lib/dl_rls/dl_rls_tests/unit/test_utils.py
@@ -0,0 +1,17 @@
+import pytest
+
+from dl_rls.utils import (
+    quote_by_quote,
+    split_by_quoted_quote,
+)
+
+
+TEST_DATA = ["'abc'de", "'ab''c'''de", "'ab''c'''"]
+EXPECTED = [('abc', 'de'), ("ab'c'", 'de'), ("ab'c'", '')]
+
+
+@pytest.mark.parametrize(["string", "expected"], zip(TEST_DATA, EXPECTED))
+def test_quoting(string, expected):
+    split = split_by_quoted_quote(string)
+    assert split == expected
+    assert quote_by_quote(split[0]) + split[1] == string
