b42144879c0d112aaa58b860a75223e8eda0b00f
br0vk1n-ia <br0vk1n-ia@yandex-team.ru>
2024-07-03 14:13:13 +0300

    BI-5526: again testing 1.1.0 pyptoject-hooks

--- docker_build/target_base_ci/scripts/700-venv.sh
+++ docker_build/target_base_ci/scripts/700-venv.sh
@@ -7,7 +7,7 @@ python -m venv /venv
 source /venv/bin/activate
 
 # TODO FIX: pyproject-hooks fix version: https://github.com/python-poetry/poetry/issues/9351
-pip install --no-input poetry==1.7.1 pyproject-hooks==1.0.0
+pip install --no-input poetry==1.7.1 pyproject-hooks==1.1.0
 
 cd /src/metapkg
 poetry export --without=dev --without=ci --without-hashes --format=requirements.txt > requirements.txt
