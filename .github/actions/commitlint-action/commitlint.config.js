5be8187ad897bb38cade28db65c4972735eca353
ovsds <ovsds@yandex-team.ru>
2024-07-03 17:45:07 +0200

    ci: composite action

--- .github/actions/commitlint-action/commitlint.config.js
+++ .github/actions/commitlint-action/commitlint.config.js
@@ -1,5 +1,45 @@
+import {
+	RuleConfigSeverity,
+} from '@commitlint/types';
+
 const Configuration = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+			RuleConfigSeverity.Error,
+			'always',
+			[
+				'breaking-change',
+				'new-feature',
+				'bug-fix',
+				'sec',
+				'deprecation',
+				'dev',
+				'tests',
+				'ci',
+				'chore',
+				'build',
+				'docs',
+			],
+		],
+		'scope-enum': [
+			RuleConfigSeverity.Error,
+			'always',
+			[
+				'charts',
+				'connectors',
+				'navigation',
+				'general',
+				'dashboards',
+				'datasets',
+				'auth',
+				'optimization',
+				'role-model',
+				'docs',
+				'formula',
+			],
+		],
+    },
 }
 
 export default Configuration;
