8158c8b7857e716afed257d3727da16d8e939e56
ovsds <ovsds@yandex-team.ru>
2024-07-04 11:39:22 +0200

    ci: BI-5650 composite action

--- .github/actions/commitlint-action/src/commitlint.config.js
+++ .github/actions/commitlint-action/src/commitlint.config.js
@@ -2,6 +2,9 @@
 
 import { RuleConfigSeverity } from "@commitlint/types";
 
+const CONFIG_SOURCE_URL =
+  "https://raw.githubusercontent.com/datalens-tech/datalens/main/.github/workflows/scripts/changelog/changelog_config.json";
+
 const DEFAULT_TYPE_ENUM = [
   "breaking-change",
   "new-feature",
@@ -30,6 +33,30 @@ const DEFAULT_SCOPE_ENUM = [
   "formula",
 ];
 
+const getChangelogConfig = async () => {
+  const response = await fetch(CONFIG_SOURCE_URL);
+  const data = response.json();
+
+  console.log(`INFO::Received data from config file: ${data}`);
+
+  const typeEnum = data.section_tags.tags.map((tag) => tag.id);
+  const scopeEnum = data.component_tags.tags.map((tag) => tag.id);
+
+  return { typeEnum, scopeEnum };
+};
+
+const getEnums = async () => {
+  try {
+    const { typeEnum, scopeEnum } = await getChangelogConfig();
+    return { typeEnum, scopeEnum };
+  } catch (error) {
+    console.error(`ERROR::Failed to fetch config file: ${error}`);
+    return { typeEnum: DEFAULT_TYPE_ENUM, scopeEnum: DEFAULT_SCOPE_ENUM };
+  }
+};
+
+const { TYPE_ENUM, SCOPE_ENUM } = await getEnums();
+
 const subjectPrefixRule = ({ subject }) => {
   const PREFIX_REGEX = /\W*-\d*/;
   return [subject.match(PREFIX_REGEX), `Subject must start with a prefix like "BI-1234"`];
@@ -38,8 +65,8 @@ const subjectPrefixRule = ({ subject }) => {
 const Configuration = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [RuleConfigSeverity.Error, "always", DEFAULT_TYPE_ENUM],
-    "scope-enum": [RuleConfigSeverity.Error, "always", DEFAULT_SCOPE_ENUM],
+    "type-enum": [RuleConfigSeverity.Error, "always", TYPE_ENUM],
+    "scope-enum": [RuleConfigSeverity.Error, "always", SCOPE_ENUM],
     "subject-case": [RuleConfigSeverity.Error, "never", ["start-case", "pascal-case", "upper-case"]],
     "subject-prefix": [RuleConfigSeverity.Error, "always"],
   },
