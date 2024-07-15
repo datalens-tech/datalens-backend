4ffb166dd9d12377c0370555f52e374551a5a5b0
Nick Proskurin <mcpn@yandex-team.ru>
2024-07-15 21:46:06 +0200

    refactor(connectors): BI-5695 remove legacy connector settings

--- lib/dl_connector_chyt/dl_connector_chyt/core/settings.py
+++ lib/dl_connector_chyt/dl_connector_chyt/core/settings.py
@@ -1,15 +1,7 @@
-from typing import (
-    ClassVar,
-    Optional,
-)
-
 import attr
 
-from dl_configs.connectors_data import ConnectorsDataBase
-from dl_configs.connectors_settings import (
-    ConnectorsConfigType,
-    ConnectorSettingsBase,
-)
+from dl_configs.connectors_settings import ConnectorSettingsBase
+from dl_configs.settings_loaders.fallback_cfg_resolver import ObjectLikeConfig
 from dl_configs.settings_loaders.meta_definition import s_attrib
 from dl_configs.utils import split_by_comma
 from dl_core.connectors.settings.primitives import (
@@ -26,27 +18,13 @@ class CHYTConnectorSettings(ConnectorSettingsBase):
     DEFAULT_CLIQUE:     clique that is set by default in the connection form
     """
 
-    PUBLIC_CLIQUES: tuple[str] = s_attrib("PUBLIC_CLIQUES", missing_factory=tuple, env_var_converter=split_by_comma)  # type: ignore  # 2024-01-30 # TODO: Incompatible types in assignment (expression has type "Attribute[Any]", variable has type "tuple[str]")  [assignment]
-    FORBIDDEN_CLIQUES: tuple[str] = s_attrib("FORBIDDEN_CLIQUES", missing_factory=tuple, env_var_converter=split_by_comma)  # type: ignore  # 2024-01-30 # TODO: Incompatible types in assignment (expression has type "Attribute[Any]", variable has type "tuple[str]")  [assignment]
-    DEFAULT_CLIQUE: Optional[str] = s_attrib("DEFAULT_CLIQUE", missing=None)  # type: ignore  # 2024-01-30 # TODO: Incompatible types in assignment (expression has type "Attribute[Any]", variable has type "str | None")  [assignment]
+    PUBLIC_CLIQUES: tuple[str] = s_attrib("PUBLIC_CLIQUES", missing_factory=tuple, env_var_converter=split_by_comma)  # type: ignore
+    FORBIDDEN_CLIQUES: tuple[str] = s_attrib("FORBIDDEN_CLIQUES", missing_factory=tuple, env_var_converter=split_by_comma)  # type: ignore
+    DEFAULT_CLIQUE: Optional[str] = s_attrib("DEFAULT_CLIQUE", missing=None)  # type: ignore
 
 
-class ConnectorsDataCHYTBase(ConnectorsDataBase):
-    CONN_CHYT_PUBLIC_CLIQUES: ClassVar[Optional[tuple[str, ...]]] = None
-    CONN_CHYT_FORBIDDEN_CLIQUES: ClassVar[Optional[tuple[str, ...]]] = None
-    CONN_CHYT_DEFAULT_CLIQUE: ClassVar[Optional[str]] = None
-
-    @classmethod
-    def connector_name(cls) -> str:
-        return "CHYT"
-
-
-def chyt_settings_fallback(full_cfg: ConnectorsConfigType) -> dict[str, ConnectorSettingsBase]:
-    cfg = get_connectors_settings_config(
-        full_cfg,
-        object_like_config_key="CHYT",
-        connector_data_class=ConnectorsDataCHYTBase,
-    )
+def chyt_settings_fallback(full_cfg: ObjectLikeConfig) -> dict[str, ConnectorSettingsBase]:
+    cfg = get_connectors_settings_config(full_cfg, object_like_config_key="CHYT")
     if cfg is None:
         return {}
     return dict(
--- lib/dl_connector_ydb/dl_connector_ydb/core/ydb/settings.py
+++ lib/dl_connector_ydb/dl_connector_ydb/core/ydb/settings.py
@@ -1,15 +1,7 @@
-from typing import (
-    ClassVar,
-    Optional,
-)
-
 import attr
 
-from dl_configs.connectors_data import ConnectorsDataBase
-from dl_configs.connectors_settings import (
-    ConnectorsConfigType,
-    ConnectorSettingsBase,
-)
+from dl_configs.connectors_settings import ConnectorSettingsBase
+from dl_configs.settings_loaders.fallback_cfg_resolver import ObjectLikeConfig
 from dl_configs.settings_loaders.meta_definition import s_attrib
 from dl_core.connectors.settings.primitives import (
     ConnectorSettingsDefinition,
@@ -19,24 +11,12 @@ from dl_core.connectors.settings.primitives import (
 
 @attr.s(frozen=True)
 class YDBConnectorSettings(ConnectorSettingsBase):
-    ENABLE_AUTH_TYPE_PICKER: Optional[bool] = s_attrib("ENABLE_AUTH_TYPE_PICKER", missing=False)  # type: ignore  # 2024-01-30 # TODO: Incompatible types in assignment (expression has type "Attribute[Any]", variable has type "bool | None")  [assignment]
-    DEFAULT_HOST_VALUE: Optional[str] = s_attrib("DEFAULT_HOST_VALUE", missing=None)  # type: ignore  # 2024-01-30 # TODO: Incompatible types in assignment (expression has type "Attribute[Any]", variable has type "str | None")  [assignment]
+    ENABLE_AUTH_TYPE_PICKER: Optional[bool] = s_attrib("ENABLE_AUTH_TYPE_PICKER", missing=False)  # type: ignore
+    DEFAULT_HOST_VALUE: Optional[str] = s_attrib("DEFAULT_HOST_VALUE", missing=None)  # type: ignore
 
 
-class ConnectorsDataYDBBase(ConnectorsDataBase):
-    ENABLE_AUTH_TYPE_PICKER: ClassVar[Optional[bool]] = False
-
-    @classmethod
-    def connector_name(cls) -> str:
-        return "YDB"
-
-
-def ydb_settings_fallback(full_cfg: ConnectorsConfigType) -> dict[str, ConnectorSettingsBase]:
-    cfg = get_connectors_settings_config(
-        full_cfg,
-        object_like_config_key="YDB",
-        connector_data_class=ConnectorsDataYDBBase,
-    )
+def ydb_settings_fallback(full_cfg: ObjectLikeConfig) -> dict[str, ConnectorSettingsBase]:
+    cfg = get_connectors_settings_config(full_cfg, object_like_config_key="YDB")
     if cfg is None:
         return {}
     return dict(YDB=YDBConnectorSettings(ENABLE_AUTH_TYPE_PICKER=cfg.ENABLE_AUTH_TYPE_PICKER))  # type: ignore  # 2024-01-30 # TODO: Unexpected keyword argument "ENABLE_AUTH_TYPE_PICKER" for "YDBConnectorSettings"  [call-arg]
