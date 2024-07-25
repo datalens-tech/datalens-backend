7edc240e4e361342286c0d45329a45239c03e9c8
Nick Proskurin <42863572+MCPN@users.noreply.github.com>
2024-07-25 15:00:58 +0200

    fix: BI-5673 fix mypy for serializers, add more tests (#550)

--- lib/dl_core/dl_core_tests/unit/test_json_serializer.py
+++ lib/dl_core/dl_core_tests/unit/test_json_serializer.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import datetime
 import decimal
 import json
+import uuid
 
 from dl_model_tools.serialization import (
     RedisDatalensDataJSONDecoder,
@@ -10,22 +11,51 @@ from dl_model_tools.serialization import (
 )
 
 
+TZINFO = datetime.timezone(datetime.timedelta(seconds=-1320))
 SAMPLE_DATA = dict(
-    some_dt=datetime.datetime(
-        2019, 6, 17, 7, 1, 41, 79585, tzinfo=datetime.timezone(datetime.timedelta(seconds=-1320))
-    ),
+    # Scalars
+    some_int=42,
+    some_float=19.89,
+    some_str="Some say",
+    some_bool=True,
+    some_none=None,
+    # BI data values
+    some_dt=datetime.datetime(2019, 6, 17, 7, 1, 41, 79585, tzinfo=TZINFO),
     some_date=datetime.date(2020, 7, 18),
-    some_time=datetime.time(11, 22, 33, 444555, tzinfo=datetime.timezone(datetime.timedelta(seconds=1320))),
+    some_time=datetime.time(11, 22, 33, 444555, tzinfo=TZINFO),
     some_timedelta=datetime.timedelta(seconds=1320.0231),
-    some_decimal=decimal.Decimal("12345" * 10 + "." + "54321" * 10),
+    some_decimal=decimal.Decimal("12345" * 9 + "." + "54321" * 9),
+    some_uuid=uuid.UUID("12345678123456781234567812345678"),
+    # some_bytes=b"Another one bites",  TODO: currently not serializable
+)
+
+
+EXPECTED_DUMP = dict(
+    # Scalars are unchanged
+    some_int=42,
+    some_float=19.89,
+    some_str="Some say",
+    some_bool=True,
+    some_none=None,
+    # More complex types have custom dumpers
+    some_dt={"__dl_type__": "datetime", "value": [[2019, 6, 17, 7, 1, 41, 79585], -1320.0]},
+    some_date={"__dl_type__": "date", "value": "2020-07-18"},
+    some_time={"__dl_type__": "time", "value": [[11, 22, 33, 444555], -1320.0]},
+    some_timedelta={"__dl_type__": "timedelta", "value": 1320.0231},
+    some_decimal={
+        "__dl_type__": "decimal",
+        "value": "123451234512345123451234512345123451234512345.543215432154321543215432154321543215432154321",
+    },
+    some_uuid={"__dl_type__": "uuid", "value": "12345678-1234-5678-1234-567812345678"},
+    # some_bytes=b"Another one bites",
 )
 
 
 def test_json_serialization():
     data = SAMPLE_DATA
     dumped = json.dumps(data, cls=RedisDatalensDataJSONEncoder)
-    dumped_dt = json.loads(dumped)["some_dt"]
-    assert dumped_dt == {"__dl_type__": "datetime", "value": [[2019, 6, 17, 7, 1, 41, 79585], -1320.0]}, dumped_dt
+    dumped_dict = json.loads(dumped)
+    assert dumped_dict == EXPECTED_DUMP
     roundtrip = json.loads(dumped, cls=RedisDatalensDataJSONDecoder)
     assert roundtrip == data
 
@@ -36,5 +66,6 @@ def test_json_tricky_serialization():
     tricky_data = dict(normal=data, abnormal=json.loads(dumped))
     tricky_data_dumped = json.dumps(tricky_data, cls=RedisDatalensDataJSONEncoder)
     tricky_roundtrip = json.loads(tricky_data_dumped, cls=RedisDatalensDataJSONDecoder)
-    # WARNING: not currently viable: `assert tricky_roundtrip == tricky_data, tricky_roundtrip`
-    assert tricky_roundtrip
+    assert tricky_roundtrip["normal"] == tricky_data["normal"], tricky_roundtrip
+    # abnormal data contains __dl_type__ fields, so decoder considers them to be dumps of BI types and decodes them
+    assert tricky_roundtrip["abnormal"] == tricky_data["normal"], tricky_roundtrip
