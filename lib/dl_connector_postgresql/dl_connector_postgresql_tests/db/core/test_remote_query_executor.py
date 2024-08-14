9bbf6956ac86aa2e9b132a1b89da4951d4fd14a7
Nick Proskurin <mcpn@yandex-team.ru>
2024-08-14 18:03:27 +0200

    fix

--- lib/dl_connector_postgresql/dl_connector_postgresql_tests/db/core/test_remote_query_executor.py
+++ lib/dl_connector_postgresql/dl_connector_postgresql_tests/db/core/test_remote_query_executor.py
@@ -3,12 +3,14 @@ import pytest
 from dl_core_testing.testcases.remote_query_executor import BaseRemoteQueryExecutorTestClass
 
 from dl_connector_postgresql.core.postgresql_base.adapters_postgres import PostgresAdapter
+from dl_connector_postgresql.core.postgresql_base.async_adapters_postgres import AsyncPostgresAdapter
 from dl_connector_postgresql_tests.db.config import SUBSELECT_QUERY_FULL
 from dl_connector_postgresql_tests.db.core.base import BasePostgreSQLTestClass
 
 
 class TestPostgreSQLRemoteQueryExecutor(BasePostgreSQLTestClass, BaseRemoteQueryExecutorTestClass):
-    ADAPTER_CLS = PostgresAdapter
+    SYNC_ADAPTER_CLS = PostgresAdapter
+    ASYNC_ADAPTER_CLS = AsyncPostgresAdapter
 
     @pytest.mark.asyncio
     async def test_qe_result(self, remote_adapter):
