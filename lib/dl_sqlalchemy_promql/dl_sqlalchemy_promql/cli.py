d2a4d15243a4926f500d410156e9af77be81f9b5
KonstantAnxiety <konstasa@yandex-team.ru>
2024-11-05 17:08:00 +0300

    sec(connectors): BI-0 forbid redirects in promql connector

--- lib/dl_sqlalchemy_promql/dl_sqlalchemy_promql/cli.py
+++ lib/dl_sqlalchemy_promql/dl_sqlalchemy_promql/cli.py
@@ -40,7 +40,7 @@ def rebuild_prometheus_data(data):
 class SyncPromQLClient:
     def __init__(self, base_url, username, password, **kwargs):
         self._closed = False
-        self._session = requests.Session()
+        self._session = requests.Session(max_redirects=0)
         for prefix in ("http://", "https://"):
             self._session.mount(
                 prefix,
