cc66cacb92431ab4d6b29d45a9545f2da40cd16c
Nick Proskurin <mcpn@yandex-team.ru>
2024-11-08 15:52:08 +0100

    fix: BI-5864 fixes for connection replacements

--- lib/dl_connector_oracle/dl_connector_oracle/core/data_source_migration.py
+++ lib/dl_connector_oracle/dl_connector_oracle/core/data_source_migration.py
@@ -1,4 +1,5 @@
 from dl_core.connectors.sql_base.data_source_migration import DefaultSQLDataSourceMigrator
+from dl_core.data_source_spec.sql import StandardSchemaSQLDataSourceSpec
 
 from dl_connector_oracle.core.constants import (
     SOURCE_TYPE_ORACLE_SUBSELECT,
@@ -7,5 +8,6 @@ from dl_connector_oracle.core.constants import (
 
 
 class OracleDataSourceMigrator(DefaultSQLDataSourceMigrator):
+    table_dsrc_spec_cls = StandardSchemaSQLDataSourceSpec
     table_source_type = SOURCE_TYPE_ORACLE_TABLE
     subselect_source_type = SOURCE_TYPE_ORACLE_SUBSELECT
