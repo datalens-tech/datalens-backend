f6dcd729cf06c292d42047c1d3a6d8563fbfa55f
Nick Proskurin <mcpn@yandex-team.ru>
2024-11-11 11:04:46 +0100

    test: BI-0 upload test data in bigger chunks (#698)

--- lib/dl_db_testing/dl_db_testing/database/base.py
+++ lib/dl_db_testing/dl_db_testing/database/base.py
@@ -159,7 +159,7 @@ class DbTableBase:
         return self.table.name
 
     def insert(self, data: Union[dict, List[dict]], chunk_size: Optional[int] = None) -> None:
-        chunk_size = chunk_size or 1
+        chunk_size = chunk_size or 1000
         assert chunk_size is not None
 
         # TODO: Use insert_into_table
@@ -168,16 +168,10 @@ class DbTableBase:
         if isinstance(data, dict):
             self.db.execute(self.table.insert(data))
         elif isinstance(data, list):
-            if chunk_size > 1:
-                # TODO: Change to itertools.batched after switching to Python 3.12
-                rest = data
-                while rest:
-                    chunk = rest[:chunk_size]
-                    self.db.execute(self.table.insert(chunk))
-                    rest = rest[chunk_size:]
-            else:
-                for row in data:
-                    self.insert(row)
+            # TODO: Change to itertools.batched after switching to Python 3.12
+            for pos in range(0, len(data), chunk_size):
+                chunk = data[pos : pos + chunk_size]
+                self.db.execute(self.table.insert(), chunk)
         else:
             raise TypeError(type(data))
 
