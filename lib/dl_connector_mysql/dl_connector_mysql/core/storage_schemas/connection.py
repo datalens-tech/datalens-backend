492d7430bbcd449b02ca1ae8b115421bfac5cd89
khamitovdr <khamitovdr@yandex-team.ru>
2024-12-23 18:16:28 +0300

    Add missing SSL fields to MySQL connection storage schema

--- lib/dl_connector_mysql/dl_connector_mysql/core/storage_schemas/connection.py
+++ lib/dl_connector_mysql/dl_connector_mysql/core/storage_schemas/connection.py
@@ -1,3 +1,5 @@
+from marshmallow import fields as ma_fields
+
 from dl_core.us_manager.storage_schemas.connection import ConnectionSQLDataStorageSchema
 
 from dl_connector_mysql.core.us_connection import ConnectionMySQL
@@ -5,3 +7,16 @@ from dl_connector_mysql.core.us_connection import ConnectionMySQL
 
 class ConnectionMySQLDataStorageSchema(ConnectionSQLDataStorageSchema[ConnectionMySQL.DataModel]):
     TARGET_CLS = ConnectionMySQL.DataModel
+
+    ssl_enable = ma_fields.Boolean(
+        required=False,
+        allow_none=False,
+        dump_default=False,
+        load_default=False,
+    )
+    ssl_ca = ma_fields.String(
+        required=False,
+        allow_none=True,
+        dump_default=None,
+        load_default=None,
+    )
