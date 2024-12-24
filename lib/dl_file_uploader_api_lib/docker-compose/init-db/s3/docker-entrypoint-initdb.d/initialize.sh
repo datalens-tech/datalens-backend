e051b9e3cd20b5003fb8e5e8c3ac90dfbd95bfaf
KonstantAnxiety <konstasa@yandex-team.ru>
2024-12-24 20:07:32 +0300

    update init-db

--- lib/dl_file_uploader_api_lib/docker-compose/init-db/s3/docker-entrypoint-initdb.d/initialize.sh
+++ lib/dl_file_uploader_api_lib/docker-compose/init-db/s3/docker-entrypoint-initdb.d/initialize.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo 'Waiting for S3 to initialize...'
-until curl -s s3-storage:8000 > /dev/null
+until curl -s s3-storage:9000 > /dev/null
 do
     sleep 5
     echo 'Waiting for S3 to initialize...'
