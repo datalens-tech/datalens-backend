4b0e6e1548e4d3f2304581dec78465486bbc3e44
khamitovdr <khamitovdr@yandex-team.ru>
2025-01-21 11:57:27 +0300

    Storage schema

---
+++ lib/dl_connector_trino/dl_connector_trino/core/storage_schemas/connection.py
@@ -0,0 +1,24 @@
+from marshmallow import fields as ma_fields
+
+from dl_core.us_manager.storage_schemas.connection import ConnectionSQLDataStorageSchema
+
+from dl_connector_trino.core.us_connection import ConnectionTrino
+from dl_connector_trino.core.constants import TrinoAuthType
+
+
+class TrinoConnectionDataStorageSchema(ConnectionSQLDataStorageSchema[ConnectionTrino.DataModel]):
+    TARGET_CLS = ConnectionTrino.DataModel
+
+    auth_type = ma_fields.Enum(
+        TrinoAuthType,
+        required=True,
+        allow_none=False,
+        dump_default=TrinoAuthType.NONE,
+        load_default=TrinoAuthType.NONE,
+    )
+    ssl_ca = ma_fields.String(
+        required=False,
+        allow_none=True,
+        dump_default=None,
+        load_default=None,
+    )
