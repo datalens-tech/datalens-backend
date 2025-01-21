8a14cc970f73424a8b86cfc049a6ae1c4870dab6
Nick Proskurin <mcpn@yandex-team.ru>
2025-01-21 14:39:20 +0100

    add

---
+++ lib/dl_testing/dl_testing/constants.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class TestUserName(Enum):
+    APP_SYNC = "_the_tests_syncapp_user_name_"
+    APP_ASYNC = "_the_tests_asyncapp_user_name_"
+
+
+class TestUserID(Enum):
+    APP_SYNC = "_the_tests_syncapp_user_id_"
+    APP_ASYNC = "_the_tests_asyncapp_user_id_"
