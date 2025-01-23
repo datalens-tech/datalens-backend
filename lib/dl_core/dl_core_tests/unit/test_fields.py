52620af892590d623e01bb322adb7ce2d16e2afe
ovsds <ovsds@yandex-team.ru>
2025-01-23 17:25:51 +0100

    feat: BI-6017 add parameter constraint tests

--- lib/dl_core/dl_core_tests/unit/test_fields.py
+++ lib/dl_core/dl_core_tests/unit/test_fields.py
@@ -1,4 +1,14 @@
-from dl_core.fields import BIField
+from dl_constants.enums import ParameterValueConstraintType
+from dl_core.fields import (
+    AllParameterValueConstraint,
+    BIField,
+    RangeParameterValueConstraint,
+    SetParameterValueConstraint,
+)
+from dl_model_tools.schema.typed_values import (
+    IntegerValue,
+    StringValue,
+)
 from dl_testing.utils import get_log_record
 
 
@@ -9,3 +19,28 @@ def test_rename_in_formula(caplog):
 
     log_record = get_log_record(caplog, predicate=lambda r: r.message.startswith("Unknown field"), single=True)
     assert log_record.message == "Unknown field: bananas"
+
+
+def test_all_parameter_value_constraint():
+    constraint = AllParameterValueConstraint()
+    assert constraint.type == ParameterValueConstraintType.all
+    assert constraint.is_valid(StringValue("foo"))
+    assert constraint.is_valid(IntegerValue(42))
+
+
+def test_set_parameter_value_constraint():
+    constraint = SetParameterValueConstraint(values=[StringValue("foo"), StringValue("bar")])
+    assert constraint.type == ParameterValueConstraintType.set
+    assert constraint.is_valid(StringValue("foo"))
+    assert constraint.is_valid(StringValue("bar"))
+    assert not constraint.is_valid(StringValue("baz"))
+
+
+def test_range_parameter_value_constraint():
+    constraint = RangeParameterValueConstraint(min=IntegerValue(1), max=IntegerValue(5))
+    assert constraint.type == ParameterValueConstraintType.range
+    assert not constraint.is_valid(IntegerValue(0))
+    assert constraint.is_valid(IntegerValue(1))
+    assert constraint.is_valid(IntegerValue(2))
+    assert constraint.is_valid(IntegerValue(5))
+    assert not constraint.is_valid(IntegerValue(6))
