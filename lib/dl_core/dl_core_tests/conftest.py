50b8cdaba85aa8797e465bd9d625b016133b17de
Nick Proskurin <mcpn@yandex-team.ru>
2025-01-24 15:41:18 +0100

    test: BI-0 fix failing dl_core tests (#808)

--- lib/dl_core/dl_core_tests/conftest.py
+++ lib/dl_core/dl_core_tests/conftest.py
@@ -1,15 +1,11 @@
-from aiohttp.pytest_plugin import aiohttp_client
 import pytest
 
 from dl_testing.utils import get_root_certificates
 
 
+pytest_plugins = ("aiohttp.pytest_plugin",)  # and it, in turn, includes 'pytest_asyncio.plugin'
+
+
 @pytest.fixture(scope="session")
 def root_certificates() -> bytes:
     return get_root_certificates()
-
-
-# Imported fixtures
-__all__ = [
-    "aiohttp_client",
-]
