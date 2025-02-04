b843f4cae67c33445d1c5982a5fad6f663d2782a
ovsds <ovsds@yandex-team.ru>
2025-02-04 17:41:26 +0100

    feat: BI-6020 add parameter constraints api tests

---
+++ lib/dl_api_lib/dl_api_lib_tests/db/control_api/validation/test_parameters.py
@@ -0,0 +1,112 @@
+import http
+
+from dl_api_client.dsmaker.api.dataset_api import SyncHttpDatasetApiV1
+from dl_api_client.dsmaker.primitives import Dataset
+from dl_api_lib_tests.db.base import DefaultApiTestBase
+import dl_constants.enums as dl_constants_enums
+
+
+class TestValidationParameters(DefaultApiTestBase):
+    def test_add_parameter(
+        self,
+        control_api: SyncHttpDatasetApiV1,
+        saved_dataset: Dataset,
+    ):
+        ds_resp = control_api.apply_updates(
+            dataset=saved_dataset,
+            updates=[
+                {
+                    "action": "add_field",
+                    "field": {
+                        "guid": "test_guid",
+                        "title": "test_title",
+                        "calc_mode": dl_constants_enums.CalcMode.parameter.value,
+                        "cast": "integer",
+                        "default_value": "123",
+                        "value_constraint": {
+                            "type": "set",
+                            "values": ["123", "456"],
+                        },
+                    },
+                }
+            ],
+            fail_ok=True,
+        )
+        assert ds_resp.status_code == http.HTTPStatus.OK, ds_resp.response_errors
+
+    def test_add_parameter_fails_on_invalid_default_value_type(
+        self,
+        control_api: SyncHttpDatasetApiV1,
+        saved_dataset: Dataset,
+    ):
+        ds_resp = control_api.apply_updates(
+            dataset=saved_dataset,
+            updates=[
+                {
+                    "action": "add_field",
+                    "field": {
+                        "guid": "test_guid",
+                        "title": "test_title",
+                        "calc_mode": dl_constants_enums.CalcMode.parameter.value,
+                        "cast": "integer",
+                        "default_value": "abc",
+                    },
+                }
+            ],
+            fail_ok=True,
+        )
+        assert ds_resp.status_code == http.HTTPStatus.BAD_REQUEST
+
+    def test_add_parameter_fails_on_invalid_value_constraint(
+        self,
+        control_api: SyncHttpDatasetApiV1,
+        saved_dataset: Dataset,
+    ):
+        ds_resp = control_api.apply_updates(
+            dataset=saved_dataset,
+            updates=[
+                {
+                    "action": "add_field",
+                    "field": {
+                        "guid": "test_guid",
+                        "title": "test_title",
+                        "calc_mode": dl_constants_enums.CalcMode.parameter.value,
+                        "cast": "integer",
+                        "default_value": "123",
+                        "value_constraint": {
+                            "type": "set",
+                            "values": ["123", "abc"],
+                        },
+                    },
+                }
+            ],
+            fail_ok=True,
+        )
+        assert ds_resp.status_code == http.HTTPStatus.BAD_REQUEST
+
+    def test_add_parameter_fails_on_default_value_fails_constraint(
+        self,
+        control_api: SyncHttpDatasetApiV1,
+        saved_dataset: Dataset,
+    ):
+        ds_resp = control_api.apply_updates(
+            dataset=saved_dataset,
+            updates=[
+                {
+                    "action": "add_field",
+                    "field": {
+                        "guid": "test_guid",
+                        "title": "test_title",
+                        "calc_mode": dl_constants_enums.CalcMode.parameter.value,
+                        "cast": "integer",
+                        "default_value": "789",
+                        "value_constraint": {
+                            "type": "set",
+                            "values": ["123", "456"],
+                        },
+                    },
+                }
+            ],
+            fail_ok=True,
+        )
+        assert ds_resp.status_code == http.HTTPStatus.BAD_REQUEST
