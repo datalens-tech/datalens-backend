f9427a0b840ad33da270f38b8783fa605e7874e4
khamitovdr <khamitovdr@yandex-team.ru>
2025-02-05 09:28:17 +0300

    Lets sleep!

--- lib/dl_connector_trino/dl_connector_trino_tests/db/core/test_connection.py
+++ lib/dl_connector_trino/dl_connector_trino_tests/db/core/test_connection.py
@@ -3,7 +3,9 @@ from dl_core_testing.testcases.connection import DefaultConnectionTestClass
 
 from dl_connector_trino.core.constants import TrinoAuthType
 from dl_connector_trino.core.us_connection import ConnectionTrino
-from dl_connector_trino_tests.db.core.base import (  # BaseTrinoJwtTestClass,; BaseTrinoPasswordTestClass,
+from dl_connector_trino_tests.db.core.base import (
+    BaseTrinoJwtTestClass,
+    BaseTrinoPasswordTestClass,
     BaseTrinoTestClass,
 )
 
@@ -34,23 +36,23 @@ class TestTrinoConnection(
         assert "system" not in tmpl_db_names
 
 
-# class TestTrinoPasswordConnection(BaseTrinoPasswordTestClass, TestTrinoConnection):
-#     def check_saved_connection(self, conn: ConnectionTrino, params: dict) -> None:
-#         assert conn.uuid is not None
-#         assert conn.data.host == params["host"]
-#         assert conn.data.port == params["port"]
-#         assert conn.data.username == params["username"]
-#         assert conn.data.password == params["password"]
-#         assert conn.data.auth_type == TrinoAuthType.PASSWORD
-#         assert conn.data.ssl_ca == params["ssl_ca"]
+class TestTrinoPasswordConnection(BaseTrinoPasswordTestClass, TestTrinoConnection):
+    def check_saved_connection(self, conn: ConnectionTrino, params: dict) -> None:
+        assert conn.uuid is not None
+        assert conn.data.host == params["host"]
+        assert conn.data.port == params["port"]
+        assert conn.data.username == params["username"]
+        assert conn.data.password == params["password"]
+        assert conn.data.auth_type == TrinoAuthType.PASSWORD
+        assert conn.data.ssl_ca == params["ssl_ca"]
 
 
-# class TestTrinoJwtConnection(BaseTrinoJwtTestClass, TestTrinoConnection):
-#     def check_saved_connection(self, conn: ConnectionTrino, params: dict) -> None:
-#         assert conn.uuid is not None
-#         assert conn.data.host == params["host"]
-#         assert conn.data.port == params["port"]
-#         assert conn.data.username == params["username"]
-#         assert conn.data.jwt == params["jwt"]
-#         assert conn.data.auth_type == TrinoAuthType.JWT
-#         assert conn.data.ssl_ca == params["ssl_ca"]
+class TestTrinoJwtConnection(BaseTrinoJwtTestClass, TestTrinoConnection):
+    def check_saved_connection(self, conn: ConnectionTrino, params: dict) -> None:
+        assert conn.uuid is not None
+        assert conn.data.host == params["host"]
+        assert conn.data.port == params["port"]
+        assert conn.data.username == params["username"]
+        assert conn.data.jwt == params["jwt"]
+        assert conn.data.auth_type == TrinoAuthType.JWT
+        assert conn.data.ssl_ca == params["ssl_ca"]
