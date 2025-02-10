0de1e7f11ed817115544ad34531347a8b17ef1a1
khamitovdr <khamitovdr@yandex-team.ru>
2025-02-10 18:16:34 +0300

    Return to default get_schemas_for_type_recognition

--- lib/dl_connector_trino/dl_connector_trino_tests/db/core/test_connection_executor.py
+++ lib/dl_connector_trino/dl_connector_trino_tests/db/core/test_connection_executor.py
@@ -1,7 +1,4 @@
-from typing import (
-    Optional,
-    Sequence,
-)
+from typing import Optional
 
 import pytest
 
@@ -32,36 +29,4 @@ class TestTrinoSyncConnectionExecutor(
     TrinoSyncConnectionExecutorBase,
     DefaultSyncConnectionExecutorTestSuite[ConnectionTrino],
 ):
-    def get_schemas_for_type_recognition(self) -> dict[str, Sequence[DefaultSyncConnectionExecutorTestSuite.CD]]:
-        return {
-            # "trino_types_number": [
-            #     self.CD(trino_types.TINYINT(), UserDataType.integer),
-            #     self.CD(trino_types.SMALLINT(), UserDataType.integer),
-            #     self.CD(trino_types.MEDIUMINT(), UserDataType.integer),
-            #     self.CD(trino_types.INTEGER(), UserDataType.integer),
-            #     self.CD(trino_types.BIGINT(), UserDataType.integer),
-            #     self.CD(trino_types.FLOAT(), UserDataType.float),
-            #     self.CD(trino_types.DOUBLE(), UserDataType.float),
-            #     self.CD(trino_types.NUMERIC(), UserDataType.float, nt_name="decimal"),
-            #     self.CD(trino_types.DECIMAL(), UserDataType.float),
-            #     self.CD(trino_types.BIT(1), UserDataType.boolean),
-            # ],
-            # "trino_types_string": [
-            #     self.CD(trino_types.CHAR(), UserDataType.string),
-            #     self.CD(trino_types.VARCHAR(100), UserDataType.string),
-            #     self.CD(trino_types.TINYTEXT(), UserDataType.string),
-            #     self.CD(trino_types.TEXT(), UserDataType.string),
-            # ],
-            # "trino_types_date": [
-            #     self.CD(trino_types.DATE(), UserDataType.date),
-            #     self.CD(trino_types.TIMESTAMP(), UserDataType.genericdatetime),
-            #     self.CD(trino_types.DATETIME(), UserDataType.genericdatetime),
-            # ],
-            # "trino_types_other": [
-            #     self.CD(trino_types.TINYBLOB(), UserDataType.string),
-            #     self.CD(trino_types.BLOB(), UserDataType.string),
-            #     self.CD(trino_types.BINARY(), UserDataType.string),
-            #     self.CD(trino_types.VARBINARY(100), UserDataType.string),
-            #     self.CD(trino_types.ENUM("a", "b", "c", name="some_enum"), UserDataType.string),
-            # ],
-        }
+    pass
