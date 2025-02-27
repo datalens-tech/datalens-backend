58c8531fd2a87fb8df2641829cd67bd3851eb0f3
khamitovdr <khamitovdr@yandex-team.ru>
2025-02-27 19:41:37 +0300

    Add TrinoErrorTransformer and custom exception for non-existent tables

--- lib/dl_connector_trino/dl_connector_trino/core/error_transformer.py
+++ lib/dl_connector_trino/dl_connector_trino/core/error_transformer.py
@@ -1,7 +1,63 @@
-from dl_core.connectors.base.error_transformer import (
-    ChainedDbErrorTransformer,
-    DbErrorTransformer,
+from typing import Optional
+
+import sqlalchemy
+from trino.exceptions import (
+    TrinoQueryError,
+    TrinoUserError,
 )
 
+from dl_core.connectors.base.error_transformer import (
+    ChainedDbErrorTransformer,
+    DBExcKWArgs,
+)
+from dl_core.connectors.base.error_transformer import (
+    ExcMatchCondition,
+    default_error_transformer_rules,
+)
+from dl_core.connectors.base.error_transformer import ErrorTransformerRule as Rule
 
-trino_error_transformer: DbErrorTransformer = ChainedDbErrorTransformer([])
+from dl_connector_trino.core.exc import TrinoSourceDoesNotExistError
+
+
+class TrinoErrorTransformer(ChainedDbErrorTransformer):
+    @staticmethod
+    def _get_error_kw(
+        debug_compiled_query: Optional[str], orig_exc: Optional[Exception], wrapper_exc: Exception
+    ) -> DBExcKWArgs:
+        assert isinstance(orig_exc, TrinoQueryError)
+        return dict(
+            db_message=orig_exc.message,
+            query=debug_compiled_query,
+            orig=orig_exc,
+            details={
+                "error_name": orig_exc.error_name,
+                "error_type": orig_exc.error_type,
+                "error_code": orig_exc.error_code,
+                "query_id": orig_exc.query_id,
+            },
+        )
+
+
+def is_table_does_not_exist_sync_error() -> ExcMatchCondition:
+    def _(exc: Exception) -> bool:
+        if isinstance(exc, sqlalchemy.exc.ProgrammingError):
+            orig = getattr(exc, "orig", None)
+            if not isinstance(orig, TrinoUserError):
+                return False
+
+            if orig.error_name == "TABLE_NOT_FOUND":
+                return True
+        return False
+
+    return _
+
+
+trino_error_transformer = TrinoErrorTransformer(
+    rule_chain=(
+        Rule(
+            when=is_table_does_not_exist_sync_error(),
+            then_raise=TrinoSourceDoesNotExistError,
+        ),
+    )
+    + default_error_transformer_rules
+)
