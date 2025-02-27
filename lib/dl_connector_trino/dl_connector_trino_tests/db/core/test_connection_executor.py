c6d28c60d4eb6cb62596c66356de3885bfb39704
khamitovdr <khamitovdr@yandex-team.ru>
2025-02-27 19:41:53 +0300

    Add test for handling errors when selecting from non-existent sources

--- lib/dl_connector_trino/dl_connector_trino_tests/db/core/test_connection_executor.py
+++ lib/dl_connector_trino/dl_connector_trino_tests/db/core/test_connection_executor.py
@@ -2,7 +2,14 @@ from typing import Optional
 
 import pytest
 
-from dl_core.connection_models.common_models import DBIdent
+from dl_core.connection_executors.common_base import ConnExecutorQuery
+from dl_core.connection_executors.sync_base import SyncConnExecutorBase
+from dl_core.connection_models.common_models import (
+    DBIdent,
+    TableIdent,
+)
+import dl_core.exc as core_exc
+from dl_core_testing.database import Db
 from dl_core_testing.testcases.connection_executor import (
     DefaultSyncAsyncConnectionExecutorCheckBase,
     DefaultSyncConnectionExecutorTestSuite,
@@ -29,4 +36,20 @@ class TestTrinoSyncConnectionExecutor(
     TrinoSyncConnectionExecutorBase,
     DefaultSyncConnectionExecutorTestSuite[ConnectionTrino],
 ):
-    pass
+    def test_error_on_select_from_nonexistent_source(
+        self,
+        db: Db,
+        sync_connection_executor: SyncConnExecutorBase,
+        nonexistent_table_ident: TableIdent,
+    ) -> None:
+        table_ident = ".".join(
+            db.quote(s)
+            for s in (
+                nonexistent_table_ident.db_name,
+                nonexistent_table_ident.schema_name,
+                nonexistent_table_ident.table_name,
+            )
+        )
+        query = ConnExecutorQuery(query=f"SELECT * from {table_ident}")
+        with pytest.raises(core_exc.SourceDoesNotExist):
+            sync_connection_executor.execute(query)
