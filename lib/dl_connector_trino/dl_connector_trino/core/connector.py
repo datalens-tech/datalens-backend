eef60eab35ec172c529c32550be8fa8cf4444814
khamitovdr <khamitovdr@yandex-team.ru>
2025-03-10 19:22:36 +0300

    Update

--- lib/dl_connector_trino/dl_connector_trino/core/connector.py
+++ lib/dl_connector_trino/dl_connector_trino/core/connector.py
@@ -4,6 +4,9 @@ from dl_core.connectors.base.connector import (
     CoreConnector,
     CoreSourceDefinition,
 )
+from dl_core.connectors.sql_base.connector import SQLSubselectCoreSourceDefinitionBase
+from dl_core.data_source_spec.sql import StandardSQLDataSourceSpec
+from dl_core.us_manager.storage_schemas.data_source_spec_base import SQLDataSourceSpecStorageSchema
 
 from dl_connector_trino.core.adapters import TrinoDefaultAdapter
 from dl_connector_trino.core.connection_executors import TrinoConnExecutor
@@ -13,24 +16,17 @@ from dl_connector_trino.core.constants import (
     SOURCE_TYPE_TRINO_SUBSELECT,
     SOURCE_TYPE_TRINO_TABLE,
 )
-
-# from dl_connector_trino.core.data_source import (
-#     TrinoSubselectDataSource,
-#     TrinoTableDataSource,
-# )
-# from dl_connector_trino.core.data_source_spec import (
-#     TrinoSubselectDataSourceSpec,
-#     TrinoTableDataSourceSpec,
-# )
-# from dl_connector_trino.core.lifecycle import TrinoConnectionLifecycleManager
+from dl_connector_trino.core.data_source import (
+    TrinoSubselectDataSource,
+    TrinoTableDataSource,
+)
+from dl_connector_trino.core.data_source_migration import TrinoDataSourceMigrator
 from dl_connector_trino.core.storage_schemas.connection import TrinoConnectionDataStorageSchema
-
-# from dl_connector_trino.core.storage_schemas.data_source_spec import (
-#     TrinoSubselectDataSourceSpecStorageSchema,
-#     TrinoTableDataSourceSpecStorageSchema,
-# )
 from dl_connector_trino.core.type_transformer import TrinoTypeTransformer
 from dl_connector_trino.core.us_connection import ConnectionTrino
+
+
+# from dl_connector_trino.core.lifecycle import TrinoConnectionLifecycleManager
 
 
 class TrinoCoreConnectionDefinition(CoreConnectionDefinition):
@@ -40,22 +36,20 @@ class TrinoCoreConnectionDefinition(CoreConnectionDefinition):
     type_transformer_cls = TrinoTypeTransformer
     sync_conn_executor_cls = TrinoConnExecutor
     # lifecycle_manager_cls = TrinoConnectionLifecycleManager
-    # data_source_migrator_cls = ?
+    data_source_migrator_cls = TrinoDataSourceMigrator
     dialect_string = "trino"
 
 
 class TrinoCoreTableSourceDefinition(CoreSourceDefinition):
     source_type = SOURCE_TYPE_TRINO_TABLE
-    # source_cls = TrinoTableDataSource
-    # source_spec_cls = TrinoTableDataSourceSpec
-    # us_storage_schema_cls = TrinoTableDataSourceSpecStorageSchema
+    source_cls = TrinoTableDataSource
+    source_spec_cls = StandardSQLDataSourceSpec
+    us_storage_schema_cls = SQLDataSourceSpecStorageSchema
 
 
-class TrinoCoreSubselectSourceDefinition(CoreSourceDefinition):
+class TrinoCoreSubselectSourceDefinition(SQLSubselectCoreSourceDefinitionBase):
     source_type = SOURCE_TYPE_TRINO_SUBSELECT
-    # source_cls = TrinoSubselectDataSource
-    # source_spec_cls = TrinoSubselectDataSourceSpec
-    # us_storage_schema_cls = TrinoSubselectDataSourceSpecStorageSchema
+    source_cls = TrinoSubselectDataSource
 
 
 class TrinoCoreBackendDefinition(CoreBackendDefinition):
