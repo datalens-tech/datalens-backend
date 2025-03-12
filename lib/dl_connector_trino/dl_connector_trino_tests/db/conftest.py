9e2c761b78cb24e13e1ae855e1739015bb3a7387
ovsds <ovsds@yandex-team.ru>
2025-03-12 12:49:51 +0100

    fix: remove pytest_plugin attempt

--- lib/dl_connector_trino/dl_connector_trino_tests/db/conftest.py
+++ lib/dl_connector_trino/dl_connector_trino_tests/db/conftest.py
@@ -1,9 +1,12 @@
+from aiohttp.pytest_plugin import aiohttp_client
+
 from dl_api_lib_testing.initialization import initialize_api_lib_test
 
 from dl_connector_trino_tests.db.config import API_TEST_CONFIG
 
 
-pytest_plugins = ("aiohttp.pytest_plugin",)  # and it, in turn, includes 'pytest_asyncio.plugin'
+# Imported Fixtures
+aiohttp_client = aiohttp_client
 
 
 def pytest_configure(config):
