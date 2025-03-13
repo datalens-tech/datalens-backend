78b91bc0cd67291d091d1a2c1e70ebbd6584777a
khamitovdr <khamitovdr@yandex-team.ru>
2025-03-13 20:18:33 +0300

    Fix and test literals

--- lib/dl_connector_trino/dl_connector_trino/formula/literal.py
+++ lib/dl_connector_trino/dl_connector_trino/formula/literal.py
@@ -1,6 +1,7 @@
 import datetime
 
 import sqlalchemy as sa
+from trino.sqlalchemy.datatype import TIMESTAMP
 
 from dl_formula.connectors.base.literal import (
     Literal,
@@ -10,10 +11,19 @@ from dl_formula.core.dialect import DialectCombo
 
 
 class TrinoLiteralizer(Literalizer):
-    __slots__ = ()
-
     def literal_datetime(self, value: datetime.datetime, dialect: DialectCombo) -> Literal:
-        return sa.func.timestamp(value)
+        precision = 6 if value.microsecond else None
+        has_tz = value.tzinfo is not None
 
-    def literal_date(self, value: datetime.date, dialect: DialectCombo) -> Literal:
-        return sa.func.date(value)
+        if has_tz and not isinstance(value.tzinfo, datetime.timezone):
+            utc_offset = value.utcoffset()
+            assert utc_offset is not None
+            value = value.replace(tzinfo=datetime.timezone(utc_offset))
+
+        return sa.cast(
+            value,
+            TIMESTAMP(
+                timezone=has_tz,
+                precision=precision,
+            ),
+        )
