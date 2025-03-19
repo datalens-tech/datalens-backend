95c1740c6f3627bbc35e743e6e7ca93430541ea7
khamitovdr <khamitovdr@yandex-team.ru>
2025-03-19 22:17:08 +0300

    Add DataType.STRING to FuncStrFromString argument types

--- lib/dl_formula/dl_formula/definitions/functions_type.py
+++ lib/dl_formula/dl_formula/definitions/functions_type.py
@@ -526,6 +526,7 @@ class FuncStrFromDatetimeTZ(FuncStr):
 class FuncStrFromString(FuncStr):
     variants = [V(D.DUMMY | D.SQLITE, lambda value: value)]
     argument_types = [
+        ArgTypeSequence([DataType.STRING]),
         ArgTypeSequence([DataType.DATE]),
     ]
 
