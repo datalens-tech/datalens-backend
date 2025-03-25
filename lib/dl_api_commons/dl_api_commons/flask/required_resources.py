da728cf2b7f07bccc7e7f801bddaa94c7adadf7f
vallbull <bulanovavv@yandex-team.ru>
2025-03-25 15:27:35 +0300

    fix: BI-6147 Fixes in get_required_resources func (#919)

--- lib/dl_api_commons/dl_api_commons/flask/required_resources.py
+++ lib/dl_api_commons/dl_api_commons/flask/required_resources.py
@@ -21,11 +21,17 @@ class RequiredResourceCommon(enum.Enum):
 def get_required_resources() -> typing.FrozenSet[RequiredResourceCommon]:
     if request.url_rule:
         view_func = current_app.view_functions.get(request.url_rule.endpoint, None)
+    else:
+        logger.warning("Failed to get REQUIRED_RESOURCES: url_rule is not exist")
+        return frozenset()
 
     if view_func and hasattr(view_func, "view_class"):
         resource_class = view_func.view_class
+    else:
+        logger.warning("Failed to get REQUIRED_RESOURCES: resource_class is not exist")
+        return frozenset()
 
-    if hasattr(resource_class, "REQUIRED_RESOURCES"):
+    if resource_class and hasattr(resource_class, "REQUIRED_RESOURCES"):
         return resource_class.REQUIRED_RESOURCES
 
     logger.warning("Failed to get REQUIRED_RESOURCES from flask request Resource")
