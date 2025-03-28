8e927852039cb34d8a06fd57223255ab0a5eb39c
khamitovdr <khamitovdr@yandex-team.ru>
2025-03-28 13:57:37 +0300

    test_unnest_array

---
+++ lib/dl_connector_trino/dl_connector_trino_tests/db/formula/test_functions_array.py
@@ -0,0 +1,15 @@
+import pytest
+import sqlalchemy as sa
+
+from dl_formula_testing.evaluator import DbEvaluator
+from dl_formula_testing.testcases.functions_array import DefaultArrayFunctionFormulaConnectorTestSuite
+
+from dl_connector_trino_tests.db.formula.base import TrinoFormulaTestBase
+
+
+class TestArrayFunctionTrino(TrinoFormulaTestBase, DefaultArrayFunctionFormulaConnectorTestSuite):
+    def test_unnest_array(self, dbe: DbEvaluator, data_table: sa.Table) -> None:
+        pytest.skip("Not implemented")
+
+    def test_startswith_string_array(self, dbe: DbEvaluator, data_table: sa.Table) -> None:
+        pytest.skip("Not implemented")
