149ae387e2e95e465d3bf56397bb53f8b8a2e750
khamitovdr <khamitovdr@yandex-team.ru>
2025-04-01 15:33:50 +0300

    trigonometric_functions

--- lib/dl_connector_trino/dl_connector_trino/formula/definitions/functions_math.py
+++ lib/dl_connector_trino/dl_connector_trino/formula/definitions/functions_math.py
@@ -1,5 +1,6 @@
 from dl_formula.definitions.base import TranslationVariant
 import dl_formula.definitions.functions_math as base
+from dl_formula.shortcuts import n
 
 from dl_connector_trino.formula.constants import TrinoDialect as D
 
@@ -10,21 +11,25 @@ DEFINITIONS_MATH = [
     # # abs
     # base.FuncAbs.for_dialect(D.TRINO),
     # # acos
-    # base.FuncAcos.for_dialect(D.TRINO),
+    base.FuncAcos.for_dialect(D.TRINO),
     # # asin
-    # base.FuncAsin.for_dialect(D.TRINO),
+    base.FuncAsin.for_dialect(D.TRINO),
     # # atan
-    # base.FuncAtan.for_dialect(D.TRINO),
+    base.FuncAtan.for_dialect(D.TRINO),
     # # atan2
-    # base.FuncAtan2.for_dialect(D.TRINO),
+    base.FuncAtan2.for_dialect(D.TRINO),
     # # ceiling
     # base.FuncCeiling.for_dialect(D.TRINO),
     # # cos
-    # base.FuncCos.for_dialect(D.TRINO),
+    base.FuncCos.for_dialect(D.TRINO),
     # # cot
-    # base.FuncCot.for_dialect(D.TRINO),
+    base.FuncCot(
+        variants=[
+            V(D.TRINO, lambda x: n.func.POWER(n.func.TAN(x), -1)),
+        ]
+    ),
     # # degrees
-    # base.FuncDegrees.for_dialect(D.TRINO),
+    base.FuncDegrees.for_dialect(D.TRINO),
     # # div
     # base.FuncDivBasic.for_dialect(D.TRINO),
     # # div_safe
@@ -49,22 +54,22 @@ DEFINITIONS_MATH = [
     # # log10
     # base.FuncLog10.for_dialect(D.TRINO),
     # # pi
-    # base.FuncPi.for_dialect(D.TRINO),
+    base.FuncPi.for_dialect(D.TRINO),
     # # power
-    # base.FuncPower.for_dialect(D.TRINO),
+    base.FuncPower.for_dialect(D.TRINO),
     # # radians
-    # base.FuncRadians.for_dialect(D.TRINO),
+    base.FuncRadians.for_dialect(D.TRINO),
     # # round
     base.FuncRound1.for_dialect(D.TRINO),
     # base.FuncRound2.for_dialect(D.TRINO),
     # # sign
     # base.FuncSign.for_dialect(D.TRINO),
     # # sin
-    # base.FuncSin.for_dialect(D.TRINO),
+    base.FuncSin.for_dialect(D.TRINO),
     # # sqrt
     # base.FuncSqrt.for_dialect(D.TRINO),
     # # square
     # base.FuncSquare.for_dialect(D.TRINO),
     # # tan
-    # base.FuncTan.for_dialect(D.TRINO),
+    base.FuncTan.for_dialect(D.TRINO),
 ]
