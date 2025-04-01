feeda7e088a6d3d66b9946848634724f5fe4e794
Denis Khamitov <khamitovdr@yandex-team.ru>
2025-04-01 18:38:16 +0300

    feat: BI-5355 Trino formula connector Array functions (#936)
    * test_create_array
    
    * test_unnest_array
    
    * test_array_count_item
    
    * test_array_contains
    
    * test_array_contains_all
    
    * test_array_contains_any
    
    * test_array_not_contains
    
    * test_array_slice HACK
    
    * test_replace_array
    
    * test_array_cast
    
    * test_startswith_number_array
    
    * test_array_remove
    
    * test_array_intersection
    
    * lint-fix
    
    * code style
    
    * test_startswith_string_array

--- lib/dl_connector_trino/dl_connector_trino/formula/definitions/functions_array.py
+++ lib/dl_connector_trino/dl_connector_trino/formula/definitions/functions_array.py
@@ -1,4 +1,14 @@
+from typing import cast
+
 import sqlalchemy as sa
+from sqlalchemy.sql.elements import (
+    BinaryExpression,
+    BindParameter,
+    ClauseElement,
+    ColumnClause,
+    TextClause,
+)
+from sqlalchemy.sql.functions import Function
 
 from dl_formula.core.datatype import DataType
 from dl_formula.definitions.args import ArgTypeSequence
@@ -11,10 +21,94 @@ from dl_connector_trino.formula.constants import TrinoDialect as D
 V = TranslationVariant.make
 
 
-drop_null = lambda array: sa.func.filter(array, sa.text("x -> x IS NOT NULL"))
-format_float = lambda array_float: sa.func.transform(
-    array_float, sa.text("x -> regexp_extract(format('%.8f', x), '^(-?\d+(\.[1-9]+)?)(\.?0*)$', 1)")
-)
+def create_non_const_array(*args: ColumnClause) -> TextClause:
+    compiled_args = [str(arg.compile(compile_kwargs={"literal_binds": True})) for arg in args]
+    array_elements = ",".join(compiled_args)
+    return sa.text(f"ARRAY[{array_elements}]")
+
+
+def drop_null(array: ClauseElement) -> Function:
+    return sa.func.filter(array, sa.text("x -> x IS NOT NULL"))
+
+
+def format_float(array_float: ClauseElement) -> Function:
+    return sa.func.transform(
+        array_float,
+        sa.text("x -> regexp_extract(format('%.16f', x), '^(-?\\d+(\\.[1-9]+)?)(\\.?0*)$', 1)"),
+    )
+
+
+def array_equals(x: ColumnClause, y: ColumnClause) -> Function:
+    pairwise_non_distinct = sa.func.zip_with(
+        x,
+        y,
+        sa.text("(x, y) -> x IS NOT DISTINCT FROM y"),
+    )
+    return sa.func.reduce(
+        pairwise_non_distinct,
+        True,
+        sa.text("(x, y) -> x AND y"),
+        sa.text("x -> x"),
+    )
+
+
+def array_startswith(x: ColumnClause, y: ColumnClause) -> Function:
+    len_y = sa.func.cardinality(y)
+    x_slice = sa.func.slice(x, 1, len_y)
+    return sa.func.if_(
+        sa.func.cardinality(x) < len_y,
+        False,
+        array_equals(
+            x_slice,
+            y,
+        ),
+    )
+
+
+def array_intersect(*arrays: ColumnClause) -> Function:
+    return sa.func.reduce(
+        create_non_const_array(*arrays[1:]),
+        arrays[0],
+        sa.text("(x, y) -> array_intersect(x, y)"),
+        sa.text("x -> x"),
+    )
+
+
+def count_item(array: ColumnClause, value: BindParameter) -> Function:
+    return sa.func.if_(
+        value.is_(None),
+        sa.func.cardinality(sa.func.filter(array, sa.text("x -> x IS NULL"))),
+        sa.func.cardinality(
+            sa.func.filter(
+                array,
+                sa.text(f"x -> x IS NOT DISTINCT FROM {value.compile(compile_kwargs=dict(literal_binds=True))}"),
+            )
+        ),
+    )
+
+
+def array_contains(array: ColumnClause, value: BindParameter) -> BinaryExpression:
+    return cast(BinaryExpression, count_item(array, value) > 0)
+
+
+def array_not_contains(array: ColumnClause, value: BindParameter) -> BinaryExpression:
+    return cast(BinaryExpression, count_item(array, value) == 0)
+
+
+def replace_array(array: ColumnClause, old_value: BindParameter, new_value: BindParameter) -> Function:
+    old_value_repr = old_value.compile(compile_kwargs=dict(literal_binds=True))
+    new_value_repr = new_value.compile(compile_kwargs=dict(literal_binds=True))
+    return sa.func.if_(
+        old_value.is_(None),
+        sa.func.transform(
+            array,
+            sa.text(f"x -> if(x IS NULL, {new_value_repr}, x)"),
+        ),
+        sa.func.transform(
+            array,
+            sa.text(f"x -> if(x = {old_value_repr}, {new_value_repr}, x)"),
+        ),
+    )
 
 
 class FuncArrStr3Trino(base.FuncArrStr3):
@@ -85,16 +179,16 @@ class FuncFloatArrStr1Trino(base.FuncArrStr1):
 
 DEFINITIONS_ARRAY = [
     # arr_remove
-    # base.FuncArrayRemoveLiteralNull(
-    #     variants=[
-    #         V(D.TRINO, sa.func.array_remove),
-    #     ]
-    # ),
-    # base.FuncArrayRemoveDefault(
-    #     variants=[
-    #         V(D.TRINO, sa.func.array_remove),
-    #     ]
-    # ),
+    base.FuncArrayRemoveLiteralNull(
+        variants=[
+            V(D.TRINO, lambda array, _null: sa.func.filter(array, sa.text("x -> x IS NOT NULL"))),
+        ]
+    ),
+    base.FuncArrayRemoveDefault(
+        variants=[
+            V(D.TRINO, sa.func.array_remove),
+        ]
+    ),
     # arr_str
     FuncArrStr1Trino(),
     FuncFloatArrStr1Trino(),
@@ -106,161 +200,119 @@ DEFINITIONS_ARRAY = [
     base.FuncConstArrayFloat.for_dialect(D.TRINO),
     base.FuncConstArrayInt.for_dialect(D.TRINO),
     base.FuncConstArrayStr.for_dialect(D.TRINO),
-    # base.FuncNonConstArrayInt(
-    #     variants=[
-    #         V(D.TRINO, lambda *args: sa_postgresql.array(args)),
-    #     ]
-    # ),
+    base.FuncNonConstArrayInt(
+        variants=[
+            V(D.TRINO, create_non_const_array),
+        ]
+    ),
     # base.FuncNonConstArrayFloat(
     #     variants=[
     #         V(D.TRINO, lambda *args: sa_postgresql.array(args)),
     #     ]
     # ),
-    # base.FuncNonConstArrayStr(
-    #     variants=[
-    #         V(D.TRINO, lambda *args: sa_postgresql.array(args)),
-    #     ]
-    # ),
+    base.FuncNonConstArrayStr(
+        variants=[
+            V(D.TRINO, create_non_const_array),
+        ]
+    ),
     # cast_arr_float
-    # base.FuncFloatArrayFromIntArray(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda arr: sa.func.array(sa.select(sa.cast(sa.func.unnest(arr), sa_postgresql.DOUBLE_PRECISION))),
-    #         ),
-    #     ]
-    # ),
-    # base.FuncFloatArrayFromFloatArray.for_dialect(D.TRINO),
-    # base.FuncFloatArrayFromStringArray(
-    #     variants=[
-    #         V(D.TRINO, lambda arr: sa.func.array(sa.select(sa.cast(sa.func.unnest(arr), sa.FLOAT)))),
-    #     ]
-    # ),
+    base.FuncFloatArrayFromIntArray(
+        variants=[
+            V(
+                D.TRINO,
+                lambda arr: sa.func.transform(arr, sa.text("x -> cast(x as double)")),
+            ),
+        ]
+    ),
+    base.FuncFloatArrayFromFloatArray.for_dialect(D.TRINO),
+    base.FuncFloatArrayFromStringArray(
+        variants=[
+            V(
+                D.TRINO,
+                lambda arr: sa.func.transform(arr, sa.text("x -> cast(x as double)")),
+            ),
+        ]
+    ),
     # cast_arr_int
-    # base.FuncIntArrayFromIntArray.for_dialect(D.TRINO),
-    # base.FuncIntArrayFromFloatArray(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda arr: sa.func.array(sa.select(sa.cast(sa.func.floor(sa.func.unnest(arr)), sa.BIGINT))),
-    #         ),
-    #     ]
-    # ),
-    # base.FuncIntArrayFromStringArray(
-    #     variants=[
-    #         V(D.TRINO, lambda arr: sa.func.array(sa.select(sa.cast(sa.func.unnest(arr), sa.BIGINT)))),
-    #     ]
-    # ),
+    base.FuncIntArrayFromIntArray.for_dialect(D.TRINO),
+    base.FuncIntArrayFromFloatArray(
+        variants=[
+            V(
+                D.TRINO,
+                lambda arr: sa.func.transform(arr, sa.text("x -> cast(x as bigint)")),
+            ),
+        ]
+    ),
+    base.FuncIntArrayFromStringArray(
+        variants=[
+            V(
+                D.TRINO,
+                lambda arr: sa.func.transform(arr, sa.text("x -> cast(x as bigint)")),
+            ),
+        ]
+    ),
     # cast_arr_str
-    # base.FuncStringArrayFromIntArray(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda arr: sa.func.array(sa.select(sa.func.to_char(sa.func.unnest(arr), PG_INT_64_TO_CHAR_FMT))),
-    #         ),
-    #     ]
-    # ),
-    # base.FuncStringArrayFromFloatArray(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda arr: sa.func.array(sa.select(sa.func.to_char(sa.func.unnest(arr), "FM999999999990.0999999999"))),
-    #         ),
-    #     ]
-    # ),
-    # base.FuncStringArrayFromStringArray.for_dialect(D.TRINO),
-    # Array contains(_all, _any) functions comment:
-    # Straight-forward `array_1.contains(array_2)` doesn't work due to postgres specifics in NULL and arrays interaction
-    # [NULL, 1, 2].contains([NULL, 1]) = False in postgres, that is why branching on null containment needed
-    # In PostgreSQL 9.5+ use sa.func.array_position(array, None) != None instead of array_remove comparison
+    base.FuncStringArrayFromIntArray(
+        variants=[
+            V(
+                D.TRINO,
+                lambda arr: sa.func.transform(arr, sa.text("x -> cast(x as varchar)")),
+            ),
+        ]
+    ),
+    base.FuncStringArrayFromFloatArray(
+        variants=[
+            V(D.TRINO, format_float),
+        ]
+    ),
+    base.FuncStringArrayFromStringArray.for_dialect(D.TRINO),
     # contains
-    # base.FuncArrayContains(
-    #     variants=[
-    #         V(D.TRINO, _array_contains),
-    #     ]
-    # ),
+    base.FuncArrayContains(
+        variants=[
+            V(D.TRINO, array_contains),
+        ]
+    ),
     # notcontains
-    # base.FuncArrayNotContains(
-    #     variants=[
-    #         V(D.TRINO, _array_notcontains),
-    #     ]
-    # ),
+    base.FuncArrayNotContains(
+        variants=[
+            V(D.TRINO, array_not_contains),
+        ]
+    ),
     # contains_all
-    # base.FuncArrayContainsAll(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda array_1, array_2: n.func.IF(
-    #                 array_2 != sa.func.array_remove(array_2, None),  # array_2 contains null
-    #                 n.func.IF(
-    #                     array_1 != sa.func.array_remove(array_1, None),  # array_1 contains null
-    #                     sa.type_coerce(array_1, sa_postgresql.ARRAY(TypeEngine)).contains(
-    #                         sa.type_coerce(sa.func.array_remove(array_2, None), sa_postgresql.ARRAY(TypeEngine))
-    #                     ),
-    #                     False,
-    #                 ),
-    #                 sa.type_coerce(array_1, sa_postgresql.ARRAY(TypeEngine)).contains(
-    #                     sa.type_coerce(array_2, sa_postgresql.ARRAY(TypeEngine))
-    #                 ),
-    #             ),
-    #         ),
-    #     ]
-    # ),
+    base.FuncArrayContainsAll(
+        variants=[
+            V(
+                D.TRINO,
+                lambda array_1, array_2: sa.func.cardinality(sa.func.array_intersect(array_1, array_2))
+                == sa.func.cardinality(sa.func.array_distinct(array_2)),
+            ),
+        ]
+    ),
     # contains_any
-    # base.FuncArrayContainsAny(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda array_1, array_2: n.func.IF(
-    #                 array_2 != sa.func.array_remove(array_2, None),  # array_2 contains null
-    #                 n.func.IF(
-    #                     array_1 != sa.func.array_remove(array_1, None),  # array_1 contains null
-    #                     True,
-    #                     sa.type_coerce(array_1, sa_postgresql.ARRAY(TypeEngine)).overlap(
-    #                         sa.type_coerce(sa.func.array_remove(array_2, None), sa_postgresql.ARRAY(TypeEngine))
-    #                     ),
-    #                 ),
-    #                 sa.type_coerce(array_1, sa_postgresql.ARRAY(TypeEngine)).overlap(
-    #                     sa.type_coerce(array_2, sa_postgresql.ARRAY(TypeEngine))
-    #                 ),
-    #             ),
-    #         ),
-    #     ]
-    # ),
+    base.FuncArrayContainsAny(
+        variants=[
+            V(
+                D.TRINO,
+                lambda array_1, array_2: sa.func.cardinality(sa.func.array_intersect(array_1, array_2)) > 0,
+            ),
+        ]
+    ),
     # count_item
-    # base.FuncArrayCountItemInt(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda array, value: (
-    #                 sa.func.array_length(array, 1) - sa.func.array_length(sa.func.array_remove(array, value), 1)
-    #             ),
-    #         ),
-    #     ]
-    # ),
-    # base.FuncArrayCountItemFloat(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda array, value: (
-    #                 sa.func.array_length(array, 1)
-    #                 - sa.func.array_length(
-    #                     sa.func.array_remove(array, sa.cast(value, sa_postgresql.DOUBLE_PRECISION)), 1
-    #                 )
-    #             ),
-    #         ),
-    #     ]
-    # ),
-    # base.FuncArrayCountItemStr(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda array, value: (
-    #                 sa.func.array_length(array, 1) - sa.func.array_length(sa.func.array_remove(array, value), 1)
-    #             ),
-    #         ),
-    #     ]
-    # ),
+    base.FuncArrayCountItemInt(
+        variants=[
+            V(D.TRINO, count_item),
+        ]
+    ),
+    base.FuncArrayCountItemFloat(
+        variants=[
+            V(D.TRINO, count_item),
+        ]
+    ),
+    base.FuncArrayCountItemStr(
+        variants=[
+            V(D.TRINO, count_item),
+        ]
+    ),
     # get_item
     base.FuncGetArrayItemFloat(
         variants=[
@@ -278,54 +330,46 @@ DEFINITIONS_ARRAY = [
         ]
     ),
     # len
-    # base.FuncLenArray(
-    #     variants=[
-    #         V(D.TRINO, lambda val: sa.func.ARRAY_LENGTH(val, 1)),
-    #     ]
-    # ),
+    base.FuncLenArray(
+        variants=[
+            V(D.TRINO, sa.func.cardinality),
+        ]
+    ),
     # replace
     # base.FuncReplaceArrayLiteralNull(
     #     variants=[
     #         V(D.TRINO, sa.func.array_replace),
     #     ]
     # ),
-    # base.FuncReplaceArrayDefault(
-    #     variants=[
-    #         V(D.TRINO, sa.func.array_replace),
-    #     ]
-    # ),
+    base.FuncReplaceArrayDefault(
+        variants=[
+            V(D.TRINO, replace_array),
+        ]
+    ),
     # slice
-    # base.FuncArraySlice(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda array, offset, length: Grouping(sa.type_coerce(array, sa_postgresql.ARRAY(TypeEngine)))[  # type: ignore  # 2024-01-24 # TODO: Argument 2 has incompatible type "Callable[[Any, Any, Any], ColumnOperators[Any]]"; expected "Callable[..., ClauseElement | FormulaItem]"  [arg-type]
-    #                 offset : offset + length - 1
-    #             ],
-    #         ),
-    #     ]
-    # ),
+    base.FuncArraySlice(
+        variants=[
+            # V(D.TRINO, sa.func.slice),  # Due to bug in sqlalchemy, pure slice doesn't work
+            V(
+                D.TRINO,
+                lambda array, start, offset: sa.func.filter(
+                    sa.func.slice(array, start, offset),
+                    sa.text("x -> true"),
+                ),
+            ),
+        ]
+    ),
     # startswith
-    # base.FuncStartswithArrayConst(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda x, y, _env: Grouping(sa.type_coerce(x, sa_postgresql.ARRAY(TypeEngine)))[1 : len(un_literal(y))]  # type: ignore  # 2024-01-24 # TODO: Invalid index type "slice" for "Grouping"; expected type "int"  [index]
-    #             == y,
-    #         ),
-    #     ]
-    # ),
-    # base.FuncStartswithArrayNonConst(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda x, y, _env: Grouping(sa.type_coerce(x, sa_postgresql.ARRAY(TypeEngine)))[  # type: ignore  # 2024-01-24 # TODO: Invalid index type "slice" for "Grouping"; expected type "int"  [index]
-    #                 1 : sa.func.array_length(y, 1)
-    #             ]
-    #             == y,
-    #         ),
-    #     ]
-    # ),
+    base.FuncStartswithArrayConst(
+        variants=[
+            V(D.TRINO, array_startswith),
+        ]
+    ),
+    base.FuncStartswithArrayNonConst(
+        variants=[
+            V(D.TRINO, array_startswith),
+        ]
+    ),
     # unnest
     # base.FuncUnnestArrayFloat(
     #     variants=[
@@ -343,14 +387,9 @@ DEFINITIONS_ARRAY = [
     #     ]
     # ),
     # intersect
-    # base.FuncArrayIntersect(
-    #     variants=[
-    #         V(
-    #             D.TRINO,
-    #             lambda *arrays: sa.func.array(
-    #                 sa.intersect(*[sa.select(sa.func.unnest(arr)) for arr in arrays]).scalar_subquery()
-    #             ),
-    #         )
-    #     ]
-    # ),
+    base.FuncArrayIntersect(
+        variants=[
+            V(D.TRINO, array_intersect),
+        ]
+    ),
 ]
