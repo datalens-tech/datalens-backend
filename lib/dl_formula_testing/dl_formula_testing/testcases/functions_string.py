266ac176f7e6445eb72812de1e5c5e8c9842bf8b
khamitovdr <khamitovdr@yandex-team.ru>
2025-04-07 20:58:20 +0300

    for trino

--- lib/dl_formula_testing/dl_formula_testing/testcases/functions_string.py
+++ lib/dl_formula_testing/dl_formula_testing/testcases/functions_string.py
@@ -131,8 +131,8 @@ class DefaultStringFunctionFormulaConnectorTestSuite(FormulaConnectorTestBase):
         if not self.supports_regex_extract_all:
             pytest.skip()
         assert dbe.eval("REGEXP_EXTRACT_ALL('100-200, 300-400', '(\\d+)-(\\d+)')") in ("['100','300']", ["100", "300"])
-        assert dbe.eval("REGEXP_EXTRACT_ALL('нет_цифр', '(\\d+)-(\\d+)')") in ("[]", None)
-        assert to_str(dbe.eval('REGEXP_EXTRACT_ALL([str_null_value], "or..")', from_=data_table)) in (None, "[]")
+        assert dbe.eval("REGEXP_EXTRACT_ALL('нет_цифр', '(\\d+)-(\\d+)')") in ("[]", [], None)
+        assert to_str(dbe.eval('REGEXP_EXTRACT_ALL([str_null_value], "or..")', from_=data_table)) in (None, [], "[]")
         assert dbe.eval("REGEXP_EXTRACT_ALL('1а2б3в4', '\\d+([а-я]*)\\d+')") in ("['а','в']", ["а", "в"])
 
     def test_regexp_extract_nth(self, dbe: DbEvaluator, data_table: sa.Table) -> None:
