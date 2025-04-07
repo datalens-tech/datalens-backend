56cbd9bf102ca7467047c8d7f4ce544466b84ef1
juliarbkv <juliarbkv@yandex-team.ru>
2025-04-07 21:09:52 +0300

    typing

--- lib/dl_connector_clickhouse/dl_connector_clickhouse/core/clickhouse/storage_schemas/connection.py
+++ lib/dl_connector_clickhouse/dl_connector_clickhouse/core/clickhouse/storage_schemas/connection.py
@@ -4,9 +4,12 @@ from dl_connector_clickhouse.core.clickhouse.us_connection import ConnectionClic
 from dl_connector_clickhouse.core.clickhouse_base.storage_schemas.connection import (
     ConnectionClickHouseBaseDataStorageSchema,
 )
+from dl_connector_clickhouse.core.clickhouse_base.us_connection import ConnectionClickhouseBase
 
 
-class ConnectionClickhouseDataStorageSchema(ConnectionClickHouseBaseDataStorageSchema[ConnectionClickhouse.DataModel]):
+class ConnectionClickhouseDataStorageSchema(
+    ConnectionClickHouseBaseDataStorageSchema[ConnectionClickhouseBase.DataModel]
+):
     TARGET_CLS = ConnectionClickhouse.DataModel
 
     readonly = ma_fields.Integer(required=False, load_default=2, dump_default=2)
