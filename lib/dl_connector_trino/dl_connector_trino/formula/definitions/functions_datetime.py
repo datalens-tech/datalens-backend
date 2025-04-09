1dbf8d822e6e92f1a74e326fafd5fe4eacd3c064
khamitovdr <khamitovdr@yandex-team.ru>
2025-04-09 16:52:24 +0300

    now, today

--- lib/dl_connector_trino/dl_connector_trino/formula/definitions/functions_datetime.py
+++ lib/dl_connector_trino/dl_connector_trino/formula/definitions/functions_datetime.py
@@ -40,7 +40,11 @@ DEFINITIONS_DATETIME = [
     # base.FuncDayofweek2.for_dialect(D.TRINO),
     # base.FuncDayofweek2TZ.for_dialect(D.TRINO),
     # genericnow
-    # base.FuncGenericNow.for_dialect(D.TRINO),
+    base.FuncGenericNow(
+        variants=[
+            V(D.TRINO, sa.func.current_timestamp),
+        ]
+    ),
     # hour
     # base.FuncHourDate.for_dialect(D.TRINO),
     # base.FuncHourDatetime.for_dialect(D.TRINO),
@@ -53,7 +57,11 @@ DEFINITIONS_DATETIME = [
     # base.FuncMonth.for_dialect(D.TRINO),
     # base.FuncMonthDatetimeTZ.for_dialect(D.TRINO),
     # now
-    # base.FuncNow.for_dialect(D.TRINO),
+    base.FuncNow(
+        variants=[
+            V(D.TRINO, sa.func.current_timestamp),
+        ]
+    ),
     # quarter
     # base.FuncQuarter.for_dialect(D.TRINO),
     # base.FuncQuarterDatetimeTZ.for_dialect(D.TRINO),
@@ -62,7 +70,11 @@ DEFINITIONS_DATETIME = [
     # base.FuncSecondDatetime.for_dialect(D.TRINO),
     # base.FuncSecondDatetimeTZ.for_dialect(D.TRINO),
     # today
-    # base.FuncToday.for_dialect(D.TRINO),
+    base.FuncToday(
+        variants=[
+            V(D.TRINO, sa.func.current_date),
+        ]
+    ),
     # week
     # base.FuncWeek.for_dialect(D.TRINO),
     # base.FuncWeekTZ.for_dialect(D.TRINO),
