f186f3a335a4869f5e3a331d827bbc26822bf04c
khamitovdr <khamitovdr@yandex-team.ru>
2025-04-11 12:59:29 +0300

    fix datetime handling

--- lib/dl_connector_trino/dl_connector_trino/formula/literal.py
+++ lib/dl_connector_trino/dl_connector_trino/formula/literal.py
@@ -5,7 +5,6 @@ from typing import (
 )
 
 import sqlalchemy as sa
-import trino.sqlalchemy.datatype as tsa
 
 from dl_formula.connectors.base.literal import (
     Literal,
@@ -18,21 +17,33 @@ from dl_connector_trino.formula.definitions.custom_constructors import TrinoArra
 
 class TrinoLiteralizer(Literalizer):
     def literal_datetime(self, value: datetime.datetime, dialect: DialectCombo) -> Literal:
-        precision = 6 if value.microsecond else None
-        has_tz = value.tzinfo is not None
+        datetime_repr = value.strftime("%Y-%m-%d %H:%M:%S")
+        if value.microsecond:
+            datetime_repr += f".{value.microsecond:06}"
 
-        if has_tz and not isinstance(value.tzinfo, datetime.timezone):
-            utc_offset = value.utcoffset()
-            assert utc_offset is not None
-            value = value.replace(tzinfo=datetime.timezone(utc_offset))
+        if value.tzinfo is None:
+            return sa.text(f"TIMESTAMP '{datetime_repr}'")
 
-        return sa.cast(
-            value,
-            tsa.TIMESTAMP(
-                timezone=has_tz,
-                precision=precision,
-            ),
-        )
+        if value.tzinfo == datetime.timezone.utc:
+            timezone_repr = "UTC"
+
+        elif hasattr(value.tzinfo, "zone"):
+            # This is a pytz timezone object
+            timezone_repr = value.tzinfo.zone
+
+        elif isinstance(value.tzinfo, datetime.timezone):
+            # Calculate the offset in hours and minutes
+            total_seconds = int(value.tzinfo.utcoffset(value).total_seconds())
+            sign = "+" if total_seconds >= 0 else "-"
+            total_seconds = abs(total_seconds)
+            hours, remainder = divmod(total_seconds, 3600)
+            minutes = remainder // 60
+            timezone_repr = f"{sign}{hours:02}:{minutes:02}"
+
+        else:
+            raise TypeError(f"Unsupported tzinfo type: {type(value.tzinfo)}")
+
+        return sa.text(f"TIMESTAMP '{datetime_repr} {timezone_repr}'")
 
     def literal_array(self, value: Union[tuple, list], dialect: DialectCombo) -> Literal:
         return cast(Literal, TrinoArray(*value))
