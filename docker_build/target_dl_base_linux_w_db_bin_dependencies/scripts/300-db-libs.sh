400cd0deab05370f86e7b5373a03900e1fdfdeab
KonstantAnxiety <konstasa@yandex-team.ru>
2025-04-15 22:50:43 +0300

    remove comment

--- docker_build/target_dl_base_linux_w_db_bin_dependencies/scripts/300-db-libs.sh
+++ docker_build/target_dl_base_linux_w_db_bin_dependencies/scripts/300-db-libs.sh
@@ -3,7 +3,6 @@ set -eu
 export DEBIAN_FRONTEND=noninteractive
 
 # For MSSQL
-# temporarily pinned (see https://github.com/datalens-tech/datalens-backend/issues/201)
 apt-get install --yes --fix-missing \
     freetds-dev \
     tdsodbc \
