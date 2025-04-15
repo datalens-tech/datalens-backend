9bb74bf78d35be3d072f34ec70549598bc81d859
KonstantAnxiety <konstasa@yandex-team.ru>
2025-04-15 22:11:42 +0300

    fix: BI-0 unpin mssql binary dependencies

--- docker_build/target_dl_base_linux_w_db_bin_dependencies/scripts/300-db-libs.sh
+++ docker_build/target_dl_base_linux_w_db_bin_dependencies/scripts/300-db-libs.sh
@@ -5,10 +5,8 @@ export DEBIAN_FRONTEND=noninteractive
 # For MSSQL
 # temporarily pinned (see https://github.com/datalens-tech/datalens-backend/issues/201)
 apt-get install --yes --fix-missing \
-    libct4=1.1.6-1.1 \
-    libsybdb5=1.1.6-1.1 \
-    freetds-dev=1.1.6-1.1 \
-    tdsodbc=1.1.6-1.1 \
+    freetds-dev \
+    tdsodbc \
     unixodbc  \
     unixodbc-dev  \
     libtcmalloc-minimal4
