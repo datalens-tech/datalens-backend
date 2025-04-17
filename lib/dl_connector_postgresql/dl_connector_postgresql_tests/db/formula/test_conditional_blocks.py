2bb458b3beb43cdf8b667e81d36037b02d70e2a6
Valeria Bulanova <bulanovavv@yandex-team.ru>
2025-04-17 10:24:14 +0300

    test

--- lib/dl_connector_postgresql/dl_connector_postgresql_tests/db/formula/test_conditional_blocks.py
+++ lib/dl_connector_postgresql/dl_connector_postgresql_tests/db/formula/test_conditional_blocks.py
@@ -4,10 +4,17 @@ from dl_connector_postgresql_tests.db.formula.base import (
     PostgreSQL_9_3TestBase,
     PostgreSQL_9_4TestBase,
 )
+from dl_testing.regulated_test import RegulatedTestParams
 
 
 class TestConditionalBlockPostgreSQL_9_3(PostgreSQL_9_3TestBase, DefaultConditionalBlockFormulaConnectorTestSuite):
-    pass
+     test_params = RegulatedTestParams(
+        mark_tests_skipped={
+            DefaultConditionalBlockFormulaConnectorTestSuite.test_if_block: "lalal",
+        }
+    )
+    # pass
+
 
 
 class TestConditionalBlockPostgreSQL_9_4(PostgreSQL_9_4TestBase, DefaultConditionalBlockFormulaConnectorTestSuite):
