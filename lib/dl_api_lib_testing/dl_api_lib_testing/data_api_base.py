1d972af9011e1eb4dc193ca277638b56e7ea20d7
khamitovdr <khamitovdr@yandex-team.ru>
2025-04-29 14:32:09 +0300

    Default fixture

--- lib/dl_api_lib_testing/dl_api_lib_testing/data_api_base.py
+++ lib/dl_api_lib_testing/dl_api_lib_testing/data_api_base.py
@@ -160,6 +160,10 @@ class StandardizedDataApiTestBase(DataApiTestBase, DatasetTestBase, metaclass=ab
             date_field="order_date",
         )
 
+    @pytest.fixture(scope="class")
+    def table_schema_name(self) -> Optional[str]:
+        return None
+
     def get_dataset_params(self, dataset_params: dict, db_table: DbTable) -> dict:
         return dataset_params | dict(
             parameters=dataset_params.get("parameters", {})
