9023959fa78cbe5f69094fafd66d9ccfd513e90d
KonstantAnxiety <konstasa@yandex-team.ru>
2025-05-07 15:32:26 +0300

    fix: BI-6252 add post_dump to file source schemas to handle special fields (#1023)
    * fix: BI-6252 add post_dump to file source schemas to handle special fields
    
    * cq

--- lib/dl_connector_bundle_chs3/dl_connector_bundle_chs3/chs3_base/api/api_schema/source.py
+++ lib/dl_connector_bundle_chs3/dl_connector_bundle_chs3/chs3_base/api/api_schema/source.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+from copy import deepcopy
 from typing import (
     Any,
     ClassVar,
+    Iterable,
     Optional,
     Type,
     final,
@@ -13,6 +15,7 @@ from marshmallow import (
     RAISE,
     Schema,
     fields,
+    post_dump,
     post_load,
     pre_load,
 )
@@ -24,6 +27,7 @@ from dl_api_connector.api_schema.source_base import (
 )
 from dl_constants.enums import (
     CreateMode,
+    ExportMode,
     FileProcessingStatus,
     ImportMode,
     UserDataType,
@@ -47,13 +51,24 @@ class BaseFileSourceSchema(Schema):
 
         target: Type[BaseFileS3Connection.FileDataSource]
 
-    @post_load(pass_many=False)
-    def post_load(self, data: dict[str, Any], **kwargs: Any) -> BaseFileS3Connection.FileDataSource:
-        return self.Meta.target(**data)
-
     @property
     def operations_mode(self) -> Optional[CreateMode | ImportMode]:
         return self.context.get(self.CTX_KEY_OPERATIONS_MODE)
+
+    @staticmethod
+    def get_field_extra(f: fields.Field) -> Optional[FieldExtra]:
+        return f.metadata.get("bi_extra", None)
+
+    @classmethod
+    def all_fields_dict(cls) -> dict[str, fields.Field]:
+        return cls._declared_fields
+
+    @classmethod
+    def fieldnames_with_extra_export_fake_info(cls) -> Iterable[str]:
+        for field_name, field in cls.all_fields_dict().items():
+            extra = cls.get_field_extra(field)
+            if extra is not None and extra.export_fake is True:
+                yield field_name
 
     @final
     def delete_unknown_fields(self, data: dict[str, Any]) -> dict[str, Any]:
@@ -64,14 +79,27 @@ class BaseFileSourceSchema(Schema):
 
         return cleaned_data
 
+    @post_load(pass_many=False)
+    def post_load(self, data: dict[str, Any], **kwargs: Any) -> BaseFileS3Connection.FileDataSource:
+        return self.Meta.target(**data)
+
     @pre_load(pass_many=False)
     def pre_load(self, data: dict[str, Any], **_: Any) -> dict[str, Any]:
         if isinstance(self.operations_mode, ImportMode):
             return self.delete_unknown_fields(data)
         return data
 
+    @post_dump(pass_many=False)
+    def post_dump(self, data: dict[str, Any], **_: Any) -> dict[str, Any]:
+        if isinstance(self.operations_mode, ExportMode):
+            data = deepcopy(data)
+            for secret_field in self.fieldnames_with_extra_export_fake_info():
+                data[secret_field] = "******"
+            return self.delete_unknown_fields(data)
+        return data
+
     id = fields.String()
-    file_id = fields.String(load_default=None)
+    file_id = fields.String(load_default=None, bi_extra=FieldExtra(export_fake=True))
     title = fields.String(bi_extra=FieldExtra(editable=True))
     status = fields.Enum(FileProcessingStatus, dump_only=True)
     raw_schema = fields.Nested(
