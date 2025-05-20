9b4974554315928e19b78d90ad2cf829394829e0
khamitovdr <khamitovdr@yandex-team.ru>
2025-05-20 16:58:33 +0300

    enable unit tests for connection form

--- lib/dl_connector_trino/dl_connector_trino_tests/unit/test_connection_form.py
+++ lib/dl_connector_trino/dl_connector_trino_tests/unit/test_connection_form.py
@@ -8,4 +8,3 @@ from dl_connector_trino.api.i18n.localizer import CONFIGS as BI_CONNECTOR_TRINO_
 class TestTrinoConnectionForm(ConnectionFormTestBase):
     CONN_FORM_FACTORY_CLS = TrinoConnectionFormFactory
     TRANSLATION_CONFIGS = BI_API_CONNECTOR_CONFIGS + BI_CONNECTOR_TRINO_CONFIGS
-    OVERWRITE_EXPECTED_FORMS = True
