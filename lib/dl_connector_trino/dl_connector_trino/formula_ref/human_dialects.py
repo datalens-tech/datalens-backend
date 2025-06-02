70ca9c1890f224ea92942e0b6548f89efae12066
khamitovdr <khamitovdr@yandex-team.ru>
2025-06-02 22:02:47 +0300

    formula_ref

---
+++ lib/dl_connector_trino/dl_connector_trino/formula_ref/human_dialects.py
@@ -0,0 +1,12 @@
+from dl_formula_ref.texts import StyledDialect
+
+from dl_connector_trino.formula.constants import TrinoDialect
+
+
+HUMAN_DIALECTS = {
+    TrinoDialect.TRINO: StyledDialect(
+        "`Trino`",
+        "`Trino`",
+        "`Trino`",
+    ),
+}
