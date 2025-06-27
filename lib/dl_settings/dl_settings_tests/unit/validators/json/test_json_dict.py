5afc36ba3c211061bd966ca8767d2b21540c013d
ovsds <ovsds@yandex-team.ru>
2025-06-27 10:35:39 +0200

    feat: BI-0 dl-settings extend validators

--- lib/dl_settings/dl_settings_tests/unit/validators/json/test_json_dict.py
+++ lib/dl_settings/dl_settings_tests/unit/validators/json/test_json_dict.py
@@ -11,32 +11,28 @@ T = typing.TypeVar("T")
 @pytest.mark.parametrize(
     "input, expected",
     [
+        (None, None),
+        ({"key": "value"}, {"key": "value"}),
         ("{}", {}),
         ('{"key": "value"}', {"key": "value"}),
         ('{"key": "value", "key2": "value2"}', {"key": "value", "key2": "value2"}),
     ],
+    ids=[
+        "none",
+        "dict",
+        "empty",
+        "one_key",
+        "two_keys",
+    ],
 )
-def test_function(
-    input: str,
+def test_default(
+    input: typing.Any,
     expected: dict[str, str],
 ) -> None:
     assert dl_settings.parse_json_dict(input) == expected
 
-
-@pytest.mark.parametrize(
-    "input, expected",
-    [
-        ("{}", {}),
-        ('{"key": "value"}', {"key": "value"}),
-        ('{"key": "value", "key2": "value2"}', {"key": "value", "key2": "value2"}),
-    ],
-)
-def test_validator(
-    input: str,
-    expected: dict[str, str],
-) -> None:
     class TestModel(dl_settings.BaseSettings):
-        value: typing.Annotated[dict[str, str], dl_settings.json_dict_validator]
+        value: typing.Annotated[dict[str, str] | None, dl_settings.json_dict_validator]
 
-    model = TestModel(value=input)  # type: ignore[arg-type]
+    model = TestModel(value=input)
     assert model.value == expected
