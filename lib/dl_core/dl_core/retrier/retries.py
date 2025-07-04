3ee0c516a0d5761d735816461a52c3f4eae4d54c
catsona <catsona@x96.dev>
2025-07-04 14:43:36 +0300

    tests for retrier

--- lib/dl_core/dl_core/retrier/retries.py
+++ lib/dl_core/dl_core/retrier/retries.py
@@ -17,12 +17,14 @@ def iter_retries(retry_policy: RetryPolicy) -> Iterator[Retry]:
     start_dt = datetime.datetime.now()
     end_dt = start_dt + datetime.timedelta(seconds=retry_policy.total_timeout)
 
-    for idx in range(retry_policy.retries_count):
+    for idx in range(retry_policy.retries_count + 1):  # First Retry is not a retry
         sleep_before_seconds = 0 if idx == 0 else retry_policy.get_backoff_at(idx - 1)
         total_timeout_remaining = (end_dt - datetime.datetime.now()).total_seconds()
 
         if sleep_before_seconds > total_timeout_remaining:
             break
+
+        total_timeout_remaining -= sleep_before_seconds
 
         yield Retry(
             request_timeout=min(retry_policy.request_timeout, total_timeout_remaining),
