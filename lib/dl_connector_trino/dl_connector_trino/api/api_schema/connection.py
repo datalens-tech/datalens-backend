4e7824cabda210df1df040139a2324a0eef123d5
khamitovdr <khamitovdr@yandex-team.ru>
2025-07-07 19:11:32 +0300

    code style

--- lib/dl_connector_trino/dl_connector_trino/api/api_schema/connection.py
+++ lib/dl_connector_trino/dl_connector_trino/api/api_schema/connection.py
@@ -1,4 +1,3 @@
-
 from dl_api_connector.api_schema.connection_base import ConnectionMetaMixin
 from dl_api_connector.api_schema.connection_base_fields import secret_string_field
 from dl_api_connector.api_schema.connection_mixins import (
