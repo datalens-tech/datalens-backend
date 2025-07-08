791e178e51e70dcc5a572de6ffa517e1deed979d
Sergei Borodin <seray@yandex-team.ru>
2025-07-08 15:06:35 +0200

    fix: BI-6398 get appropriate architecture for docker deps (#1127)

--- docker_build/debian_docker/scripts/100-docker.sh
+++ docker_build/debian_docker/scripts/100-docker.sh
@@ -13,7 +13,7 @@ apt-get install --yes \
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
-echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian/ bookworm stable" > /etc/apt/sources.list.d/docker.list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian/ bookworm stable" > /etc/apt/sources.list.d/docker.list
 
 apt-get update
 
