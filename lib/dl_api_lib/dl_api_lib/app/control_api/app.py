c0a3202bb518f110cc9888cc49c145d2d8dcb037
tigbarlogyan <tigranabov@yandex-team.ru>
2025-07-10 15:06:07 +0300

    Fix lint issues

--- lib/dl_api_lib/dl_api_lib/app/control_api/app.py
+++ lib/dl_api_lib/dl_api_lib/app/control_api/app.py
@@ -12,10 +12,6 @@ import attr
 import flask
 from flask import Flask
 from flask_marshmallow import Marshmallow
-#from statcommons.unistat.flask import (
-#    register_metrics,
-#    register_unistat_hax,
-#)
 
 from dl_api_commons.flask.middlewares.aio_event_loop_middleware import AIOEventLoopMiddleware
 from dl_api_commons.flask.middlewares.commit_rci_middleware import ReqCtxInfoMiddleware
@@ -26,7 +22,8 @@ from dl_api_commons.flask.middlewares.tracing import (
     TracingContextMiddleware,
     TracingMiddleware,
 )
-#from dl_api_commons.flask.ping import register_ping_handler_hax
+
+# from dl_api_commons.flask.ping import register_ping_handler_hax
 from dl_api_lib.app.control_api.resources import init_apis
 from dl_api_lib.app_common import SRFactoryBuilder
 from dl_api_lib.app_common_settings import ConnOptionsMutatorsFactory
@@ -43,6 +40,12 @@ from dl_constants.enums import (
 from dl_core import profiling_middleware
 from dl_core.flask_utils.services_registry_middleware import ServicesRegistryMiddleware
 from dl_core.flask_utils.us_manager_middleware import USManagerFlaskMiddleware
+
+
+# from statcommons.unistat.flask import (
+#    register_metrics,
+#    register_unistat_hax,
+# )
 
 
 if TYPE_CHECKING:
@@ -125,9 +128,9 @@ class ControlApiAppFactory(SRFactoryBuilder, Generic[TControlApiAppSettings], ab
 
         # HAX to avoid bumping into inappropriate authorization added in 'before_request'.
         # TODO: move the inappropriate authorization to common handler wrap.
-        #register_ping_handler_hax(app)
-        #register_unistat_hax(app)
-        #register_metrics(app)
+        # register_ping_handler_hax(app)
+        # register_unistat_hax(app)
+        # register_metrics(app)
 
         env_setup_result = self.set_up_environment(app=app, testing_app_settings=testing_app_settings)
 
