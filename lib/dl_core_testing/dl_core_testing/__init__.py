2f58717133eda4bb95bc4d962979813b179f2026
ovsds <ovsds@yandex-team.ru>
2025-07-10 13:13:38 +0200

    test: BI-0 fix asserts in common tests

--- lib/dl_core_testing/dl_core_testing/__init__.py
+++ lib/dl_core_testing/dl_core_testing/__init__.py
@@ -1,3 +1,10 @@
 """
 Helper functions and classes for usage in tests of all dependent projects
 """
+
+import pathlib
+
+import dl_testing
+
+
+dl_testing.register_all_assert_rewrites(__name__, pathlib.Path(__file__).parent)
