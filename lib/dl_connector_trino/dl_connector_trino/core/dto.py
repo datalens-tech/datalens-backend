33e8e76e9f82725a5776e8f1ae6c375608246747
ovsds <ovsds@yandex-team.ru>
2025-07-11 13:36:42 +0200

    refactor: BI-0 TrinoConnDTO

--- lib/dl_connector_trino/dl_connector_trino/core/dto.py
+++ lib/dl_connector_trino/dl_connector_trino/core/dto.py
@@ -12,8 +12,8 @@ from dl_connector_trino.core.constants import (
 class TrinoConnDTOBase(ConnDTO):
     conn_type = CONNECTION_TYPE_TRINO
     host: str = attr.ib()
-    port: int | None = attr.ib(default=None)
-    username: str | None = attr.ib(default=None)
+    port: int = attr.ib()
+    username: str = attr.ib()
     auth_type: TrinoAuthType = attr.ib()
     password: str | None = attr.ib(repr=False, default=None)
     jwt: str | None = attr.ib(repr=False, default=None)
@@ -30,5 +30,4 @@ class TrinoConnDTOBase(ConnDTO):
 
 @attr.s(frozen=True, kw_only=True)
 class TrinoConnDTO(TrinoConnDTOBase):
-    port: int = attr.ib()
-    username: str = attr.ib()
+    pass
