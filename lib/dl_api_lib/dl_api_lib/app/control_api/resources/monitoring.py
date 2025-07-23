06466721bd54863a2b565da0595a63ebb5dfac08
tigbarlogyan <tigranabov@yandex-team.ru>
2025-07-23 18:54:00 +0300

    Fix /metrics handler and retun base DB exception

--- lib/dl_api_lib/dl_api_lib/app/control_api/resources/monitoring.py
+++ lib/dl_api_lib/dl_api_lib/app/control_api/resources/monitoring.py
@@ -26,7 +26,6 @@ from dl_api_commons.flask.required_resources import RequiredResourceCommon
 from dl_api_lib.app.control_api.resources import API
 from dl_api_lib.app.control_api.resources.base import BIResource
 from dl_constants.exc import DLBaseException
-from dl_core.exc import DatabaseUnavailable
 
 
 LOGGER = logging.getLogger(__name__)
@@ -39,7 +38,7 @@ def _handle_monitoring_exc(exception: Exception) -> NoReturn:
         raise exception
     else:
         LOGGER.exception("Got unhandled monitoring exception")
-        raise DatabaseUnavailable("Service monitoring failed") from exception
+        raise DLBaseException("Service monitoring failed") from exception
 
 
 class MonitoringResourceBase(BIResource):
@@ -76,10 +75,7 @@ class MetricsResource(MonitoringResourceBase):
     def get(self) -> Any:
         """Prometheus metrics endpoint"""
         try:
-            data = []
-            if os.environ.get("UWSGI_STATS") and os.environ.get("QLOUD_APPLICATION"):
-                data = uwsgi_prometheus()
-
+            data = uwsgi_prometheus()
             body = "".join(dump_for_prometheus(data))
             return make_response(body)
         except Exception as e:
