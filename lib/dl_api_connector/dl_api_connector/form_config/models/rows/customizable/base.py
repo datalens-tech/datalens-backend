091dbd97b92e3988e6913f7b778f73cb4870d4a9
khamitovdr <khamitovdr@yandex-team.ru>
2025-08-19 13:58:07 +0300

    remove skip_if_null logic

--- lib/dl_api_connector/dl_api_connector/form_config/models/rows/customizable/base.py
+++ lib/dl_api_connector/dl_api_connector/form_config/models/rows/customizable/base.py
@@ -9,8 +9,7 @@ import attr
 from dl_api_connector.form_config.models.common import (
     MarkdownStr,
     SerializableConfig,
-    remap_skip_if_null,
-    skip_if_null,
+    remap,
 )
 from dl_api_connector.form_config.models.rows.base import (
     DisplayConditionsMixin,
@@ -34,19 +33,19 @@ class RowItem(SerializableConfig):
 
 @attr.s(kw_only=True, frozen=True)
 class PlaceholderMixin(SerializableConfig):
-    placeholder: Optional[str] = attr.ib(default=None, metadata=skip_if_null())
+    placeholder: Optional[str] = attr.ib(default=None)
 
 
 @attr.s(kw_only=True, frozen=True)
 class DefaultValueMixin(SerializableConfig):
     # comma separated if multiple=true
-    default_value: Optional[str | bool] = attr.ib(default=None, metadata=remap_skip_if_null("defaultValue"))
+    default_value: Optional[str | bool] = attr.ib(default=None, metadata=remap("defaultValue"))
 
 
 @attr.s(kw_only=True, frozen=True)
 class ControlRowItem(RowItem, FormFieldMixin, DisplayConditionsMixin, InnerFieldMixin, DefaultValueMixin, WidthMixin):
-    fake_value: Optional[str] = attr.ib(default=None, metadata=remap_skip_if_null("fakeValue"))
-    hint_text: Optional[MarkdownStr] = attr.ib(default=None, metadata=remap_skip_if_null("hintText"))
+    fake_value: Optional[str] = attr.ib(default=None, metadata=remap("fakeValue"))
+    hint_text: Optional[MarkdownStr] = attr.ib(default=None, metadata=remap("hintText"))
 
 
 @attr.s(kw_only=True, frozen=True)
