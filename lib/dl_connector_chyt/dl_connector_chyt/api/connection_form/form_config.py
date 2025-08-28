47ba19a0d00c392038d9fe1f999db911f9049e62
Valeria Bulanova <bulanovavv@yandex-team.ru>
2025-08-28 15:27:16 +0300

    feat: BI-6287 Export's history link fix

--- lib/dl_connector_chyt/dl_connector_chyt/api/connection_form/form_config.py
+++ lib/dl_connector_chyt/dl_connector_chyt/api/connection_form/form_config.py
@@ -117,7 +117,7 @@ class CHYTConnectionFormFactory(ConnectionFormFactory):
                 rc.collapse_advanced_settings_row(),
                 rc.data_export_forbidden_row(
                     conn_id=self._get_form_params().conn_id,
-                    exports_history_url_path=None,
+                    exports_history_url_path=self._get_form_params().exports_history_url_path,
                     mode=self.mode,
                 ),
             ],
