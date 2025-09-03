a1084ee5d22c4a812b8812b7416966a52ca9c998
catsona <catsona@x96.dev>
2025-09-03 11:17:25 +0300

    fix: DLHELP-13888 Partial solution for excel parser OOM

--- lib/dl_file_secure_reader_lib/dl_file_secure_reader_lib/resources/reader.py
+++ lib/dl_file_secure_reader_lib/dl_file_secure_reader_lib/resources/reader.py
@@ -3,24 +3,52 @@ from __future__ import annotations
 import asyncio
 import io
 import logging
+import typing
 from typing import BinaryIO
 
 from aiohttp import web
 from aiohttp.multipart import BodyPartReader
-from openpyxl import load_workbook
+import openpyxl
+import openpyxl.cell
+import openpyxl.worksheet
 
 
 LOGGER = logging.getLogger(__name__)
 
 
+def custom_get_cell(
+    sheet: openpyxl.worksheet.Worksheet,
+) -> typing.Callable[[typing.Any, typing.Any], openpyxl.cell.Cell | None]:
+    def get_cell(row: typing.Any, column: typing.Any) -> openpyxl.cell.Cell | None:
+        """
+        Custom _get_cell implementation without saving wrappers for null to sheet.
+        Motivation: when null (empty) value is read from sheet in original function,
+        it is saved to sheet cells duting read iteration over rows. This causes
+        excessive memory usage for sheets with many empty cells.
+
+        Tested on openpyxl==3.0.10.
+        """
+        if not 0 < row < 1048577:
+            raise ValueError(f"Row numbers must be between 1 and 1048576. Row number supplied was {row}")
+        coordinate = (row, column)
+        if not coordinate in sheet._cells:
+            return openpyxl.cell.Cell(sheet, row=row, column=column)
+
+        return sheet._cells[coordinate]
+
+    return get_cell
+
+
 def parse_excel_data(data: BinaryIO) -> list:
     result = []
     try:
-        wb = load_workbook(data, data_only=True, keep_links=False)
+        wb = openpyxl.load_workbook(data, data_only=True, keep_links=False)
     except Exception:
         raise web.HTTPUnprocessableEntity(reason="Invalid excel file")
     for sheetname in wb.sheetnames:
         sheet = wb[sheetname]
+        sheet._get_cell = custom_get_cell(sheet)
+
         result.append(
             {
                 "sheetname": sheetname,
