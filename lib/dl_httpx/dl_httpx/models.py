5296d754ed5c06ded5cd523d9b002ffb17db933f
Dmitrii Ovsyannikov <ovsds@yandex-team.ru>
2025-09-16 14:43:51 +0200

    feat: BI-6557 add dl_httpx.BaseRequest (#1230)

--- lib/dl_httpx/dl_httpx/models.py
+++ lib/dl_httpx/dl_httpx/models.py
@@ -1,4 +1,27 @@
+from typing import Any
+
+import attrs
+
 import dl_pydantic
+
+
+@attrs.define(kw_only=True, frozen=True)
+class BaseRequest:
+    @property
+    def path(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def method(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def query_params(self) -> dict[str, str]:
+        return {}
+
+    @property
+    def body(self) -> dict[str, Any] | None:
+        return None
 
 
 class BaseResponseModel(dl_pydantic.BaseModel):
@@ -7,4 +30,5 @@ class BaseResponseModel(dl_pydantic.BaseModel):
 
 __all__ = [
     "BaseResponseModel",
+    "BaseRequest",
 ]
