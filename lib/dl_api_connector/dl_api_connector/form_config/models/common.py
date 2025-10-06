073a5563bdab16d9cbc528b3ff2eb709adee7c85
Dmitrii Ovsyannikov <tamatuk@ya.ru>
2025-10-06 15:16:06 +0200

    Revert "feat: BI-6604 Add hidden `description` field to all forms (#1258)"
    This reverts commit 9a3b8515160b3c12a5b5723c9058865563da4358.

--- lib/dl_api_connector/dl_api_connector/form_config/models/common.py
+++ lib/dl_api_connector/dl_api_connector/form_config/models/common.py
@@ -71,11 +71,6 @@ class TopLevelFieldName(Enum):
 
 
 @unique
-class AnnotationFieldName(FormFieldName):
-    description = "description"
-
-
-@unique
 class InnerFieldName(Enum):
     """
     Prepared rows/components can have inner fields that can' be controlled by the config,
