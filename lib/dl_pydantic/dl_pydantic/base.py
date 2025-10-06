b5185d46402e35a657cf78b6b8ba6daefcfae8c5
Dmitrii Ovsyannikov <ovsds@yandex-team.ru>
2025-10-06 15:00:04 +0200

    feat: BI-6610 add dl_pydantic model diff (#1265)

--- lib/dl_pydantic/dl_pydantic/base.py
+++ lib/dl_pydantic/dl_pydantic/base.py
@@ -1,10 +1,22 @@
+import deepdiff
 import pydantic
+from typing_extensions import Self
 
 
 class BaseModel(pydantic.BaseModel):
     model_config = pydantic.ConfigDict(
         populate_by_name=True,
     )
+
+    def model_deepdiff(self, other: Self, exclude_paths: list[str] | None = None) -> deepdiff.DeepDiff:
+        exclude_paths = exclude_paths or []
+
+        return deepdiff.DeepDiff(
+            self,
+            other,
+            ignore_order=True,
+            exclude_paths=exclude_paths,
+        )
 
 
 __all__ = [
