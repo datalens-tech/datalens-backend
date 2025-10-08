0aa8a88797975b6a5f7fa3ce910bf07323d40501
juliarbkv <juliarbkv@yandex-team.ru>
2025-10-09 01:23:16 +0300

    fix draft test

--- lib/dl_connector_trino/dl_connector_trino_tests/db/api/test_connection.py
+++ lib/dl_connector_trino/dl_connector_trino_tests/db/api/test_connection.py
@@ -6,20 +6,20 @@ from dl_api_lib_testing.connector.connection_suite import DefaultConnectorConnec
 from dl_connector_trino_tests.db.api.base import TrinoConnectionTestBase
 
 
-class TestTrinoConnection(TrinoConnectionTestBase, DefaultConnectorConnectionTestSuite):  # type: ignore
+class TestTrinoConnection(TrinoConnectionTestBase, DefaultConnectorConnectionTestSuite):
     @pytest.mark.parametrize(
-        "args,expected_status,expected_data",
+        "args,expected_status",
         [
-            ("", 200, ""),
-            ("?search_text=ampl&db_name=test_memory_catalog", 200, ""),
-            ("?limit=1&db_name=test_memory_catalog", 200, ""),
-            ("?offset=1&db_name=test_memory_catalog", 200, ""),
-            ("?search_text=ampl&limit=1&offset=1&db_name=test_memory_catalog", 200, ""),
-            ("?search_text=ampl", 400, None),
-            ("?limit=1", 200, ""),
-            ("?offset=1", 400, None),
-            ("?offset=0", 200, ""),
-            ("?search_text=ampl&limit=1&offset=1", 400, None),
+            ("", 200),
+            ("?search_text=ampl&db_name=test_memory_catalog", 200),
+            ("?limit=1&db_name=test_memory_catalog", 200),
+            ("?offset=1&db_name=test_memory_catalog", 200),
+            ("?search_text=ampl&limit=1&offset=0&db_name=test_memory_catalog", 200),
+            ("?search_text=ampl", 400),
+            ("?limit=1", 200),
+            ("?offset=1", 400),
+            ("?offset=0", 200),
+            ("?search_text=ampl&limit=1&offset=1", 400),
         ],
     )
     def test_connection_sources_paginated_with_params(
@@ -29,7 +29,6 @@ class TestTrinoConnection(TrinoConnectionTestBase, DefaultConnectorConnectionTes
         bi_headers: dict[str, str] | None,
         args: str,
         expected_status: int,
-        expected_data: str | None,
     ) -> None:
         resp = control_api_sync_client.get(
             url=f"/api/v1/connections/{saved_connection_id}/info/sources{args}",
@@ -37,6 +36,10 @@ class TestTrinoConnection(TrinoConnectionTestBase, DefaultConnectorConnectionTes
         )
         assert resp.status_code == expected_status, resp.json
         resp_data = resp.json
-        assert "sources" in resp_data, resp_data
-        assert isinstance(resp_data["sources"], list), resp_data
-        assert resp_data == expected_data
+        if expected_status == 200:
+            assert "sources" in resp_data, resp_data
+            assert isinstance(resp_data["sources"], list), resp_data
+            assert resp_data
+        else:
+            assert resp_data.get("code") == "ERR.DS_API.INVALID_REQUEST"
+            assert resp_data.get("message")
