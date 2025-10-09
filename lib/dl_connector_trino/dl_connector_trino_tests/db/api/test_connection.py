c1191156513876e755a8d9752982bac2f9eaf56b
juliarbkv <juliarbkv@yandex-team.ru>
2025-10-09 13:00:10 +0300

    smol test fixes

--- lib/dl_connector_trino/dl_connector_trino_tests/db/api/test_connection.py
+++ lib/dl_connector_trino/dl_connector_trino_tests/db/api/test_connection.py
@@ -8,7 +8,7 @@ from dl_connector_trino_tests.db.api.base import TrinoConnectionTestBase
 
 class TestTrinoConnection(TrinoConnectionTestBase, DefaultConnectorConnectionTestSuite):
     @pytest.mark.parametrize(
-        "args,expected_status",
+        "search_text,limit,offset,db_name,expected_status_code",
         [
             ("", 200),
             ("?search_text=ampl&db_name=test_memory_catalog", 200),
@@ -28,18 +28,18 @@ class TestTrinoConnection(TrinoConnectionTestBase, DefaultConnectorConnectionTes
         saved_connection_id: str,
         bi_headers: dict[str, str] | None,
         args: str,
-        expected_status: int,
+        expected_status_code: int,
     ) -> None:
         resp = control_api_sync_client.get(
             url=f"/api/v1/connections/{saved_connection_id}/info/sources{args}",
             headers=bi_headers,
         )
-        assert resp.status_code == expected_status, resp.json
+        assert resp.status_code == expected_status_code, resp.json
         resp_data = resp.json
-        if expected_status == 200:
+        if expected_status_code == 200:
             assert "sources" in resp_data, resp_data
             assert isinstance(resp_data["sources"], list), resp_data
-            assert resp_data["sources"], resp_data["sources"]
+            assert resp_data["sources"], resp_data
         else:
             assert resp_data.get("code") == "ERR.DS_API.INVALID_REQUEST"
             assert resp_data.get("message")
