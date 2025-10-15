244221d0816d03ad1a042e82849f3485802128d2
juliarbkv <juliarbkv@yandex-team.ru>
2025-10-15 13:40:22 +0300

    rm unapplicable test

--- lib/dl_connector_greenplum/dl_connector_greenplum_tests/db/api/test_connection.py
+++ lib/dl_connector_greenplum/dl_connector_greenplum_tests/db/api/test_connection.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-import pytest
 
 from dl_api_client.dsmaker.api.http_sync_base import SyncHttpClientBase
 from dl_api_lib_testing.connector.connection_suite import DefaultConnectorConnectionTestSuite
@@ -33,40 +32,3 @@ class TestGreenplumSQLConnection(GreenplumConnectionTestBase, DefaultConnectorCo
         assert response.status_code == 200
         assert response.json["db_type"] == "greenplum"
         assert response.json["cache_ttl_sec"] is None
-
-    @pytest.mark.parametrize(
-        "args,expected_status_code",
-        [
-            ("", 200),
-            ("?search_text=ampl&db_name=test_memory_catalog", 400),
-            ("?limit=1&db_name=test_memory_catalog", 400),
-            ("?offset=1&db_name=test_memory_catalog", 400),
-            ("?search_text=ampl&limit=1&offset=0&db_name=test_memory_catalog", 400),
-            ("?search_text=ampl", 200),
-            ("?limit=1", 200),
-            ("?offset=1", 200),
-            ("?offset=0", 200),
-            ("?search_text=ampl&limit=1&offset=1", 200),
-        ],
-    )
-    def test_connection_sources_paginated_with_params(
-        self,
-        control_api_sync_client: SyncHttpClientBase,
-        saved_connection_id: str,
-        bi_headers: dict[str, str] | None,
-        args: str,
-        expected_status_code: int,
-    ) -> None:
-        resp = control_api_sync_client.get(
-            url=f"/api/v1/connections/{saved_connection_id}/info/sources{args}",
-            headers=bi_headers,
-        )
-        assert resp.status_code == expected_status_code, resp.json
-        resp_data = resp.json
-        if expected_status_code == 200:
-            assert "sources" in resp_data, resp_data
-            assert isinstance(resp_data["sources"], list), resp_data
-            assert resp_data["sources"], resp_data
-        else:
-            assert resp_data.get("code") == "ERR.DS_API.INVALID_REQUEST"
-            assert resp_data.get("message")
