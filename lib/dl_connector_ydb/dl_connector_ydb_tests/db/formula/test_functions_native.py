146ee3ec536d1e8130100e224c13d329a9792ce6
khamitovdr <khamitovdr@yandex-team.ru>
2025-10-22 15:32:33 +0300

    fix ydb

--- lib/dl_connector_ydb/dl_connector_ydb_tests/db/formula/test_functions_native.py
+++ lib/dl_connector_ydb/dl_connector_ydb_tests/db/formula/test_functions_native.py
@@ -11,32 +11,29 @@ class TestNativeFunctionYdb(
     DefaultNativeFunctionFormulaConnectorTestSuite,
 ):
     def test_native_functions(self, dbe: DbEvaluator) -> None:
-        # DB_CALL_INT
-        assert dbe.eval('DB_CALL_INT("ABS", -5)') == 5
-        assert dbe.eval('DB_CALL_INT("LENGTH", "hello")') == 5
+        # DB_CALL_INT - using YQL module syntax
+        assert dbe.eval('DB_CALL_INT("Math::Abs", -5)') == 5
+        assert dbe.eval('DB_CALL_INT("String::Length", "hello")') == 5
 
-        # DB_CALL_FLOAT
-        assert dbe.eval('DB_CALL_FLOAT("ABS", -5.0)') == 5.0
-        assert dbe.eval('DB_CALL_FLOAT("SQRT", 100.0)') == pytest.approx(10.0)
+        # DB_CALL_FLOAT - using YQL module syntax
+        assert dbe.eval('DB_CALL_FLOAT("Math::Abs", -5.0)') == pytest.approx(5.0)
+        assert dbe.eval('DB_CALL_FLOAT("Math::Floor", 5.7)') == pytest.approx(5.0)
 
-        # DB_CALL_STRING
-        assert dbe.eval('DB_CALL_STRING("UPPER", "hello")') == "HELLO"
+        # DB_CALL_STRING - using YQL module syntax
+        assert dbe.eval('DB_CALL_STRING("String::ToUpper", "hello")') == "HELLO"
 
-        # DB_CALL_BOOL
-        assert dbe.eval('DB_CALL_BOOL("COALESCE", True, False)') == True
+        # DB_CALL_BOOL - using YQL module syntax
+        assert dbe.eval('DB_CALL_BOOL("String::Contains", "hello world", "world")') == True
+        assert dbe.eval('DB_CALL_BOOL("String::StartsWith", "hello", "he")') == True
 
-        # DB_CALL_ARRAY_INT
-        res = dbe.eval('DB_CALL_ARRAY_INT("ListCreate", 1, 2, 3, 4, 5)')
-        assert isinstance(res, list)
-        assert len(res) >= 3
+        # DB_CALL_ARRAY_INT - using YQL list functions
+        assert dbe.eval('DB_CALL_ARRAY_INT("AsList", 1, 2, 3, 4, 5)') == [1, 2, 3, 4, 5]
 
-        # DB_CALL_ARRAY_FLOAT
-        res = dbe.eval('DB_CALL_ARRAY_FLOAT("ListCreate", 1.0, 2.0, 3.0)')
+        # DB_CALL_ARRAY_FLOAT - using YQL list functions
+        res = dbe.eval('DB_CALL_ARRAY_FLOAT("AsList", 1.0, 2.0, 3.0)')
         assert isinstance(res, list)
         assert len(res) == 3
         assert all(isinstance(x, float) for x in res)
 
-        # DB_CALL_ARRAY_STRING
-        res = dbe.eval('DB_CALL_ARRAY_STRING("ListCreate", "a", "b", "c")')
-        assert isinstance(res, list)
-        assert len(res) == 3
+        # DB_CALL_ARRAY_STRING - using YQL list functions
+        assert dbe.eval('DB_CALL_ARRAY_STRING("AsList", "a", "b", "c")') == ["a", "b", "c"]
