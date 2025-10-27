4c9a2b5984189ae402e68c3dd4afe932199754a6
khamitovdr <khamitovdr@yandex-team.ru>
2025-10-27 19:38:19 +0300

    fix ydb

--- lib/dl_connector_ydb/dl_connector_ydb_tests/db/formula/test_functions_native.py
+++ lib/dl_connector_ydb/dl_connector_ydb_tests/db/formula/test_functions_native.py
@@ -1,4 +1,5 @@
 import pytest
+import sqlalchemy as sa
 
 from dl_formula_testing.evaluator import DbEvaluator
 from dl_formula_testing.testcases.functions_native import (
@@ -45,4 +46,23 @@ class TestNativeAggregationFunctionYdb(
     YQLTestBase,
     DefaultNativeAggregationFunctionFormulaConnectorTestSuite,
 ):
-    pass
+    def test_native_aggregation_functions(
+        self,
+        dbe: DbEvaluator,
+        data_table: sa.Table,
+        native_agg_function_names: dict[str, str],
+    ) -> None:
+        int_values = data_table.int_values  # type: ignore  # 2025-10-27 # TODO: "Table" has no attribute "int_values"  [attr-defined]
+        str_values = data_table.str_values  # type: ignore  # 2025-10-27 # TODO: "Table" has no attribute "str_values"  [attr-defined]
+
+        sum_function = native_agg_function_names["sum"]
+        avg_function = native_agg_function_names["avg"]
+        max_function = native_agg_function_names["max"]
+
+        assert dbe.eval(f'DB_CALL_AGG_INT("{sum_function}", [int_value])', from_=data_table) == sum(int_values)
+        assert dbe.eval(f'DB_CALL_AGG_FLOAT("{avg_function}", [int_value])', from_=data_table) == sum(int_values) / len(
+            int_values
+        )
+        assert dbe.eval(f'DB_CALL_AGG_STRING("{max_function}", [str_value])', from_=data_table) == max(
+            str_values
+        ).encode("utf-8")
