3d53e8178d8f1e7889e69c0f55553706bfd9d338
ovsds <ovsds@yandex-team.ru>
2025-10-27 13:42:00 +0100

    tests: BI-5083 add dl_pydantic datetime deserialization tests

--- lib/dl_pydantic/dl_pydantic_tests/unit/jsonable/test_datetime.py
+++ lib/dl_pydantic/dl_pydantic_tests/unit/jsonable/test_datetime.py
@@ -18,13 +18,43 @@ def test_model_validate_with_original_type() -> None:
     assert model.value == expected_value
 
 
-def test_model_validate_json() -> None:
+@pytest.mark.parametrize(
+    "value,expected_value",
+    [
+        (
+            "2025-01-02T03:04:05.000006Z",
+            dl_pydantic.JsonableDatetime(2025, 1, 2, 3, 4, 5, 6, tzinfo=datetime.timezone.utc),
+        ),
+        (
+            "2025-01-02T03:04:05.000006+03:00",
+            dl_pydantic.JsonableDatetime(2025, 1, 2, 3, 4, 5, 6, tzinfo=datetime.timezone(datetime.timedelta(hours=3))),
+        ),
+        (
+            "2025-01-02T03:04:05Z",
+            dl_pydantic.JsonableDatetime(2025, 1, 2, 3, 4, 5, tzinfo=datetime.timezone.utc),
+        ),
+        (
+            "2025-01-02T03:04:05.006Z",
+            dl_pydantic.JsonableDatetime(2025, 1, 2, 3, 4, 5, 6000, tzinfo=datetime.timezone.utc),
+        ),
+    ],
+    ids=[
+        "utc",
+        "offset",
+        "no_microseconds",
+        "microseconds_different_precision",
+    ],
+)
+def test_model_validate_json(
+    value: str,
+    expected_value: dl_pydantic.JsonableDatetime,
+) -> None:
     class Model(dl_pydantic.BaseModel):
         value: dl_pydantic.JsonableDatetime
 
-    model = Model.model_validate_json('{"value": "2025-01-02T03:04:05.000006Z"}')
+    model = Model.model_validate_json(f'{{"value": "{value}"}}')
 
-    assert model.value == dl_pydantic.JsonableDatetime(2025, 1, 2, 3, 4, 5, 6, tzinfo=datetime.timezone.utc)
+    assert model.value == expected_value
 
 
 @pytest.mark.parametrize(
