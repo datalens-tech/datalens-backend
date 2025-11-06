216c46874b804e7eb30dfb1992280024d5e86f18
ovsds <ovsds@yandex-team.ru>
2025-11-06 15:48:07 +0100

    feat: BI-6714 add USMasterAuthProvider

--- lib/dl_auth/dl_auth/auth_providers.py
+++ lib/dl_auth/dl_auth/auth_providers.py
@@ -3,6 +3,8 @@ from typing import Protocol
 
 import attrs
 
+import dl_constants
+
 
 class AuthProviderProtocol(Protocol):
     @abc.abstractmethod
@@ -33,8 +35,12 @@ class OauthAuthProvider(AuthProviderProtocol):
         return {}
 
 
-__all__ = [
-    "AuthProviderProtocol",
-    "NoAuthProvider",
-    "OauthAuthProvider",
-]
+@attrs.define(kw_only=True)
+class USMasterTokenAuthProvider(AuthProviderProtocol):
+    token: str
+
+    def get_headers(self) -> dict[str, str]:
+        return {dl_constants.DLHeadersCommon.US_MASTER_TOKEN.value: self.token}
+
+    def get_cookies(self) -> dict[str, str]:
+        return {}
