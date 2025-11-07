9bb59404fd18ec89cc2208344606b031cb9f68c3
ovsds <ovsds@yandex-team.ru>
2025-11-07 16:32:45 +0100

    fix: BI-6711 JsonableType schema from None

--- lib/dl_pydantic/dl_pydantic_tests/unit/jsonable/test_date.py
+++ lib/dl_pydantic/dl_pydantic_tests/unit/jsonable/test_date.py
@@ -1,6 +1,7 @@
 import datetime
 
 import pydantic
+import pytest
 
 import dl_pydantic
 
@@ -30,6 +31,14 @@ def test_model_validate_json() -> None:
     model = Model.model_validate_json(f'{{"value": {JSON_VALUE}}}')
 
     assert model.value == EXPECTED
+
+
+def test_raises_validation_error_on_none() -> None:
+    class Model(dl_pydantic.BaseModel):
+        value: dl_pydantic.JsonableDate
+
+    with pytest.raises(pydantic.ValidationError):
+        Model.model_validate({"value": None})
 
 
 def test_model_dump_json() -> None:
