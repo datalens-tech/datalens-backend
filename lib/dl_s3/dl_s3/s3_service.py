cad3637428f35bb02c4a8d95685f90c7e9939a45
KonstantAnxiety <konstasa@yandex-team.ru>
2025-11-18 17:07:19 +0300

    feat: BI-6723 pass certificates from settings to S3 clients (#1347)
    * feat: BI-6723 pass certificates from settings to S3 clients
    
    * fix ssl context
    
    * fix ssl context once more
    
    * fix ssl context x3
    
    * fix ssl context x4
    
    * fix ssl context x5
    
    * fix finally
    
    * fix merge & import
    
    * typing...
    
    * fix
    
    * refactor & fix

--- lib/dl_s3/dl_s3/s3_service.py
+++ lib/dl_s3/dl_s3/s3_service.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+import ssl
 from typing import (
     TYPE_CHECKING,
     Any,
@@ -8,10 +9,12 @@ from typing import (
 )
 
 from aiobotocore.config import AioConfig
+import aiobotocore.httpsession
 from aiobotocore.session import get_session
-from aiohttp import web
+import aiohttp
 import attr
 import boto3
+from botocore.httpsession import get_cert_path
 import typing_extensions
 
 
@@ -23,6 +26,41 @@ if TYPE_CHECKING:
 LOGGER = logging.getLogger(__name__)
 
 
+class S3AIOHTTPSessionWithSSL(aiobotocore.httpsession.AIOHTTPSession):
+    """
+    Overrides ssl_context handling from upstream to allow passing it from the outside all the way into the aiohttp.TCPConnector
+    Ignoring some typing because aiobotocore makes mypy mad
+    """
+
+    def _get_ssl_context(self) -> ssl.SSLContext:
+        return self._connector_args["ssl_context"]  # type: ignore[attr-defined]
+
+    def _create_connector(self, proxy_url: str) -> aiohttp.TCPConnector:
+        ssl_context = None
+        if bool(self._verify):  # type: ignore[attr-defined]
+            if proxy_url:
+                ssl_context = self._setup_proxy_ssl_context(proxy_url)  # type: ignore[attr-defined]
+            else:
+                ssl_context = self._get_ssl_context()
+
+            if ssl_context:
+                if self._cert_file:  # type: ignore[attr-defined]
+                    ssl_context.load_cert_chain(
+                        self._cert_file,  # type: ignore[attr-defined]
+                        self._key_file,  # type: ignore[attr-defined]
+                    )
+                ca_certs = get_cert_path(self._verify)  # type: ignore[attr-defined]
+                if ca_certs:
+                    ssl_context.load_verify_locations(ca_certs, None, None)
+
+        self._connector_args["ssl_context"] = ssl_context  # type: ignore[attr-defined]
+
+        return aiohttp.TCPConnector(
+            limit=self._max_pool_connections,  # type: ignore[attr-defined]
+            **self._connector_args,  # type: ignore[attr-defined]
+        )
+
+
 @attr.s(kw_only=True)
 class S3Service:
     APP_KEY: ClassVar[str] = "S3_SERVICE"
@@ -31,12 +69,14 @@ class S3Service:
     _secret_access_key: str = attr.ib(repr=False)
     _endpoint_url: str = attr.ib()
     _use_virtual_host_addressing: bool = attr.ib(default=False)
+    _ca_data: bytes = attr.ib(repr=False)
 
     tmp_bucket_name: str = attr.ib()
     persistent_bucket_name: str = attr.ib()
 
     _client: AsyncS3Client = attr.ib(init=False, repr=False, hash=False, cmp=False)
     _client_init_params: dict[str, Any] = attr.ib(init=False, repr=False, hash=False, cmp=False)
+    _connector_args: dict[str, Any] = attr.ib(init=False, repr=False, hash=False, cmp=False)
 
     @property
     def client(self) -> AsyncS3Client:
@@ -46,22 +86,33 @@ class S3Service:
     def get_full_app_key(cls) -> str:
         return cls.APP_KEY
 
-    async def init_hook(self, target_app: web.Application) -> None:
+    async def init_hook(self, target_app: aiohttp.web.Application) -> None:
         target_app[self.APP_KEY] = self
         await self.initialize()
 
-    async def tear_down_hook(self, target_app: web.Application) -> None:
+    async def tear_down_hook(self, target_app: aiohttp.web.Application) -> None:
         await self.tear_down()
 
+    @property
+    def _s3_addressing_style(self) -> str:
+        return "virtual" if self._use_virtual_host_addressing else "auto"
+
     def _init_client_config(self) -> None:
+        ssl_context = ssl.create_default_context(cadata=self._ca_data.decode("ascii"))
+        self._connector_args = dict(
+            ssl_context=ssl_context,
+        )
+
         self._client_init_params = dict(
             service_name="s3",
             aws_access_key_id=self._access_key_id,
             aws_secret_access_key=self._secret_access_key,
             endpoint_url=self._endpoint_url,
             config=AioConfig(
+                connector_args=self._connector_args,
+                http_session_cls=S3AIOHTTPSessionWithSSL,
                 signature_version="s3v4",  # v4 signature is required to generate presigned URLs with restriction policies
-                s3={"addressing_style": "virtual" if self._use_virtual_host_addressing else "auto"},
+                s3={"addressing_style": self._s3_addressing_style},
             ),
         )
 
@@ -79,7 +130,7 @@ class S3Service:
         await self._client.close()
 
     @classmethod
-    def get_app_instance(cls, app: web.Application) -> typing_extensions.Self:
+    def get_app_instance(cls, app: aiohttp.web.Application) -> typing_extensions.Self:
         service = app.get(cls.APP_KEY, None)
         if service is None:
             raise ValueError("S3BucketService was not initiated for application")
