ae8e3d2cc469a1986c5709fd7816f5dc3bb3ad23
ovsds <ovsds@yandex-team.ru>
2025-11-19 12:30:40 +0100

    feat: BI-6687 add AuthProviderSettings

--- lib/dl_auth/dl_auth/auth_providers.py
+++ lib/dl_auth/dl_auth/auth_providers.py
@@ -2,8 +2,14 @@ import abc
 from typing import Protocol
 
 import attrs
+from typing_extensions import Self
 
 import dl_constants
+import dl_settings
+
+
+class AuthProviderSettings(dl_settings.TypedBaseSettings):
+    ...
 
 
 class AuthProviderProtocol(Protocol):
@@ -16,7 +22,18 @@ class AuthProviderProtocol(Protocol):
         ...
 
 
+class NoAuthProviderSettings(AuthProviderSettings):
+    ...
+
+
+AuthProviderSettings.register("NONE", NoAuthProviderSettings)
+
+
 class NoAuthProvider(AuthProviderProtocol):
+    @classmethod
+    def from_settings(cls, settings: NoAuthProviderSettings) -> Self:
+        return cls()
+
     def get_headers(self) -> dict[str, str]:
         return {}
 
@@ -24,9 +41,20 @@ class NoAuthProvider(AuthProviderProtocol):
         return {}
 
 
+class OauthAuthProviderSettings(AuthProviderSettings):
+    token: str
+
+
+AuthProviderSettings.register("OAUTH", OauthAuthProviderSettings)
+
+
 @attrs.define(kw_only=True)
 class OauthAuthProvider(AuthProviderProtocol):
     token: str
+
+    @classmethod
+    def from_settings(cls, settings: OauthAuthProviderSettings) -> Self:
+        return cls(token=settings.token)
 
     def get_headers(self) -> dict[str, str]:
         return {"Authorization": f"OAuth {self.token}"}
@@ -35,9 +63,20 @@ class OauthAuthProvider(AuthProviderProtocol):
         return {}
 
 
+class USMasterTokenAuthProviderSettings(AuthProviderSettings):
+    token: str
+
+
+AuthProviderSettings.register("US_MASTER_TOKEN", USMasterTokenAuthProviderSettings)
+
+
 @attrs.define(kw_only=True)
 class USMasterTokenAuthProvider(AuthProviderProtocol):
     token: str
+
+    @classmethod
+    def from_settings(cls, settings: USMasterTokenAuthProviderSettings) -> Self:
+        return cls(token=settings.token)
 
     def get_headers(self) -> dict[str, str]:
         return {dl_constants.DLHeadersCommon.US_MASTER_TOKEN.value: self.token}
