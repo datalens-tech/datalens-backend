469f1c95c36e5a4d80b829c5c6eedba78545e6e6
ovsds <ovsds@yandex-team.ru>
2025-11-21 17:16:15 +0100

    feat: BI-6687 add temporal shutdown callbacks

--- lib/dl_temporal/dl_temporal/utils/app/models.py
+++ lib/dl_temporal/dl_temporal/utils/app/models.py
@@ -1,9 +1,12 @@
+import asyncio
 from typing import (
     Any,
+    Callable,
     Coroutine,
 )
 
 import attr
+from typing_extensions import Self
 
 
 @attr.define(frozen=True, kw_only=True)
@@ -11,3 +14,16 @@ class Callback:
     coroutine: Coroutine[Any, Any, None]
     name: str
     exception: bool = attr.field(default=True)
+
+    @classmethod
+    def from_sync_function(
+        cls,
+        function: Callable[[], Any],
+        name: str,
+        exception: bool = True,
+    ) -> Self:
+        return cls(
+            coroutine=asyncio.to_thread(function),
+            name=name,
+            exception=exception,
+        )
