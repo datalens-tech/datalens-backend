3ea801c622882718058136231c52a5994e4cf40e
khamitovdr <khamitovdr@yandex-team.ru>
2025-11-25 15:52:51 +0300

    and...

--- lib/dl_connector_oracle/dl_connector_oracle/formula/definitions/functions_hash.py
+++ lib/dl_connector_oracle/dl_connector_oracle/formula/definitions/functions_hash.py
@@ -14,7 +14,7 @@ DEFINITIONS_HASH = [
         variants=[
             V(
                 D.ORACLE,
-                lambda value: sa.func.RAWTOHEX(sa.func.STANDARD_HASH(sa.func.UTL_RAW.CAST_TO_RAW(value), "MD5")),
+                lambda value: sa.func.RAWTOHEX(sa.func.STANDARD_HASH(sa.func.UTL_RAW.CAST_TO_RAW(value), sa.literal("MD5"))),
             ),
         ]
     ),
@@ -22,15 +22,14 @@ DEFINITIONS_HASH = [
         variants=[
             V(
                 D.ORACLE,
-                lambda value: sa.func.RAWTOHEX(sa.func.STANDARD_HASH(sa.func.UTL_RAW.CAST_TO_RAW(value), "SHA1")),
-            ),
+                lambda value: sa.func.RAWTOHEX(sa.func.STANDARD_HASH(sa.func.UTL_RAW.CAST_TO_RAW(value), sa.literal("SHA1")))),
         ]
     ),
     base.SHA256(
         variants=[
             V(
                 D.ORACLE,
-                lambda value: sa.func.RAWTOHEX(sa.func.STANDARD_HASH(sa.func.UTL_RAW.CAST_TO_RAW(value), "SHA256")),
+                lambda value: sa.func.RAWTOHEX(sa.func.STANDARD_HASH(sa.func.UTL_RAW.CAST_TO_RAW(value), sa.literal("SHA256"))),
             ),
         ]
     ),
