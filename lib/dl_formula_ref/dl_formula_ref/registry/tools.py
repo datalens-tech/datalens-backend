2c90106bd50057d768cd18cf1bbce79bb60c99e4
DataLens Backend <151861227+robot-datalens-back@users.noreply.github.com>
2025-12-16 15:31:58 +0300

    Hotfix v0.2429.1 (#1410)
    docs: BI-6377 Formula development 2025Q3 (#1303)
    
    * docs
    
    * update native docs
    
    * translations
    
    * po + mo
    
    * fix
    
    * po + mo
    
    Co-authored-by: Denis Khamitov <khamitovdr@yandex-team.ru>

--- lib/dl_formula_ref/dl_formula_ref/registry/tools.py
+++ lib/dl_formula_ref/dl_formula_ref/registry/tools.py
@@ -3,9 +3,11 @@ from __future__ import annotations
 from dl_formula_ref.functions.aggregation import FUNCTIONS_AGGREGATION
 from dl_formula_ref.functions.array import FUNCTIONS_ARRAY
 from dl_formula_ref.functions.date import FUNCTIONS_DATE
+from dl_formula_ref.functions.hash import FUNCTIONS_HASH
 from dl_formula_ref.functions.logical import FUNCTIONS_LOGICAL
 from dl_formula_ref.functions.markup import FUNCTIONS_MARKUP
 from dl_formula_ref.functions.mathematical import FUNCTIONS_MATHEMATICAL
+from dl_formula_ref.functions.native import FUNCTIONS_NATIVE
 from dl_formula_ref.functions.operator import FUNCTIONS_OPERATOR
 from dl_formula_ref.functions.string import FUNCTIONS_STRING
 from dl_formula_ref.functions.time_series import FUNCTIONS_TIME_SERIES
@@ -19,9 +21,11 @@ EXPLICITLY_DEFINED_FUNCTIONS: list[FunctionDocRegistryItem] = [
     *FUNCTIONS_AGGREGATION,
     *FUNCTIONS_ARRAY,
     *FUNCTIONS_DATE,
+    *FUNCTIONS_HASH,
     *FUNCTIONS_LOGICAL,
     *FUNCTIONS_MARKUP,
     *FUNCTIONS_MATHEMATICAL,
+    *FUNCTIONS_NATIVE,
     *FUNCTIONS_OPERATOR,
     *FUNCTIONS_STRING,
     *FUNCTIONS_TIME_SERIES,
