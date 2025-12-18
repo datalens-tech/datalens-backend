ddfd8a78aea60547c5aa31f5f9d1562fc5cff699
Denis Khamitov <khamitovdr@yandex-team.ru>
2025-12-18 18:06:15 +0300

    docs: BI-6377 Rollback mathematical functions category rename (#1414)
    revert numeric

--- lib/dl_formula_ref/dl_formula_ref/categories/mathematical.py
+++ lib/dl_formula_ref/dl_formula_ref/categories/mathematical.py
@@ -2,7 +2,7 @@ from dl_formula_ref.registry.base import FunctionDocCategory
 
 
 CATEGORY_MATHEMATICAL = FunctionDocCategory(
-    name="mathematical",
+    name="numeric",
     description="",
     keywords="",
 )
