5a1a8d0a4045ef9a38dcce97b550a4a9b383684e
ForrestGump <seray@yandex-team.ru>
2026-01-15 15:36:24 +0100

    feat: BI-6891 optional schema generation

--- lib/dl_app_api_base/dl_app_api_base/openapi/models.py
+++ lib/dl_app_api_base/dl_app_api_base/openapi/models.py
@@ -40,57 +40,53 @@ class OpenApiSpec:
             if not route.handler.OPENAPI_INCLUDE:
                 continue
 
-            request_schema = route.handler._request_schema
-            required_fields = {"path", "query", "body"}
-            actual_fields = set(request_schema.model_fields.keys())
-            if not required_fields <= actual_fields:
-                raise TypeError(
-                    f"RequestSchema must contain path, query, body fields. "
-                    f"Missing: {required_fields - actual_fields}, got: {actual_fields}"
-                )
-
             responses: dict = {}
 
             parameters: list[dict] = []
 
-            path_type = request_schema.model_fields["path"].annotation
-            assert path_type is not None and issubclass(path_type, dl_pydantic.BaseModel)
-            path_schema = path_type.model_json_schema()
-            for property_name, property_schema in path_schema.get("properties", {}).items():
-                parameters.append(
-                    {
-                        "name": property_name,
-                        "in": "path",
-                        "required": "default" not in property_schema,
-                        "schema": property_schema,
-                    }
-                )
+            request_schema = route.handler._request_schema
 
-            query_type = request_schema.model_fields["query"].annotation
-            assert query_type is not None and issubclass(query_type, dl_pydantic.BaseModel)
-            query_schema = query_type.model_json_schema()
-            for property_name, property_schema in query_schema.get("properties", {}).items():
-                parameters.append(
-                    {
-                        "name": property_name,
-                        "in": "query",
-                        "required": "default" not in property_schema,
-                        "schema": property_schema,
-                    }
-                )
+            if "path" in request_schema.model_fields:
+                path_type = request_schema.model_fields["path"].annotation
+                assert path_type is not None and issubclass(path_type, dl_pydantic.BaseModel)
+                path_schema = path_type.model_json_schema()
+                for property_name, property_schema in path_schema.get("properties", {}).items():
+                    parameters.append(
+                        {
+                            "name": property_name,
+                            "in": "path",
+                            "required": "default" not in property_schema,
+                            "schema": property_schema,
+                        }
+                    )
 
-            body_type = request_schema.model_fields["body"].annotation
-            assert body_type is not None and issubclass(body_type, dl_pydantic.BaseModel)
-            body_schema = body_type.model_json_schema()
-            if len(body_schema.get("properties", {})) > 0:
-                parameters.append(
-                    {
-                        "name": "body",
-                        "in": "body",
-                        "required": True,
-                        "schema": body_schema,
-                    }
-                )
+            if "query" in request_schema.model_fields:
+                query_type = request_schema.model_fields["query"].annotation
+                assert query_type is not None and issubclass(query_type, dl_pydantic.BaseModel)
+                query_schema = query_type.model_json_schema()
+                for property_name, property_schema in query_schema.get("properties", {}).items():
+                    parameters.append(
+                        {
+                            "name": property_name,
+                            "in": "query",
+                            "required": "default" not in property_schema,
+                            "schema": property_schema,
+                        }
+                    )
+
+            if "body" in request_schema.model_fields:
+                body_type = request_schema.model_fields["body"].annotation
+                assert body_type is not None and issubclass(body_type, dl_pydantic.BaseModel)
+                body_schema = body_type.model_json_schema()
+                if len(body_schema.get("properties", {})) > 0:
+                    parameters.append(
+                        {
+                            "name": "body",
+                            "in": "body",
+                            "required": True,
+                            "schema": body_schema,
+                        }
+                    )
 
             for status, response_schema in route.handler._response_schemas.items():
                 response_schema_dict = response_schema.model_json_schema()
