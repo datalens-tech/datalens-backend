58377d4efc8433da4a6f5763034622675eef0d69
catsona <creampie@yandex-team.ru>
2026-01-23 14:31:09 +0300

    fix tests

--- lib/dl_connector_ydb/dl_connector_ydb_tests/db/formula/test_functions_datetime.py
+++ lib/dl_connector_ydb/dl_connector_ydb_tests/db/formula/test_functions_datetime.py
@@ -34,26 +34,41 @@ class TestDateTimeFunctionYQL(YQLTestBase, DefaultDateTimeFunctionFormulaConnect
         ydb_data_table: sa.Table,
         ydb_data_table_field_types: dict[str, DataType],
     ) -> None:
-        assert dbe.eval("AVG([datetime_value])", from_=ydb_data_table) == dt_avg(
-            ydb_data_table.datetime_values,
+        assert dbe.eval(
+            "AVG([datetime_value])",
+            from_=ydb_data_table,
             field_types=ydb_data_table_field_types,
-        )
-        assert dbe.eval("MAX([datetime_value])", from_=ydb_data_table) == dt_max(
+        ) == dt_avg(
             ydb_data_table.datetime_values,
-            field_types=ydb_data_table_field_types,
         )
-        assert dbe.eval("MAX(DATETIME([datetime_value]))", from_=ydb_data_table) == dt_max(
+        assert dbe.eval(
+            "MAX([datetime_value])",
+            from_=ydb_data_table,
+            field_types=ydb_data_table_field_types,
+        ) == dt_max(
             ydb_data_table.datetime_values,
-            field_types=ydb_data_table_field_types,
         )
-        assert dbe.eval("MAX(INT([datetime_value]))", from_=ydb_data_table) == int(
+        assert dbe.eval(
+            "MAX(DATETIME([datetime_value]))",
+            from_=ydb_data_table,
+            field_types=ydb_data_table_field_types,
+        ) == dt_max(
+            ydb_data_table.datetime_values,
+        )
+        assert dbe.eval(
+            "MAX(INT([datetime_value]))",
+            from_=ydb_data_table,
+            field_types=ydb_data_table_field_types,
+        ) == int(
             dt_max(ydb_data_table.datetime_values).replace(tzinfo=datetime.timezone.utc).timestamp(),
-            field_types=ydb_data_table_field_types,
         )
-        assert dbe.eval("MAX(FLOAT([datetime_value]))", from_=ydb_data_table) == float(
+        assert dbe.eval(
+            "MAX(FLOAT([datetime_value]))",
+            from_=ydb_data_table,
+            field_types=ydb_data_table_field_types,
+        ) == float(
             int(
                 dt_max(ydb_data_table.datetime_values).replace(tzinfo=datetime.timezone.utc).timestamp(),
-                field_types=ydb_data_table_field_types,
             )
         )
 
@@ -61,23 +76,38 @@ class TestDateTimeFunctionYQL(YQLTestBase, DefaultDateTimeFunctionFormulaConnect
     def test_timestamp_functions(
         self, dbe: DbEvaluator, ydb_data_table: sa.Table, ydb_data_table_field_types: dict[str, DataType]
     ) -> None:
-        assert dbe.eval("AVG([timestamp_value])", from_=ydb_data_table) == dt_avg(
-            ydb_data_table.timestamp_values,
+        assert dbe.eval(
+            "AVG([timestamp_value])",
+            from_=ydb_data_table,
             field_types=ydb_data_table_field_types,
-        )
-        assert dbe.eval("MAX([timestamp_value])", from_=ydb_data_table) == dt_max(
+        ) == dt_avg(
             ydb_data_table.timestamp_values,
-            field_types=ydb_data_table_field_types,
         )
-        assert dbe.eval("MAX(DATETIME([timestamp_value]))", from_=ydb_data_table) == dt_max(
+        assert dbe.eval(
+            "MAX([timestamp_value])",
+            from_=ydb_data_table,
+            field_types=ydb_data_table_field_types,
+        ) == dt_max(
             ydb_data_table.timestamp_values,
-            field_types=ydb_data_table_field_types,
         )
-        assert dbe.eval("MAX(INT([timestamp_value]))", from_=ydb_data_table) == int(
+        assert dbe.eval(
+            "MAX(DATETIME([timestamp_value]))",
+            from_=ydb_data_table,
+            field_types=ydb_data_table_field_types,
+        ) == dt_max(
+            ydb_data_table.timestamp_values,
+        )
+        assert dbe.eval(
+            "MAX(INT([timestamp_value]))",
+            from_=ydb_data_table,
+            field_types=ydb_data_table_field_types,
+        ) == int(
             dt_max(ydb_data_table.timestamp_values).replace(tzinfo=datetime.timezone.utc).timestamp(),
-            field_types=ydb_data_table_field_types,
         )
-        assert dbe.eval("MAX(FLOAT([timestamp_value]))", from_=ydb_data_table) == float(
-            int(dt_max(ydb_data_table.timestamp_values).replace(tzinfo=datetime.timezone.utc).timestamp()),
+        assert dbe.eval(
+            "MAX(FLOAT([timestamp_value]))",
+            from_=ydb_data_table,
             field_types=ydb_data_table_field_types,
+        ) == float(
+            int(dt_max(ydb_data_table.timestamp_values).replace(tzinfo=datetime.timezone.utc).timestamp()),
         )
