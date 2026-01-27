f670610df1731d288835145e905ee759728d5f75
chl0rum <cl.chl0rum@gmail.com>
2026-01-27 17:13:44 +0300

    rm clickhouse conditional blocks test suite

--- lib/dl_connector_bundle_chs3/dl_connector_bundle_chs3_tests/db/file/formula/test_conditional_blocks.py
+++ lib/dl_connector_bundle_chs3/dl_connector_bundle_chs3_tests/db/file/formula/test_conditional_blocks.py
@@ -1,6 +1,7 @@
+from dl_formula_testing.testcases.conditional_blocks import DefaultConditionalBlockFormulaConnectorTestSuite
+
 from dl_connector_bundle_chs3_tests.db.file.formula.base import FileTestBase
-from dl_connector_clickhouse.formula.testing.test_suites import ConditionalBlockClickHouseTestSuite
 
 
-class TestMainAggFunctionFile(FileTestBase, ConditionalBlockClickHouseTestSuite):
+class TestMainAggFunctionFile(FileTestBase, DefaultConditionalBlockFormulaConnectorTestSuite):
     pass
