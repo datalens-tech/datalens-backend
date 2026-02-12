1fe9a9fd5079e1e7d9abbdb969949a646fc19d66
ovsds <ovsds@yandex-team.ru>
2026-02-12 11:05:17 +0100

    feat: BI-7011 use make_uuid_from_parts in generate_child_request_id

--- lib/dl_utils/dl_utils/__init__.py
+++ lib/dl_utils/dl_utils/__init__.py
@@ -1,4 +1,7 @@
-from .utils import request_id_generator
+from .utils import (
+    make_uuid_from_parts,
+    request_id_generator,
+)
 from .wait import (
     await_for,
     wait_for,
@@ -9,4 +12,5 @@ __all__ = [
     "await_for",
     "wait_for",
     "request_id_generator",
+    "make_uuid_from_parts",
 ]
