a8d000b217b3186054417b00cc4c38442f8f2fab
chl0rum <cl.chl0rum@gmail.com>
2026-03-12 14:32:34 +0300

    add obfuscation middlewares to init

--- lib/dl_api_commons/dl_api_commons/aio/middlewares/__init__.py
+++ lib/dl_api_commons/dl_api_commons/aio/middlewares/__init__.py
@@ -1,4 +1,5 @@
 from .commit_rci import commit_rci_middleware
+from .obfuscation_context import obfuscation_context_middleware
 from .rci_headers import rci_headers_middleware
 from .request_bootstrap import RequestBootstrap
 from .request_id import RequestId
@@ -8,5 +9,6 @@ __all__ = [
     "RequestBootstrap",
     "RequestId",
     "commit_rci_middleware",
+    "obfuscation_context_middleware",
     "rci_headers_middleware",
 ]
--- lib/dl_api_commons/dl_api_commons/flask/middlewares/__init__.py
+++ lib/dl_api_commons/dl_api_commons/flask/middlewares/__init__.py
@@ -2,6 +2,7 @@ from .aio_event_loop_middleware import AIOEventLoopMiddleware
 from .commit_rci_middleware import ReqCtxInfoMiddleware
 from .context_var_middleware import ContextVarMiddleware
 from .logging_context import RequestLoggingContextControllerMiddleWare
+from .obfuscation_context import ObfuscationContextMiddleware
 from .rci_headers_middleware import RCIHeadersMiddleware
 from .request_id import (
     RequestContextInfo,
@@ -12,6 +13,7 @@ from .request_id import (
 __all__ = [
     "AIOEventLoopMiddleware",
     "ContextVarMiddleware",
+    "ObfuscationContextMiddleware",
     "RCIHeadersMiddleware",
     "ReqCtxInfoMiddleware",
     "RequestContextInfo",
