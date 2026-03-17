2f292d55922e519a473f2cc53c98c4b417dc6ebf
Denis Khamitov <khamitovdr@yandex-team.ru>
2026-03-17 22:30:53 +0300

    feat: BI-6615 async AuthProvider methods (#1639)
    * squash
    
    * fixes
    
    * fix
    
    * fix
    
    * drop pytest
    
    * fix

--- lib/dl_auth/dl_auth_tests/unit/auth_providers/test_oauth.py
+++ lib/dl_auth/dl_auth_tests/unit/auth_providers/test_oauth.py
@@ -1,3 +1,5 @@
+import pytest
+
 import dl_auth
 
 
@@ -5,3 +7,10 @@ def test_oauth_auth_provider() -> None:
     auth_provider = dl_auth.OauthAuthProvider(token="test-token")
     assert auth_provider.get_headers() == {"Authorization": "OAuth test-token"}
     assert auth_provider.get_cookies() == {}
+
+
+@pytest.mark.asyncio
+async def test_oauth_auth_provider_async() -> None:
+    auth_provider = dl_auth.OauthAuthProvider(token="test-token")
+    assert await auth_provider.get_headers_async() == {"Authorization": "OAuth test-token"}
+    assert await auth_provider.get_cookies_async() == {}
