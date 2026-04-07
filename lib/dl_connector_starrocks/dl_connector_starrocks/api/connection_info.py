55f35bbf8dd077b4ec97f840334fdc494d21baf8
KonstantAnxiety <konstasa@yandex-team.ru>
2026-04-07 16:29:29 +0300

    feat: BI-0 add starrocks icons to info provider (1408712b1)

--- lib/dl_connector_starrocks/dl_connector_starrocks/api/connection_info.py
+++ lib/dl_connector_starrocks/dl_connector_starrocks/api/connection_info.py
@@ -1,7 +1,18 @@
+from importlib.resources import files
+
 from dl_api_connector.connection_info import ConnectionInfoProvider
 
 from dl_connector_starrocks.api.i18n.localizer import Translatable
+from dl_connector_starrocks.assets.icons import (
+    nav,
+    standard,
+)
 
 
 class StarRocksConnectionInfoProvider(ConnectionInfoProvider):
     title_translatable = Translatable("label_connector-starrocks")
+
+    def __attrs_post_init__(self) -> None:
+        self.icon_data_standard_filepath = files(standard).joinpath("starrocks.svg")
+        self.icon_data_nav_filepath = files(nav).joinpath("starrocks.svg")
+        super().__attrs_post_init__()
