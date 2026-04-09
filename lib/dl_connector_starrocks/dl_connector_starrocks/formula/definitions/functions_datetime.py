6575dc4060900bd2c74c16723341a33831d707c8
Julia <cl.chl0rum@gmail.com>
2026-04-09 11:41:59 +0300

    feat: BI-7171 starrocks functions: datetime (8334eab76)
    * starrocks datetime func
    
    * lint fix
    
    * add missed func

--- lib/dl_connector_starrocks/dl_connector_starrocks/formula/definitions/functions_datetime.py
+++ lib/dl_connector_starrocks/dl_connector_starrocks/formula/definitions/functions_datetime.py
@@ -1,7 +1,9 @@
 import sqlalchemy as sa
 
 from dl_formula.definitions.base import TranslationVariant
+from dl_formula.definitions.common_datetime import datetime_interval
 import dl_formula.definitions.functions_datetime as base
+from dl_formula.definitions.literals import un_literal
 
 from dl_connector_starrocks.formula.constants import StarRocksDialect as D
 
@@ -10,14 +12,66 @@ V = TranslationVariant.make
 
 
 DEFINITIONS_DATETIME = [
-    # TODO: BI-7171 dateadd, datepart, datetrunc
+    # dateadd
+    base.FuncDateadd1.for_dialect(D.STARROCKS),
+    base.FuncDateadd2Unit.for_dialect(D.STARROCKS),
+    base.FuncDateadd2Number.for_dialect(D.STARROCKS),
+    base.FuncDateadd3Legacy.for_dialect(D.STARROCKS),
+    base.FuncDateadd3DateConstNum(
+        variants=[
+            V(
+                D.STARROCKS,
+                lambda date, what, num: sa.cast(date + datetime_interval(un_literal(what), un_literal(num)), sa.Date()),
+            ),
+        ]
+    ),
+    base.FuncDateadd3DatetimeConstNum(
+        variants=[
+            V(D.STARROCKS, lambda dt, what, num: (dt + datetime_interval(what.value, num.value))),
+        ]
+    ),
+    # datepart
+    base.FuncDatepart2Legacy.for_dialect(D.STARROCKS),
+    base.FuncDatepart2.for_dialect(D.STARROCKS),
+    base.FuncDatepart3Const.for_dialect(D.STARROCKS),
+    base.FuncDatepart3NonConst.for_dialect(D.STARROCKS),
+    # datetrunc — use native StarRocks DATE_TRUNC
+    base.FuncDatetrunc2Date(
+        variants=[
+            V(
+                D.STARROCKS,
+                lambda date, unit: (
+                    sa.cast(sa.func.date_trunc(base.norm_datetrunc_unit(unit), date), sa.Date)
+                    if base.norm_datetrunc_unit(unit) in {"year", "quarter", "month", "week", "day"}
+                    else date
+                ),
+            ),
+        ]
+    ),
+    base.FuncDatetrunc2Datetime(
+        variants=[
+            V(
+                D.STARROCKS,
+                lambda date, unit: sa.func.date_trunc(base.norm_datetrunc_unit(unit), date),
+            ),
+        ],
+    ),
     # day
     base.FuncDay(
         variants=[
             V(D.STARROCKS, sa.func.DAYOFMONTH),
         ]
     ),
-    # TODO: BI-7171 dayofweek
+    # dayofweek
+    base.FuncDayofweek1.for_dialect(D.STARROCKS),
+    base.FuncDayofweek2(
+        variants=[
+            V(
+                D.STARROCKS,
+                lambda date, firstday: base.dow_firstday_shift((sa.func.DAYOFWEEK(date) + 5) % 7 + 1, firstday),
+            ),
+        ]
+    ),
     # genericnow
     base.FuncGenericNow(
         variants=[
