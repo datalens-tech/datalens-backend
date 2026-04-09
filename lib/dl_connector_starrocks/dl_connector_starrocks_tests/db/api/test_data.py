e24165c13516df0d8a0e459e1aeb35e2f6f4d1fe
Julia <cl.chl0rum@gmail.com>
2026-04-09 22:04:45 +0300

    test: BI-7171 enable formula api tests in starrocks (bbef13bba)
    enable formula api tests

--- lib/dl_connector_starrocks/dl_connector_starrocks_tests/db/api/test_data.py
+++ lib/dl_connector_starrocks/dl_connector_starrocks_tests/db/api/test_data.py
@@ -13,13 +13,6 @@ from dl_connector_starrocks_tests.db.api.base import StarRocksDataApiTestBase
 
 class TestStarRocksDataResult(StarRocksDataApiTestBase, DefaultConnectorDataResultTestSuite):
     test_params = RegulatedTestParams(
-        mark_tests_skipped={
-            DefaultConnectorDataResultTestSuite.test_basic_result: "TODO: BI-7172 formula definitions not implemented",
-            DefaultConnectorDataResultTestSuite.test_duplicated_expressions: "TODO: BI-7172 formula definitions not implemented",
-            DefaultConnectorDataResultTestSuite.test_dates: "TODO: BI-7172 formula definitions not implemented",
-            DefaultConnectorDataResultTestSuite.test_get_result_with_formula_in_where: "TODO: BI-7172 formula definitions not implemented",
-            DefaultConnectorDataResultTestSuite.test_get_result_with_string_filter_operations_for_numbers: "TODO: BI-7172 formula definitions not implemented",
-        },
         mark_features_skipped={
             DefaultConnectorDataResultTestSuite.array_support: "TODO: BI-7172 formula definitions not implemented",
         },
@@ -27,27 +20,15 @@ class TestStarRocksDataResult(StarRocksDataApiTestBase, DefaultConnectorDataResu
 
 
 class TestStarRocksDataGroupBy(StarRocksDataApiTestBase, DefaultConnectorDataGroupByFormulaTestSuite):
-    test_params = RegulatedTestParams(
-        mark_tests_skipped={
-            DefaultConnectorDataGroupByFormulaTestSuite.test_complex_result: "TODO: BI-7172 formula definitions not implemented",
-        },
-    )
+    pass
 
 
 class TestStarRocksDataRange(StarRocksDataApiTestBase, DefaultConnectorDataRangeTestSuite):
-    test_params = RegulatedTestParams(
-        mark_tests_skipped={
-            DefaultConnectorDataRangeTestSuite.test_basic_range: "TODO: BI-7172 formula definitions not implemented",
-        },
-    )
+    pass
 
 
 class TestStarRocksDataDistinct(StarRocksDataApiTestBase, DefaultConnectorDataDistinctTestSuite):
-    test_params = RegulatedTestParams(
-        mark_tests_skipped={
-            DefaultConnectorDataDistinctTestSuite.test_date_filter_distinct: "TODO: BI-7172 formula definitions not implemented",
-        },
-    )
+    pass
 
 
 class TestStarRocksDataPreview(StarRocksDataApiTestBase, DefaultConnectorDataPreviewTestSuite):
@@ -56,8 +37,3 @@ class TestStarRocksDataPreview(StarRocksDataApiTestBase, DefaultConnectorDataPre
 
 class TestStarRocksDataCache(StarRocksDataApiTestBase, DefaultConnectorDataCacheTestSuite):
     data_caches_enabled = True
-    test_params = RegulatedTestParams(
-        mark_tests_skipped={
-            DefaultConnectorDataCacheTestSuite.test_cache_with_filter_with_constants: "TODO: BI-7172 formula definitions not implemented",
-        },
-    )
