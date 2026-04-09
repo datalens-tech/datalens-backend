24cd4e0ec59568ef605d408f8c2425b5a216656e
Julia <cl.chl0rum@gmail.com>
2026-04-09 12:13:33 +0300

    feat: BI-7171 starrocks functions: math (c2ccebd91)
    starrocks math func

--- lib/dl_connector_starrocks/dl_connector_starrocks_tests/db/formula/test_functions_math.py
+++ lib/dl_connector_starrocks/dl_connector_starrocks_tests/db/formula/test_functions_math.py
@@ -1,16 +1,7 @@
 from dl_formula_testing.testcases.functions_math import DefaultMathFunctionFormulaConnectorTestSuite
-from dl_testing.regulated_test import (
-    RegulatedTestCase,
-    RegulatedTestParams,
-)
 
 from dl_connector_starrocks_tests.db.formula.base import StarRocksTestBase
 
 
-class TestMathFunctionStarRocks(StarRocksTestBase, RegulatedTestCase, DefaultMathFunctionFormulaConnectorTestSuite):
-    test_params = RegulatedTestParams(
-        mark_tests_failed={
-            DefaultMathFunctionFormulaConnectorTestSuite.test_greatest: "TODO: BI-7171 GREATEST returns int for Date args",
-            DefaultMathFunctionFormulaConnectorTestSuite.test_least: "TODO: BI-7171 LEAST returns int for Date args",
-        },
-    )
+class TestMathFunctionStarRocks(StarRocksTestBase, DefaultMathFunctionFormulaConnectorTestSuite):
+    pass
