e27c12ba75fd83e247512fb21c1bdef8c30c14e8
Dmitrii Ovsyannikov <ovsds@yandex-team.ru>
2026-04-13 15:22:21 +0200

    feat: BI-7152 add APP_INFO to logging context (1058619da)

--- lib/dl_app_api_base/dl_app_api_base/middlewares/__init__.py
+++ lib/dl_app_api_base/dl_app_api_base/middlewares/__init__.py
@@ -1,8 +1,12 @@
 from .logging import LoggingMiddleware
-from .logging_context import LoggingContextMiddleware
+from .logging_context import (
+    LoggingContextMiddleware,
+    StaticLoggingContext,
+)
 
 
 __all__ = [
     "LoggingContextMiddleware",
     "LoggingMiddleware",
+    "StaticLoggingContext",
 ]
--- lib/dl_app_api_base/dl_app_api_base/middlewares/logging_context.py
+++ lib/dl_app_api_base/dl_app_api_base/middlewares/logging_context.py
@@ -13,8 +13,19 @@ LOGGER = logging.getLogger(__name__)
 
 
 @attr.define(frozen=True, kw_only=True)
+class StaticLoggingContext:
+    app_name: str
+    app_version: str
+
+    def put_to_context(self) -> None:
+        dl_logging.put_to_context("application.name", self.app_name)
+        dl_logging.put_to_context("application.version", self.app_version)
+
+
+@attr.define(frozen=True, kw_only=True)
 class LoggingContextMiddleware:
     _request_context_provider: request_context.RequestContextProviderProtocol[headers.HeadersRequestContextMixin]
+    _static_logging_context: StaticLoggingContext
 
     @aiohttp.web.middleware
     async def process(
@@ -25,16 +36,16 @@ class LoggingContextMiddleware:
         request_context = self._request_context_provider.get()
         request_id = request_context.get_request_id()
 
-        initial_log_context = dl_logging.get_log_context()
-
-        if len(initial_log_context) > 0:
+        log_context = dl_logging.get_log_context()
+        if len(log_context) > 0:
             LOGGER.error(
                 "Initial log context for request %s is not empty: %s",
                 request_id,
-                initial_log_context,
+                log_context,
             )
             dl_logging.reset_context()
 
+        self._static_logging_context.put_to_context()
         dl_logging.put_to_context("parent_request_id", request_id)
         dl_logging.put_to_context("trace_id", request_context.get_trace_id())
         dl_logging.put_to_context("user_ip", request_context.get_user_ip())
