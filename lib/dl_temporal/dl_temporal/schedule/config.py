827b960d41e01c266152aad55866300ebfa30673
Dmitrii Ovsyannikov <ovsds@yandex-team.ru>
2026-04-13 14:28:43 +0200

    feat: BI-7188 schedule_sync_service.sync on_startup (daa9e50f9)

--- lib/dl_temporal/dl_temporal/schedule/config.py
+++ lib/dl_temporal/dl_temporal/schedule/config.py
@@ -12,8 +12,11 @@ class ScheduleConfig(dl_pydantic.BaseModel):
     TASK_QUEUE: str
     INTERVAL: dl_pydantic.JsonableTimedelta
     PARAMS: dict[str, Any] = pydantic.Field(default_factory=dict)
+    PAUSED: bool = False
+    PHASE: dl_pydantic.JsonableTimedelta | None = None
 
 
 class TemporalSchedulesDynConfig(dl_dynconfig.DynConfig):
+    ENABLED: bool = True
     SYNC_INTERVAL: dl_pydantic.JsonableTimedelta = dl_pydantic.JsonableTimedelta(seconds=60)
     SCHEDULES: list[ScheduleConfig] = pydantic.Field(default_factory=list)
