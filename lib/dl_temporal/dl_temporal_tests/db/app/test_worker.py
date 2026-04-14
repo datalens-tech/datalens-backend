53ec00f025e83b2bd83b10607eac9a1f30c6ed6a
Sergei Borodin <seray@yandex-team.ru>
2026-04-14 11:04:45 +0200

    feat: BI-7239 add list_schedule_executions method (d559cddd9)
    list schedule

--- lib/dl_temporal/dl_temporal_tests/db/app/test_worker.py
+++ lib/dl_temporal/dl_temporal_tests/db/app/test_worker.py
@@ -1,7 +1,9 @@
+import asyncio
 import datetime
 import uuid
 
 import pytest
+import temporalio.client
 
 import dl_pydantic
 import dl_temporal
@@ -100,3 +102,54 @@ async def test_error(
     assert workflow_desc.search_attributes.get(dl_temporal.base.SearchAttribute.RESULT_CODE) == [
         "ERROR_RESULT_CODE",
     ]
+
+
+@pytest.mark.asyncio
+async def test_list_schedule_executions(
+    temporal_client: dl_temporal.TemporalClient,
+    temporal_task_queue: str,
+) -> None:
+    schedule_id = str(uuid.uuid4())
+
+    params = workflows.Workflow.Params(
+        workflow_int_param=1,
+        workflow_str_param="test",
+        workflow_bool_param=True,
+        workflow_list_param=[1, 2, 3],
+        workflow_dict_param={"1": 1, "2": 2, "3": 3},
+        workflow_timedelta_param=dl_pydantic.JsonableTimedelta(seconds=1),
+        workflow_uuid_param=dl_pydantic.JsonableUUID(str(uuid.uuid4())),
+        workflow_date_param=dl_pydantic.JsonableDate.today(),
+        workflow_datetime_param=dl_pydantic.JsonableDatetime.now(tz=datetime.timezone.utc),
+        workflow_datetime_with_timezone_param=dl_pydantic.JsonableDatetimeWithTimeZone.now(tz=datetime.timezone.utc),
+        workflow_nested_param=common.NestedModel(test_int=1),
+        parent_context=dl_temporal.ParentContext(request_id="test_list_schedule_executions"),
+        return_error=False,
+    )
+    handle = await temporal_client.create_schedule(
+        schedule_id=schedule_id,
+        workflow=workflows.Workflow,
+        params=params,
+        task_queue=temporal_task_queue,
+        spec=temporalio.client.ScheduleSpec(
+            intervals=[temporalio.client.ScheduleIntervalSpec(every=datetime.timedelta(hours=1))],
+        ),
+    )
+
+    await handle.trigger()
+
+    try:
+        executions: list[temporalio.api.workflow.v1.WorkflowExecutionInfo] = []
+        deadline = asyncio.get_running_loop().time() + 10
+        while True:
+            executions, _ = await temporal_client.list_schedule_executions(schedule_id, page_size=10)
+            if len(executions) >= 1:
+                break
+            if asyncio.get_running_loop().time() > deadline:
+                pytest.fail("Timeout waiting for executions")
+            await asyncio.sleep(0.1)
+
+        assert len(executions) == 1
+        assert executions[0].execution.workflow_id != ""
+    finally:
+        await handle.delete()
