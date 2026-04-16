0419cf1974738c1ee667fa92355874bee58c9ef5
Julia <cl.chl0rum@gmail.com>
2026-04-16 10:54:20 +0300

    feat: BI-7172 starrocks functions: window (a30dd9780)
    * starrocks window functions
    
    * add mutator

--- lib/dl_connector_starrocks/dl_connector_starrocks_tests/db/formula/test_functions_window.py
+++ lib/dl_connector_starrocks/dl_connector_starrocks_tests/db/formula/test_functions_window.py
@@ -1,23 +1,7 @@
 from dl_formula_testing.testcases.functions_window import DefaultWindowFunctionFormulaConnectorTestSuite
-from dl_testing.regulated_test import (
-    RegulatedTestCase,
-    RegulatedTestParams,
-)
 
 from dl_connector_starrocks_tests.db.formula.base import StarRocksTestBase
 
 
-class TestWindowFunctionStarRocks(StarRocksTestBase, RegulatedTestCase, DefaultWindowFunctionFormulaConnectorTestSuite):
-    test_params = RegulatedTestParams(
-        mark_tests_failed={
-            DefaultWindowFunctionFormulaConnectorTestSuite.test_sum: "TODO: BI-7171",
-            DefaultWindowFunctionFormulaConnectorTestSuite.test_min: "TODO: BI-7171",
-            DefaultWindowFunctionFormulaConnectorTestSuite.test_max: "TODO: BI-7171",
-            DefaultWindowFunctionFormulaConnectorTestSuite.test_count: "TODO: BI-7171",
-            DefaultWindowFunctionFormulaConnectorTestSuite.test_avg: "TODO: BI-7171",
-            DefaultWindowFunctionFormulaConnectorTestSuite.test_sum_if: "TODO: BI-7171",
-            DefaultWindowFunctionFormulaConnectorTestSuite.test_count_if: "TODO: BI-7171",
-            DefaultWindowFunctionFormulaConnectorTestSuite.test_avg_if: "TODO: BI-7171",
-            DefaultWindowFunctionFormulaConnectorTestSuite.test_lag: "TODO: BI-7171",
-        },
-    )
+class TestWindowFunctionStarRocks(StarRocksTestBase, DefaultWindowFunctionFormulaConnectorTestSuite):
+    pass
