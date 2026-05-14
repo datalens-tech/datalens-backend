2c1e880f901ac2e165e68af8beddb4031db038e6
catsona <creampie@yandex-team.ru>
2026-05-14 18:14:13 +0300

    feat: BI-7129: Add dl_extract with common enums (c1d3369c4)

---
+++ lib/dl_extract/dl_extract/constants.py
@@ -0,0 +1,8 @@
+from dl_constants.enums import (
+    ConnectionType,
+    DataSourceType,
+)
+
+
+CONNECTION_TYPE_EXTRACT = ConnectionType.declare("extract")
+SOURCE_TYPE_EXTRACT = DataSourceType.declare("EXTRACT")
